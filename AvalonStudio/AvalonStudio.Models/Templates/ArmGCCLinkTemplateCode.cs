4392d17f0da57ab31163a51fc3a348a4ab132214
unknown <danw@THINKE73.GridXitekLtd.local>
2015-09-25 12:45:07 +0100

    added missing files.

---
+++ AvalonStudio/AvalonStudio.Models/Templates/ArmGCCLinkTemplateCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvalonStudio.Models.Solutions;
+
+namespace AvalonStudio.Models.Templates
+{
+    partial class ArmGCCLinkTemplate
+    {
+        public ArmGCCLinkTemplate(ProjectConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private ProjectConfiguration configuration;
+    }
+}
