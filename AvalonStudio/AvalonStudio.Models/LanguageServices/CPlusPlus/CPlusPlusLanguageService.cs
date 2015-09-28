c15e2b3b472a2a6da890d45f33dc116c0a1967c5
dan.walmsley <dan.walmsley@gxgroup.com>
2015-09-28 16:21:46 +0100

    Added skeleton classes for language services.

---
+++ AvalonStudio/AvalonStudio.Models/LanguageServices/CPlusPlus/CPlusPlusLanguageService.cs
@@ -0,0 +1,13 @@
+﻿namespace AvalonStudio.Models.LanguageServices.CPlusPlus
+{
+    using System;
+    using System.Collections.Generic;
+
+    class CPlusPlusLanguageService : LanguageService
+    {
+        public List<CodeCompletionData> CodeCompleteAt(uint line, uint column)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
