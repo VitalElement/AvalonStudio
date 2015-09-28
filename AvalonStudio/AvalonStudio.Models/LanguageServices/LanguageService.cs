4aef2d6705e4ae859d13f6ea8feb84b407d3dff6
dan.walmsley <dan.walmsley@gxgroup.com>
2015-09-28 16:21:46 +0100

    Added skeleton classes for language services.

---
+++ AvalonStudio/AvalonStudio.Models/LanguageServices/LanguageService.cs
@@ -0,0 +1,9 @@
+﻿namespace AvalonStudio.Models.LanguageServices
+{
+    using System.Collections.Generic;
+
+    interface LanguageService
+    {
+        List<CodeCompletionData> CodeCompleteAt(uint line, uint column);
+    }
+}
