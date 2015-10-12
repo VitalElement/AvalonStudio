89757823f70e7ae072c3ec1e8291a2beffa4c237
danwalmsley <dan@walms.co.uk>
2015-10-12 10:46:24 +0100

    Added basic text editor implementation based on Perspex textbox.

--- AvalonStudio/AvalonStudio/TextEditor/TextView.cs
+++ AvalonStudio/AvalonStudio/TextEditor/TextView.cs
@@ -6,238 +6,425 @@
     using Perspex.Controls.Primitives;
     using Perspex.Input;
     using Perspex.Media;
+    using Perspex.Threading;
+    using System.Reactive.Linq;
+    using Perspex.VisualTree;
 
-    public class TextView : Control, IScrollInfo
+    public class TextView : TextBlock
     {
+        public static readonly PerspexProperty<int> CaretIndexProperty =
+            TextBox.CaretIndexProperty.AddOwner<TextView>();
+
+        public static readonly PerspexProperty<int> SelectionStartProperty =
+            TextBox.SelectionStartProperty.AddOwner<TextView>();
+
+        public static readonly PerspexProperty<int> SelectionEndProperty =
+            TextBox.SelectionEndProperty.AddOwner<TextView>();
+
+        private readonly DispatcherTimer _caretTimer;
+
+        private bool _caretBlink;
+
+        private IObservable<bool> _canScrollHorizontally;
+
         public TextView()
         {
-            Text = "Not Clicked";
+            _caretTimer = new DispatcherTimer();
+            _caretTimer.Interval = TimeSpan.FromMilliseconds(500);
+            _caretTimer.Tick += CaretTimerTick;
 
+            _canScrollHorizontally = GetObservable(TextWrappingProperty)
+                .Select(x => x == TextWrapping.NoWrap);
+
+            Observable.Merge(
+                GetObservable(SelectionStartProperty),
+                GetObservable(SelectionEndProperty))
+                .Subscribe(_ => InvalidateFormattedText());
+
+            GetObservable(CaretIndexProperty)
+                .Subscribe(CaretIndexChanged);
         }
 
-        public static readonly PerspexProperty<string> TextProperty = PerspexProperty.Register<TextView, string>("Text", null, false, BindingMode.TwoWay);
-
-        protected override void OnPointerPressed(PointerPressEventArgs e)
+        public int CaretIndex
         {
-
+            get { return GetValue(CaretIndexProperty); }
+            set { SetValue(CaretIndexProperty, value); }
         }
 
-        protected override void OnPointerMoved(PointerEventArgs e)
+        public int SelectionStart
         {
-            double y = e.Device.GetPosition(this).Y;
-            y /= lineHeight;
-            InvalidateVisual();
+            get { return GetValue(SelectionStartProperty); }
+            set { SetValue(SelectionStartProperty, value); }
         }
 
-        double lineHeight = 20;        
+        public int SelectionEnd
+        {
+            get { return GetValue(SelectionEndProperty); }
+            set { SetValue(SelectionEndProperty, value); }
+        }
+
+        public int GetCaretIndex(Point point)
+        {
+            var hit = FormattedText.HitTestPoint(point);
+            return hit.TextPosition + (hit.IsTrailing ? 1 : 0);
+        }
 
         public override void Render(DrawingContext context)
         {
-            var textSize = new FormattedText("X", "Consolas", 12, FontStyle.Normal, TextAlignment.Left, FontWeight.Normal) { Constraint = Bounds.Size };
-            lineHeight = textSize.Measure().Height;
+            var selectionStart = SelectionStart;
+            var selectionEnd = SelectionEnd;
 
-            if (Text != null)
+            if (selectionStart != selectionEnd)
             {
-                var text = new FormattedText(Text, "Consolas", 12, FontStyle.Normal, TextAlignment.Left, FontWeight.Normal) { Constraint = Bounds.Size };
+                var start = Math.Min(selectionStart, selectionEnd);
+                var length = Math.Max(selectionStart, selectionEnd) - start;
+                var rects = FormattedText.HitTestTextRange(start, length);
 
-                var renderSize = new Rect(Bounds.Size);
+                var brush = new SolidColorBrush(0xff086f9e);
 
-                context.FillRectangle(Brush.Parse("#1e1e1e"), renderSize);
-
-                var lines = (int)(Bounds.Height / lineHeight);
-
-                context.FillRectangle(Brush.Parse("#242424"), new Rect(Bounds.X + 14, (SelectedLine * lineHeight), Bounds.Width, lineHeight), 4);
-                context.FillRectangle(Brush.Parse("#0f0f0f"), new Rect(Bounds.X + 14 + 2, (SelectedLine * lineHeight) + 2, Bounds.Width - 4, lineHeight - 4), 4);
-
-                for (int i = 0; i < lines; i++)
+                foreach (var rect in rects)
                 {
-                    context.DrawText(Brushes.WhiteSmoke, new Point(0, 4 + (i * lineHeight)), new FormattedText((i + 1).ToString(), "Consolas", 12, FontStyle.Normal, TextAlignment.Left, FontWeight.Normal) { Constraint = Bounds.Size });
+                    context.FillRectangle(brush, rect);
+                }
+            }
+
+            base.Render(context);
+
+            if (selectionStart == selectionEnd)
+            {
+                var charPos = FormattedText.HitTestTextPosition(CaretIndex);
+
+                var backgroundColor = (((Control)TemplatedParent).GetValue(BackgroundProperty) as SolidColorBrush)?.Color;
+                var caretBrush = Brushes.Black;
+
+                if (backgroundColor.HasValue)
+                {
+                    byte red = (byte)~(backgroundColor.Value.R);
+                    byte green = (byte)~(backgroundColor.Value.G);
+                    byte blue = (byte)~(backgroundColor.Value.B);
+
+                    caretBrush = new SolidColorBrush(Color.FromRgb(red, green, blue));
                 }
 
-                text.SetForegroundBrush(Brushes.Red, 50, 20);
-                text.SetForegroundBrush(Brushes.Pink, 10, 5);
-                text.SetForegroundBrush(Brushes.Green, 200, 500);
+                if (_caretBlink)
+                {
+                    var x = Math.Floor(charPos.X) + 0.5;
+                    var y = Math.Floor(charPos.Y) + 0.5;
+                    var b = Math.Ceiling(charPos.Bottom) - 0.5;
 
-                
-                context.DrawText(Brushes.White, new Point(18, 4), text);
-
-                
+                    context.DrawLine(
+                        new Pen(caretBrush, 1),
+                        new Point(x, y),
+                        new Point(x, b));
+                }
             }
         }
 
-        public void LineLeft()
+        public void ShowCaret()
         {
-            throw new NotImplementedException();
+            _caretBlink = true;
+            _caretTimer.Start();
+            InvalidateVisual();
         }
 
-        public void LineRight()
+        public void HideCaret()
         {
-            throw new NotImplementedException();
+            _caretBlink = false;
+            _caretTimer.Stop();
+            InvalidateVisual();
         }
 
-        public void MouseWheelLeft()
+        internal void CaretIndexChanged(int caretIndex)
         {
-            throw new NotImplementedException();
-        }
-
-        public void MouseWheelRight()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void PageLeft()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void PageRight()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void LineDown()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void LineUp()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void MouseWheelDown()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void MouseWheelUp()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void PageDown()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void PageUp()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Rect MakeVisible(Visual visual, Rect rectangle)
-        {
-            throw new NotImplementedException();
-        }
-
-        private int selectedLine;
-        public int SelectedLine
-        {
-            get { return selectedLine; }
-            set { selectedLine = value; }
-        }
-
-        public string Text
-        {
-            get { return GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
-        }
-
-        public double ExtentWidth
-        {
-            get
+            if (this.GetVisualParent() != null)
             {
-                throw new NotImplementedException();
+                _caretBlink = true;
+                _caretTimer.Stop();
+                _caretTimer.Start();
+                InvalidateVisual();
+
+                var rect = FormattedText.HitTestTextPosition(caretIndex);
+                this.BringIntoView(rect);
             }
         }
 
-        public double ViewportWidth
+        protected override FormattedText CreateFormattedText(Size constraint)
         {
-            get
+            var result = base.CreateFormattedText(constraint);
+            var selectionStart = SelectionStart;
+            var selectionEnd = SelectionEnd;
+            var start = Math.Min(selectionStart, selectionEnd);
+            var length = Math.Max(selectionStart, selectionEnd) - start;
+
+            if (length > 0)
             {
-                throw new NotImplementedException();
+                result.SetForegroundBrush(Brushes.White, start, length);
+            }
+
+            return result;
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var text = Text;
+
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                return base.MeasureOverride(availableSize);
+            }
+            else
+            {
+                // TODO: Pretty sure that measuring "X" isn't the right way to do this...
+                using (var formattedText = new FormattedText(
+                    "X",
+                    FontFamily,
+                    FontSize,
+                    FontStyle,
+                    TextAlignment.Left,
+                    FontWeight))
+                {
+                    return formattedText.Measure();
+                }
             }
         }
 
-        public double HorizontalOffset
+        private void CaretTimerTick(object sender, EventArgs e)
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public bool CanHorizontallyScroll
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public double VerticalOffset
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public double ExtentHeight
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public double ViewportHeight
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public bool CanVerticallyScroll
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public ScrollViewer ScrollOwner
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
+            _caretBlink = !_caretBlink;
+            InvalidateVisual();
         }
     }
+    //public class TextView : Control, IScrollInfo
+    //{
+    //    public TextView()
+    //    {
+    //        Text = "Not Clicked";
+
+    //    }
+
+    //    public static readonly PerspexProperty<string> TextProperty = PerspexProperty.Register<TextView, string>("Text", null, false, BindingMode.TwoWay);
+
+    //    protected override void OnPointerPressed(PointerPressEventArgs e)
+    //    {
+
+    //    }
+
+    //    protected override void OnPointerMoved(PointerEventArgs e)
+    //    {
+    //        double y = e.Device.GetPosition(this).Y;
+    //        y /= lineHeight;
+    //        InvalidateVisual();
+    //    }
+
+    //    double lineHeight = 20;        
+
+    //    public override void Render(DrawingContext context)
+    //    {
+    //        var textSize = new FormattedText("X", "Consolas", 12, FontStyle.Normal, TextAlignment.Left, FontWeight.Normal) { Constraint = Bounds.Size };
+    //        lineHeight = textSize.Measure().Height;
+
+    //        if (Text != null)
+    //        {
+    //            var text = new FormattedText(Text, "Consolas", 12, FontStyle.Normal, TextAlignment.Left, FontWeight.Normal) { Constraint = Bounds.Size };
+
+    //            var renderSize = new Rect(Bounds.Size);
+
+    //            context.FillRectangle(Brush.Parse("#1e1e1e"), renderSize);
+
+    //            var lines = (int)(Bounds.Height / lineHeight);
+
+    //            context.FillRectangle(Brush.Parse("#242424"), new Rect(Bounds.X + 14, (SelectedLine * lineHeight), Bounds.Width, lineHeight), 4);
+    //            context.FillRectangle(Brush.Parse("#0f0f0f"), new Rect(Bounds.X + 14 + 2, (SelectedLine * lineHeight) + 2, Bounds.Width - 4, lineHeight - 4), 4);
+
+    //            for (int i = 0; i < lines; i++)
+    //            {
+    //                context.DrawText(Brushes.WhiteSmoke, new Point(0, 4 + (i * lineHeight)), new FormattedText((i + 1).ToString(), "Consolas", 12, FontStyle.Normal, TextAlignment.Left, FontWeight.Normal) { Constraint = Bounds.Size });
+    //            }
+
+    //            text.SetForegroundBrush(Brushes.Red, 50, 20);
+    //            text.SetForegroundBrush(Brushes.Pink, 10, 5);
+    //            text.SetForegroundBrush(Brushes.Green, 200, 500);
+
+                
+    //            context.DrawText(Brushes.White, new Point(18, 4), text);
+
+                
+    //        }
+    //    }
+
+    //    public void LineLeft()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void LineRight()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void MouseWheelLeft()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void MouseWheelRight()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void PageLeft()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void PageRight()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void LineDown()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void LineUp()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void MouseWheelDown()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void MouseWheelUp()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void PageDown()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void PageUp()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Rect MakeVisible(Visual visual, Rect rectangle)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    private int selectedLine;
+    //    public int SelectedLine
+    //    {
+    //        get { return selectedLine; }
+    //        set { selectedLine = value; }
+    //    }
+
+    //    public string Text
+    //    {
+    //        get { return GetValue(TextProperty); }
+    //        set { SetValue(TextProperty, value); }
+    //    }
+
+    //    public double ExtentWidth
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public double ViewportWidth
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public double HorizontalOffset
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+
+    //        set
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public bool CanHorizontallyScroll
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+
+    //        set
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public double VerticalOffset
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+
+    //        set
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public double ExtentHeight
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public double ViewportHeight
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public bool CanVerticallyScroll
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+
+    //        set
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+
+    //    public ScrollViewer ScrollOwner
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+
+    //        set
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+    //}
 }
