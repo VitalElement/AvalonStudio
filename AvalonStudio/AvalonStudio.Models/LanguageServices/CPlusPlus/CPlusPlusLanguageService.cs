0b0f02e7835e9664798f77130ef7239530552c0f
danwalmsley <dan@walms.co.uk>
2015-10-13 10:55:11 +0100

    added correct abstraction of analysis down to language service interface.

--- AvalonStudio/AvalonStudio.Models/LanguageServices/CPlusPlus/CPlusPlusLanguageService.cs
+++ AvalonStudio/AvalonStudio.Models/LanguageServices/CPlusPlus/CPlusPlusLanguageService.cs
@@ -2,12 +2,28 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
 
-    class CPlusPlusLanguageService : LanguageService
+    public class CPlusPlusLanguageService : ILanguageService
     {
         public List<CodeCompletionData> CodeCompleteAt(uint line, uint column)
         {
             throw new NotImplementedException();
+        }
+
+        public void RunCodeAnalysis(Func<bool> interruptRequested)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+
+                if (interruptRequested())
+                {
+                    return;
+                }
+            }
+
+            Console.WriteLine("Code analysis complete.");
         }
     }
 }
