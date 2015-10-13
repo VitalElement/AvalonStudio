41eea8f1d1612f85011be79e3b960132a002ff08
danwalmsley <dan@walms.co.uk>
2015-10-13 10:31:18 +0100

    added ability for Editor to be shutdown.

--- AvalonStudio/AvalonStudio/Program.cs
+++ AvalonStudio/AvalonStudio/Program.cs
@@ -1,26 +1,24 @@
-﻿using AvalonStudio.Controls;
-using Perspex.Input;
-using ReactiveUI;
-using System;
-
-namespace AvalonStudio
+﻿namespace AvalonStudio
 {
+    using AvalonStudio.Controls;
+
     class Program
     {        
         static void Main(string[] args)
         {            
             var app = new App();
-            EditorModel editorModel = new EditorModel();
-            Workspace.This = new Workspace(editorModel);
-            
-            ReactiveCommand.Create();
             var window = new MainWindow();
-			var vm = Workspace.This;
-            window.DataContext = vm;
 
+            EditorModel editorModel = new EditorModel();
+
+            Workspace.This = new Workspace(editorModel);
+            window.DataContext = Workspace.This;
 
             window.Show();
+
             app.Run(window);
+
+            editorModel.Shutdown();
         }
     }
 }
