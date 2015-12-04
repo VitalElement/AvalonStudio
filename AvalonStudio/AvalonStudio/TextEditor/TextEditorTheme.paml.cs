5d5aa863a44ce92489ed2573628c159a06f5ca2a
danwalmsley <dan@walms.co.uk>
2015-12-04 14:22:14 +0000

    Added xaml version of template and theme for text editor... Not working at the moment.

---
+++ AvalonStudio/AvalonStudio/TextEditor/TextEditorTheme.paml.cs
@@ -0,0 +1,13 @@
+﻿namespace AvalonStudio.TextEditor
+{
+    using Perspex.Markup.Xaml;
+    using Perspex.Styling;
+
+    public class TextEditorTheme : Styles
+    {
+        public TextEditorTheme()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
