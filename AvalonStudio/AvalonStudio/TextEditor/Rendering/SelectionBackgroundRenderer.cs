a0bea3b5becfc94279e111cb14e3e166bafb7823
danwalmsley <dan@walms.co.uk>
2015-12-06 19:58:18 +0000

    Added selection background render and re-wrote GetRects for Segment to actually work.

---
+++ AvalonStudio/AvalonStudio/TextEditor/Rendering/SelectionBackgroundRenderer.cs
@@ -0,0 +1,39 @@
+﻿namespace AvalonStudio.TextEditor.Rendering
+{
+    using Document;
+    using Perspex.Media;
+
+    class SelectionBackgroundRenderer : IBackgroundRenderer
+    {
+        public SelectionBackgroundRenderer()
+        {
+            selectionBrush = Brush.Parse("#AA569CD6");
+        }
+
+        private Brush selectionBrush;
+
+        public void Draw(TextView textView, DrawingContext drawingContext)
+        {
+            if (textView.SelectionStart != textView.SelectionEnd)
+            {
+                TextSegment selection;
+
+                if (textView.SelectionEnd > textView.SelectionStart)
+                {
+                    selection = new TextSegment { StartOffset = textView.SelectionStart, EndOffset = textView.SelectionEnd };
+                }
+                else
+                {
+                    selection = new TextSegment { StartOffset = textView.SelectionEnd, EndOffset = textView.SelectionStart };
+                }
+
+                var rects = VisualLineGeometryBuilder.GetRectsForSegment(textView, selection);
+
+                foreach (var rect in rects)
+                {
+                    drawingContext.FillRectangle(selectionBrush, rect);
+                }
+            }
+        }
+    }
+}
