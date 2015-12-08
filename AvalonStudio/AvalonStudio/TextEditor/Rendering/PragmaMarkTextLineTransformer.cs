3f1269b43cd4623d602986049698d874ee006ee5
danwalmsley <dan@walms.co.uk>
2015-12-08 12:47:21 +0000

    Added pragma mark text transformer.

---
+++ AvalonStudio/AvalonStudio/TextEditor/Rendering/PragmaMarkTextLineTransformer.cs
@@ -0,0 +1,21 @@
+﻿namespace AvalonStudio.TextEditor.Rendering
+{
+    using System;    
+    using Document;
+    using Perspex.Media;
+
+    class PragmaMarkTextLineTransformer : IDocumentLineTransformer
+    {
+        private SolidColorBrush pragmaBrush = new SolidColorBrush(Color.FromArgb(0x88, 0xB8, 0x48, 0xFF));
+        private SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(0xD0, 0xB8, 0x48, 0xFF));
+
+        public void TransformLine(DocumentLine line, FormattedText formattedText)
+        {
+            if (formattedText.Text.Contains("#pragma mark"))
+            {
+                formattedText.SetForegroundBrush(pragmaBrush, 0, 12);
+                formattedText.SetForegroundBrush(brush, 12, formattedText.Text.Length);                
+            }
+        }
+    }
+}
