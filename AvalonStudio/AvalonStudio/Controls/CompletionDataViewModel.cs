61b738865cbaf10acc0770a0aff0971b9b309d09
danwalmsley <dan@walms.co.uk>
2015-12-10 16:14:49 +0000

    began implementing code completion.

---
+++ AvalonStudio/AvalonStudio/Controls/CompletionDataViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AvalonStudio.Controls
+{
+    using AvalonStudio.MVVM;
+    using Models.LanguageServices;
+
+
+    public class CompletionDataViewModel : ViewModel<CodeCompletionData>
+    {
+        public CompletionDataViewModel (CodeCompletionData model) : base (model)
+        {
+
+        }
+
+        public string Title { get { return Model.Suggestion; } }
+    }
+}
