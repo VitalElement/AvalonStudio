dfc5faa979575be9c6cdcda85ee40136696de51c
danwalmsley <dan@walms.co.uk>
2015-12-11 10:54:47 +0000

    implemented style key on metro window.

--- AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
+++ AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
@@ -1,12 +1,15 @@
 ﻿namespace AvalonStudio.Controls
 {
-    using Perspex.Controls;    
+    using Perspex.Controls;
+    using Perspex.Styling;
+    using System;
 
-    public class MetroWindow : Window
+    public class MetroWindow : Window, IStyleable
     {
         public MetroWindow()
         {
-            //HasSystemDecorations = false;
         }
+
+        Type IStyleable.StyleKey => typeof(MetroWindow);
     }
 }
