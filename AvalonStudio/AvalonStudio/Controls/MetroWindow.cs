a9a4d0b22e7f188e00566a3f22317f1428326272
danwalmsley <dan@walms.co.uk>
2015-12-12 03:06:22 +0000

    fully implemented resize grips.

--- AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
+++ AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
@@ -14,13 +14,52 @@
         Type IStyleable.StyleKey => typeof(MetroWindow);
 
         private Grid titleBar;
-        private Border windowBorder;
         private Button restoreButton;
         private Button closeButton;
+        private Grid topHorizontalGrip;
+        private Grid bottomHorizontalGrip;
+        private Grid leftVerticalGrip;
+        private Grid rightVerticalGrip;
+        private Grid topLeftGrip;
+        private Grid bottomLeftGrip;
+        private Grid topRightGrip;
+        private Grid bottomRightGrip;
 
         protected override void OnPointerPressed(PointerPressEventArgs e)
         {
-            if(titleBar.IsPointerOver)
+            if (topHorizontalGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.North);
+            }
+            else if (bottomHorizontalGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.South);
+            }
+            else if (leftVerticalGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.West);
+            }
+            else if (rightVerticalGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.East);
+            }
+            else if(topLeftGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.NorthWest);
+            }
+            else if(bottomLeftGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.SouthWest);
+            }
+            else if (topRightGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.NorthEast);
+            }
+            else if(bottomRightGrip.IsPointerOver)
+            {
+                BeginResizeDrag(WindowEdge.SouthEast);
+            }
+            else if (titleBar.IsPointerOver)
             {
                 BeginMoveDrag();
             }
@@ -31,9 +70,18 @@
         protected override void OnTemplateApplied(INameScope nameScope)
         {
             titleBar = nameScope.Find<Grid>("titlebar");
-            windowBorder = nameScope.Find<Border>("windowBorder");
             restoreButton = nameScope.Find<Button>("restoreButton");
             closeButton = nameScope.Find<Button>("closeButton");
+
+            topHorizontalGrip = nameScope.Find<Grid>("topHorizontalGrip");
+            bottomHorizontalGrip = nameScope.Find<Grid>("bottomHorizontalGrip");
+            leftVerticalGrip = nameScope.Find<Grid>("leftVerticalGrip");
+            rightVerticalGrip = nameScope.Find<Grid>("rightVerticalGrip");
+
+            topLeftGrip = nameScope.Find<Grid>("topLeftGrip");
+            bottomLeftGrip = nameScope.Find<Grid>("bottomLeftGrip");
+            topRightGrip = nameScope.Find<Grid>("topRightGrip");
+            bottomRightGrip = nameScope.Find<Grid>("bottomRightGrip");            
 
             restoreButton.Click += (sender, e) =>
             {
@@ -45,7 +93,6 @@
                 Close();
             };
 
-            windowBorder.Cursor = new Cursor(StandardCursorType.SizeAll);
             base.OnTemplateApplied(nameScope);
         }
     }
