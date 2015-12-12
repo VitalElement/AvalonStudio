b1155254f1881a179e9f51dbae8f9f60bd440c7c
danwalmsley <dan@walms.co.uk>
2015-12-12 00:55:08 +0000

    implemented draging, and added placeholder icon and buttons.

--- AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
+++ AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
@@ -3,6 +3,7 @@
     using Perspex.Controls;
     using Perspex.Styling;
     using System;
+    using Perspex.Input;
 
     public class MetroWindow : Window, IStyleable
     {
@@ -11,5 +12,27 @@
         }
 
         Type IStyleable.StyleKey => typeof(MetroWindow);
+
+        private Grid titleBar;
+        private Border windowBorder;
+
+        protected override void OnPointerPressed(PointerPressEventArgs e)
+        {
+            if(titleBar.IsPointerOver)
+            {
+                BeginMoveDrag();
+            }
+
+            base.OnPointerPressed(e);
+        }
+
+        protected override void OnTemplateApplied(INameScope nameScope)
+        {
+            titleBar = nameScope.Find<Grid>("titlebar");
+            windowBorder = nameScope.Find<Border>("windowBorder");
+
+            windowBorder.Cursor = new Cursor(StandardCursorType.SizeAll);
+            base.OnTemplateApplied(nameScope);
+        }
     }
 }
