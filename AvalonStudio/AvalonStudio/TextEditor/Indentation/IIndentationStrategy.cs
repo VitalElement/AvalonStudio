cd4725d16842f17538b8c9de87d13b47295630c2
danwalmsley <dan@walms.co.uk>
2015-12-21 00:57:28 +0000

    Consolidated TextUtilities, Added IndentationStrategy interface from AvalonEdit.

---
+++ AvalonStudio/AvalonStudio/TextEditor/Indentation/IIndentationStrategy.cs
@@ -0,0 +1,21 @@
+﻿namespace AvalonStudio.TextEditor.Indentation
+{
+    using AvalonStudio.TextEditor.Document;
+
+    /// <summary>
+	/// Strategy how the text editor handles indentation when new lines are inserted.
+	/// </summary>
+	public interface IIndentationStrategy
+    {
+        /// <summary>
+        /// Sets the indentation for the specified line.
+        /// Usually this is constructed from the indentation of the previous line.
+        /// </summary>
+        void IndentLine(TextDocument document, DocumentLine line);
+
+        /// <summary>
+        /// Reindents a set of lines.
+        /// </summary>
+        void IndentLines(TextDocument document, int beginLine, int endLine);
+    }
+}
