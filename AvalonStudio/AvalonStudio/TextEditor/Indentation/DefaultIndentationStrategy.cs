63fd29716acbbbf07928f60803126590b43ec4ec
danwalmsley <dan@walms.co.uk>
2015-12-21 00:57:28 +0000

    Consolidated TextUtilities, Added IndentationStrategy interface from AvalonEdit.

---
+++ AvalonStudio/AvalonStudio/TextEditor/Indentation/DefaultIndentationStrategy.cs
@@ -0,0 +1,42 @@
+﻿namespace AvalonStudio.TextEditor.Indentation
+{
+    using AvalonStudio.TextEditor.Document;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+	/// Handles indentation by copying the indentation from the previous line.
+	/// Does not support indenting multiple lines.
+	/// </summary>
+	public class DefaultIndentationStrategy : IIndentationStrategy
+    {
+        /// <inheritdoc/>
+        public virtual void IndentLine(TextDocument document, DocumentLine line)
+        {
+            if (document == null)
+                throw new ArgumentNullException("document");
+            if (line == null)
+                throw new ArgumentNullException("line");
+            DocumentLine previousLine = line.PreviousLine;
+            if (previousLine != null)
+            {
+                ISegment indentationSegment = TextUtilities.GetWhitespaceAfter(document, previousLine.Offset);
+                string indentation = document.GetText(indentationSegment);
+                // copy indentation to line
+                indentationSegment = TextUtilities.GetWhitespaceAfter(document, line.Offset);
+                document.Replace(indentationSegment, indentation);
+            }
+        }
+
+        /// <summary>
+        /// Does nothing: indenting multiple lines is useless without a smart indentation strategy.
+        /// </summary>
+        public virtual void IndentLines(TextDocument document, int beginLine, int endLine)
+        {
+        }
+    }
+
+}
