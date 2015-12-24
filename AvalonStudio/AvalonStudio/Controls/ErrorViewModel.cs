19c25913f1ea45e59b96bde1e0ecd6eda56cb119
danwalmsley <dan@walms.co.uk>
2015-12-24 22:08:49 +0000

    Implemented placeholder Error Level Icon in error list, and error list now shows other relevant fields.

--- AvalonStudio/AvalonStudio/Controls/ErrorViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/ErrorViewModel.cs
@@ -2,7 +2,8 @@
 {
     using AvalonStudio.Languages;
     using AvalonStudio.MVVM;
-
+    using Perspex.Media;
+    using System.IO;
     public class ErrorViewModel : ViewModel<Diagnostic>
     {
         public ErrorViewModel (Diagnostic model) : base (model)
@@ -10,13 +11,36 @@
             offset = model.Offset;
         }
 
-        public string File { get { return Model.File; } }
+        public string File { get { return Path.GetFileName(Model.File); } }
 
         public string Spelling { get { return Model.Spelling; } }
+
+        public string Project { get { return Model.Project.Name; } }
+
+        public int Line { get { return Model.Line; } }
 
         private int offset { get; set; }
 
         public DiagnosticLevel Level { get { return Model.Level; } }
+
+        public Brush LevelBrush
+        {
+            get
+            {
+                switch(Level)
+                {
+                    case DiagnosticLevel.Error:
+                    case DiagnosticLevel.Fatal:
+                        return Brush.Parse("#E51400");
+
+                    case DiagnosticLevel.Warning:
+                        return Brush.Parse("#FFCC00");
+
+                    default:
+                        return Brush.Parse("#1BA1E2");
+                }
+            }
+        }
 
         public bool IsEqual(ErrorViewModel other)
         {
