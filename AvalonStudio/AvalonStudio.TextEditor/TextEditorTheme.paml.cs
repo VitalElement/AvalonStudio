5ae09c895c5ad5b70b8eccfaa79a51d72de6a22c
danwalmsley <dan@walms.co.uk>
2015-12-25 14:07:57 +0000

    finalsed migration of TextEditor to its own assembly, now running normally.

--- AvalonStudio/AvalonStudio.TextEditor/TextEditorTheme.paml.cs
+++ AvalonStudio/AvalonStudio.TextEditor/TextEditorTheme.paml.cs
@@ -2,11 +2,19 @@
 {
     using Perspex.Markup.Xaml;
     using Perspex.Styling;
+    using System;
 
     public class TextEditorTheme : Styles
     {
         public TextEditorTheme()
         {
+            var res = System.Reflection.Assembly.GetAssembly(typeof(TextEditorTheme)).GetManifestResourceNames();
+
+            foreach (var resource in res)
+            {
+                Console.WriteLine(resource);
+            }
+
             PerspexXamlLoader.Load(this);
         }
     }
