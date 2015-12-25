dd2b961597b4e1da3d07a29a8805a1a8a43a655a
danwalmsley <dan@walms.co.uk>
2015-12-25 14:07:57 +0000

    finalsed migration of TextEditor to its own assembly, now running normally.

--- AvalonStudio/AvalonStudio/Program.cs
+++ AvalonStudio/AvalonStudio/Program.cs
@@ -5,7 +5,7 @@
     public class Program
     {
         private static void Main(string[] args)
-        {
+        {            
             if (args == null)
             {
                 throw new ArgumentNullException(nameof(args));
