653d74327a95be465a311d5af240437f1d680c05
danwalmsley <dan@walms.co.uk>
2015-12-25 13:42:17 +0000

    Moved TextEditor to seperate assembly.

---
+++ AvalonStudio/AvalonStudio.TextEditor/TextEditor/Document/UndoOperationGroup.cs
@@ -0,0 +1,64 @@
+﻿namespace AvalonStudio.TextEditor.Document
+{
+    using Utils;
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// This class stacks the last x operations from the undostack and makes
+    /// one undo/redo operation from it.
+    /// </summary>
+    sealed class UndoOperationGroup : IUndoableOperationWithContext
+    {
+        IUndoableOperation[] undolist;
+
+        public UndoOperationGroup(Deque<IUndoableOperation> stack, int numops)
+        {
+            if (stack == null)
+            {
+                throw new ArgumentNullException("stack");
+            }
+
+            Debug.Assert(numops > 0, "UndoOperationGroup : numops should be > 0");
+            Debug.Assert(numops <= stack.Count);
+
+            undolist = new IUndoableOperation[numops];
+            for (int i = 0; i < numops; ++i)
+            {
+                undolist[i] = stack.PopBack();
+            }
+        }
+
+        public void Undo()
+        {
+            for (int i = 0; i < undolist.Length; ++i)
+            {
+                undolist[i].Undo();
+            }
+        }
+
+        public void Undo(UndoStack stack)
+        {
+            for (int i = 0; i < undolist.Length; ++i)
+            {
+                stack.RunUndo(undolist[i]);
+            }
+        }
+
+        public void Redo()
+        {
+            for (int i = undolist.Length - 1; i >= 0; --i)
+            {
+                undolist[i].Redo();
+            }
+        }
+
+        public void Redo(UndoStack stack)
+        {
+            for (int i = undolist.Length - 1; i >= 0; --i)
+            {
+                stack.RunRedo(undolist[i]);
+            }
+        }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.TextEditor/TextEditor/Rendering/IncludeTextLineTransformer.cs
@@ -0,0 +1,22 @@
+﻿namespace AvalonStudio.TextEditor.Rendering
+{
+    using Document;
+    using Perspex;
+    using Perspex.Media;
+    using System;
+
+    class IncludeTextLineTransformer : IDocumentLineTransformer
+    {
+        private Brush brush = Brush.Parse("#D69D85");
+
+        public void TransformLine(TextView textView, DrawingContext context, Rect lineBounds, VisualLine line)
+        {
+            if (line.RenderedText.Text.Contains("#include"))
+            {
+                int startIndex = line.RenderedText.Text.IndexOf("#include");
+
+                line.RenderedText.SetForegroundBrush(brush, startIndex, line.RenderedText.Text.Length - startIndex);                
+            }
+        }
+    }
+}
