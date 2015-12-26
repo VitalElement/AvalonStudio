256501ba2875a7c11fd1dd8e5c3fbe69d01d805a
danwalmsley <dan@walms.co.uk>
2015-12-26 00:33:27 +0000

    TextMarkerService is now implemented as a line transformer and so can access the text and work out how much of the line to highlight if the language service doesnt specify.

--- AvalonStudio/AvalonStudio.TextEditor/Rendering/IDocumentLineTransformer.cs
+++ AvalonStudio/AvalonStudio.TextEditor/Rendering/IDocumentLineTransformer.cs
@@ -1,8 +1,8 @@
 ﻿namespace AvalonStudio.TextEditor.Rendering
-{
-    using AvalonStudio.TextEditor.Document;
+{    
     using Perspex;
     using Perspex.Media;
+
     public interface IDocumentLineTransformer
     {
         void TransformLine(TextView textView, DrawingContext context, Rect lineBounds, VisualLine line);
