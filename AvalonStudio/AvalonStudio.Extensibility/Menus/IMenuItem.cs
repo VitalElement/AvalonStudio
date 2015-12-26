0d425710dd68389334246ac00eaed71de48fac71
danwalmsley <dan@walms.co.uk>
2015-12-26 17:27:20 +0000

    Added basics of Project types being able to add menu items.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Menus/IMenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace AvalonStudio.Extensibility.Menus
+{
+    using System.Windows.Input;
+
+    public interface IMenuItem
+    {
+        string Title { get; }
+        ICommand Command { get; } 
+    }
+}
