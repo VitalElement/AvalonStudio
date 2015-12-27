77e91ac7c6c1860b6aff15d5aee34bf8ee15c5bc
danwalmsley <dan@walms.co.uk>
2015-12-27 03:36:02 +0000

    Added further implementation of new package manager.

--- AvalonStudio/AvalonStudio.Extensibility/Utils/Constants.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Utils/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public const string WindowsPlatformString = "Windows";
-        public const string MacOSXPlatformString = "MaxOSX";
+        public const string MacOSXPlatformString = "Mac";
         public const string LinuxPlatformString = "Linux";
         public const string UnknownPlatformString = "Unknown";
     }
