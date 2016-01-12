5f4406385a5af4c536d23daf86ad3d327434f187
danwalmsley <dan@walms.co.uk>
2016-01-12 21:31:59 +0000

    Added an example of setting a ViewModel on an injected view.

---
+++ AvalonStudio/AvalonStudio.Projects.VEBuild/ToolchainSettingsFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace AvalonStudio.Projects.VEBuild
+{
+    using AvalonStudio.MVVM;
+    using ReactiveUI;
+    using System.Collections.Generic;
+
+    public class ToolchainSettingsFormViewModel : ViewModel
+    {
+        public ToolchainSettingsFormViewModel()
+        {
+            toolchains = new List<string>();
+            toolchains.Add("STM32");
+        }
+
+        private List<string> toolchains;
+        public List<string> Toolchains
+        {
+            get { return toolchains; }
+            set { this.RaiseAndSetIfChanged(ref toolchains, value); }
+        }
+
+    }
+}
