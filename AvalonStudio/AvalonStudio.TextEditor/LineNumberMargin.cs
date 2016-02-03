bd90b801eea8cefbe6ef09ae80032c51ba1eccdd
danwalmsley <dan@walms.co.uk>
2016-02-03 10:56:15 +0000

    fixed line number margin and correct copying of plugins to directory.

--- AvalonStudio/AvalonStudio.TextEditor/LineNumberMargin.cs
+++ AvalonStudio/AvalonStudio.TextEditor/LineNumberMargin.cs
@@ -13,14 +13,19 @@
 
         protected override Size MeasureOverride(Size availableSize)
         {
-            return new Size(100, availableSize.Height);
+            if (double.IsNaN(Width))
+            {
+                Width = 0;
+            }
+
+            return new Size(Width, availableSize.Height);
         }
 
         public override void Render(DrawingContext context, TextInfo textInfo)
         {
             if (textView.TextDocument != null)
             {                
-                Width = textInfo.CharWidth * textInfo.NumLines.ToString().Length + 5;
+                Width = (textInfo.CharWidth * textInfo.NumLines.ToString().Length) + 8;
 
                 if (textView != null)
                 {
@@ -28,7 +33,7 @@
 
                     for (int i = 0; i < textInfo.NumLines && (i + firstLine) <= textView.TextDocument.LineCount; i++)
                     {
-                        context.DrawText(Brush.Parse("#5d5d5d"), new Point(-5, textInfo.LineHeight * i), new FormattedText((i + firstLine).ToString(), "Consolas", textView.FontSize, FontStyle.Normal, TextAlignment.Right, FontWeight.Normal) { Constraint = new Size(Width, Bounds.Height) });
+                        context.DrawText(Brush.Parse("#5d5d5d"), new Point(-4, textInfo.LineHeight * i), new FormattedText((i + firstLine).ToString(), "Consolas", textView.FontSize, FontStyle.Normal, TextAlignment.Right, FontWeight.Normal) { Constraint = new Size(Width, Bounds.Height) });
                     }
 
                     context.DrawLine(new Pen(Brush.Parse("#5d5d5d")), new Point(Width, 0), new Point(Width, Bounds.Height));
