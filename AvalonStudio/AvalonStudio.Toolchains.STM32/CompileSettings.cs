e57e42239362f65f14ac8d275df0e529afa21557
danwalmsley <dan@walms.co.uk>
2016-02-03 15:12:42 +0000

    working demo of toolchain storing its own settings.

---
+++ AvalonStudio/AvalonStudio.Toolchains.STM32/CompileSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace AvalonStudio.Toolchains.STM32
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CompileSettings
+    {
+        public CompileSettings()
+        {
+            Defines = new List<string>();
+        }
+
+        public List<string> Defines { get; set; }
+    }
+}
