34c51560e947e0be84dea174456c046d420f8dd0
danwalmsley <dan@walms.co.uk>
2016-02-16 11:06:56 +0000

    Added interface for project templates.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Projects/IProjectTemplate.cs
@@ -0,0 +1,20 @@
+﻿namespace AvalonStudio.Extensibility.Projects
+{
+    using AvalonStudio.Projects;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [InheritedExport(typeof(IProjectTemplate))]
+    public interface IProjectTemplate
+    {
+        string Title { get; }
+
+        string Description { get; }
+
+        IProject Generate();        
+    }
+}
