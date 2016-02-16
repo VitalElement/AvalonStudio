2dfafa4b907298ecece5796ea92197e2bbdb80d8
danwalmsley <dan@walms.co.uk>
2016-02-16 23:46:47 +0000

    Added example of adding template file to a project.

--- AvalonStudio/AvalonStudio.Extensibility/Projects/IProjectTemplate.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Projects/IProjectTemplate.cs
@@ -21,6 +21,6 @@
         /// Templates can add 1 or more projects to a solution.
         /// </summary>
         /// <param name="solution">Solution that the template will install to.</param>        
-        void Generate(ISolution solution, string name);        
+        IProject Generate(ISolution solution, string name);        
     }
 }
