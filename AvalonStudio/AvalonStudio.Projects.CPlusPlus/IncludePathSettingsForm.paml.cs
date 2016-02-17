0dcf976d1f8f782eb1198b188f66dc939d094f13
danwalmsley <dan@walms.co.uk>
2016-02-17 17:08:01 +0000

    CPlusPlus projects can now add and remove their own include paths.

---
+++ AvalonStudio/AvalonStudio.Projects.CPlusPlus/IncludePathSettingsForm.paml.cs
@@ -0,0 +1,18 @@
+﻿using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace AvalonStudio.Projects.CPlusPlus
+{
+    public class IncludePathSettingsForm : TabItem
+    {
+        public IncludePathSettingsForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
