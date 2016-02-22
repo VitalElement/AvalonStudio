55c8b79c524847d946c5ba3759c707c8ebe2b8c9
danwalmsley <dan@walms.co.uk>
2016-02-22 22:29:09 +0000

    DocumentLineTransformers can now notify when there data has changed, in order to request a re-draw. This serves as a very fast re-render of syntax highlighting data.

--- AvalonStudio/AvalonStudio.Languages.CPlusPlus/IncludeTextLineTransformer.cs
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/IncludeTextLineTransformer.cs
@@ -1,11 +1,14 @@
 ﻿namespace AvalonStudio.Languages.CPlusPlus.Rendering
 {
+    using System;
     using Perspex;
     using Perspex.Media;
     using TextEditor.Rendering;
     class IncludeTextLineTransformer : IDocumentLineTransformer
     {
         private Brush brush = Brush.Parse("#D69D85");
+
+        public event EventHandler<EventArgs> DataChanged;
 
         public void TransformLine(TextView textView, DrawingContext context, Rect lineBounds, VisualLine line)
         {
