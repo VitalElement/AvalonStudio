b2a3f43b89d12a72c3fd8c4ff0fc693a1f16eb47
danwalmsley <dan@walms.co.uk>
2016-02-27 00:40:19 +0000

    added almost complete implementation of symbol querying with mouse hover.

---
+++ AvalonStudio/AvalonStudio/Controls/Editor/SymbolViewModel.cs
@@ -0,0 +1,275 @@
+﻿namespace AvalonStudio.Controls
+{
+    using AvalonStudio.Languages;
+    using AvalonStudio.MVVM;
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SymbolViewModel : ViewModel<Symbol>
+    {
+        private async void SetCursorDescriptionToFunction()
+        {
+            string arguments = string.Empty;
+
+            //for (int i = 0; i < cursor.ArgumentCount; i++)
+            //{
+            //    var argument = cursor.GetArgument(i);
+
+            //    ArgumentViewModel newArg;
+
+            //    if (IsBuiltInType(argument.CursorType))
+            //    {
+            //        newArg = new ArgumentViewModel() { BuiltInTypeDescription = argument.CursorType.Spelling.Replace(" &", "&").Replace(" *", "*") + " ", Spelling = argument.Spelling };
+            //    }
+            //    else
+            //    {
+            //        newArg = new ArgumentViewModel() { TypeDescription = argument.CursorType.Spelling.Replace(" &", "&").Replace(" *", "*") + " ", Spelling = argument.Spelling };
+            //    }
+
+            //    if (i < cursor.ArgumentCount - 1)
+            //    {
+            //        newArg.Spelling += ", ";
+            //    }
+
+            //    Workspace.This.DispatchUi(() =>
+            //    {
+            //        CursorDescription.AddArgument(newArg);
+            //    });
+            //}
+
+            //if (Arguments.Count == 0)
+            //{
+            //    Workspace.This.DispatchUi(() =>
+            //    {
+            //        AddArgument(new ArgumentViewModel() { BuiltInTypeDescription = "void" });
+            //    });
+            //}
+
+            if (Model.ResultType != null && Model.Name != null)
+            {
+                switch (Model.Kind)
+                {
+                    case CursorKind.Constructor:
+                    case CursorKind.Destructor:
+                        break;
+
+                    default:
+                        if(Model.IsBuiltInType)
+                        {
+                            BuiltInTypeDescription = Model.ResultType + " ";
+                        }
+                        else
+                        {
+                            TypeDescription = Model.ResultType + " ";
+                        }
+                        break;
+                }
+
+                Spelling = Model.Name;
+                Description = Model.BriefComment;
+            }
+        }
+
+        public SymbolViewModel(Symbol model) : base(model)
+        {
+            switch (model.Kind)
+            {
+                case CursorKind.FunctionDeclaration:
+                case CursorKind.CXXMethod:
+                case CursorKind.Constructor:
+                case CursorKind.Destructor:
+                    SetCursorDescriptionToFunction();
+                    break;
+
+                case CursorKind.ClassDeclaration:
+                case CursorKind.CXXThisExpression:
+                    BuiltInTypeDescription = "class ";
+                    TypeDescription = model.TypeDescription;
+                    break;
+
+                case CursorKind.TypedefDeclaration:
+                    BuiltInTypeDescription = "typedef ";
+                    TypeDescription = model.TypeDescription;
+                    break;
+
+                case CursorKind.EnumDeclaration:
+                    BuiltInTypeDescription = "enum ";
+                    EnumTypeDescription = model.TypeDescription;
+                    break;
+
+                case CursorKind.StructDeclaration:
+                    BuiltInTypeDescription = "struct ";
+                    TypeDescription = model.TypeDescription;
+                    break;
+
+                case CursorKind.EnumConstantDeclaration:
+                    EnumTypeDescription = model.TypeDescription;
+                    Spelling = model.Name + " = ";
+                    EnumValue = model.EnumDescription;
+                    break;
+
+                case CursorKind.ClassTemplate:
+                case CursorKind.TemplateReference:
+                    BuiltInTypeDescription = "class ";
+                    TypeDescription = model.Definition;
+                    break;
+
+                case CursorKind.VarDeclaration:
+                    switch (model.Linkage)
+                    {
+                        case LinkageKind.NoLinkage:
+                            ScopeDescription = "(local variable) ";
+                            break;
+
+                        case LinkageKind.Internal:
+                            ScopeDescription = "(static variable) ";
+                            break;
+
+                        case LinkageKind.External:
+                            ScopeDescription = "(global variable) ";
+                            break;
+                    }
+
+                    if (model.IsBuiltInType)
+                    {
+                        BuiltInTypeDescription = model.SymbolType;
+                    }
+                    else
+                    {
+                        TypeDescription = model.SymbolType;
+                    }
+
+                    Spelling = model.Name;
+                    break;
+
+                case CursorKind.FieldDeclaration:
+                    ScopeDescription = "(field) ";
+
+                    if (model.IsBuiltInType)
+                    {
+                        BuiltInTypeDescription = model.SymbolType;
+                    }
+                    else
+                    {
+                        TypeDescription = model.SymbolType;
+                    }
+
+                    Spelling = model.Name;
+                    break;
+
+                case CursorKind.NonTypeTemplateParameter:
+                case CursorKind.ParmDeclaration:
+                    ScopeDescription = "(parameter) ";
+
+                    if (model.IsBuiltInType)
+                    {
+                        BuiltInTypeDescription = model.SymbolType;
+                    }
+                    else
+                    {
+                        TypeDescription = model.SymbolType;
+                    }
+
+                    Spelling = model.Name;
+                    break;
+
+
+
+                default:
+                   // dontShow = true;
+                    //DebugData = cursor.Kind.ToString().Replace(" &", "&").Replace(" *", "*");
+                    break;
+            }
+        }
+
+        private string scopeDescription;
+        public string ScopeDescription
+        {
+            get { return scopeDescription; }
+            set { this.RaiseAndSetIfChanged(ref scopeDescription, value); }
+        }
+
+        private string builtInTypeDescription;
+        public string BuiltInTypeDescription
+        {
+            get { return builtInTypeDescription; }
+            set { this.RaiseAndSetIfChanged(ref builtInTypeDescription, value); }
+        }
+
+        private string enumTypeDescription;
+        public string EnumTypeDescription
+        {
+            get { return enumTypeDescription; }
+            set { this.RaiseAndSetIfChanged(ref enumTypeDescription, value); }
+        }
+
+        private string enumValue;
+        public string EnumValue
+        {
+            get { return enumValue; }
+            set { this.RaiseAndSetIfChanged(ref enumValue, value); }
+        }
+
+        private string typeDescription;
+        public string TypeDescription
+        {
+            get { return typeDescription; }
+            set { this.RaiseAndSetIfChanged(ref typeDescription, value); }
+        }
+
+        private string classDescription;
+        public string ClassDescription
+        {
+            get { return classDescription; }
+            set { this.RaiseAndSetIfChanged(ref classDescription, value); }
+        }
+
+        private string spelling;
+        public string Spelling
+        {
+            get { return spelling; }
+            set { this.RaiseAndSetIfChanged(ref spelling, value); }
+        }
+
+        //private ObservableCollection<ArgumentViewModel> arguments;
+        //public ObservableCollection<ArgumentViewModel> Arguments
+        //{
+        //    get { return arguments; }
+        //    set { arguments = value; OnPropertyChanged(); }
+        //}
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { this.RaiseAndSetIfChanged(ref description, value); this.RaisePropertyChanged(() => DescriptionVisibility); }
+        }
+
+        private bool isVisible;
+        public bool IsVisible
+        {
+            get { return isVisible; }
+            set { this.RaiseAndSetIfChanged(ref isVisible, value); }
+        }
+
+        public bool DescriptionVisibility
+        {
+            get
+            {
+                return (!string.IsNullOrEmpty(description));                
+            }
+        }
+
+        //public bool ArgumentsVisibility
+        //{
+        //    get
+        //    {
+        //        return Arguments.Count != 0);
+        //    }
+        //}
+    }
+}
