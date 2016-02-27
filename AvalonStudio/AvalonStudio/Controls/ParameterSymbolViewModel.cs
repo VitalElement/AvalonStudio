0db9f977903e9ad398829bc154d861a7159033d7
danwalmsley <dan@walms.co.uk>
2016-02-27 12:44:42 +0000

    Hovering over code to retrieve symbol information now fully implemented.

---
+++ AvalonStudio/AvalonStudio/Controls/ParameterSymbolViewModel.cs
@@ -0,0 +1,39 @@
+﻿using AvalonStudio.Languages;
+using AvalonStudio.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Controls
+{
+    public class ParameterSymbolViewModel : ViewModel<ParameterSymbol>
+    {
+        public ParameterSymbolViewModel(ParameterSymbol model) : base(model)
+        {
+            if(model.IsBuiltInType)
+            {
+                BuiltInTypeDescription = model.TypeDescription + " ";
+            }
+            else
+            {
+                TypeDescription = model.TypeDescription + " ";
+            }            
+        }
+
+        public string Name
+        {
+            get { return Model.Name; }
+        }
+
+        public string TypeDescription { get; set; }
+
+        public string BuiltInTypeDescription { get; set; }
+
+        public string Comment
+        {
+            get { return Model.Comment; }
+        }
+    }
+}
