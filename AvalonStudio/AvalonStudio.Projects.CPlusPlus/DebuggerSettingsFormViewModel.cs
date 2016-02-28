bdd8e574d82b32f11b35f2f3a92698eb4eca48f0
danwalmsley <dan@walms.co.uk>
2016-02-28 17:18:09 +0000

    Implemented ability to select debugger, and provision debuggers settings onto project file. (Debug settings will later be moved to user file.)

---
+++ AvalonStudio/AvalonStudio.Projects.CPlusPlus/DebuggerSettingsFormViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace AvalonStudio.Projects.CPlusPlus
+{
+    using AvalonStudio.Debugging;
+    using AvalonStudio.MVVM;
+    using Extensibility;
+    using ReactiveUI;
+    using System.Collections.Generic;
+
+    public class DebuggerSettingsFormViewModel : ViewModel<CPlusPlusProject>
+    {
+        public DebuggerSettingsFormViewModel(CPlusPlusProject project) : base(project)
+        {
+            debuggers = new List<IDebugger>(Workspace.Instance.Debuggers);
+            selectedDebugger = project.Debugger;
+        }
+
+        public void Save()
+        {
+            Model.DebuggerReference = selectedDebugger?.GetType().ToString();
+            selectedDebugger.ProvisionSettings(Model);
+            Model.Save();
+        }
+
+        private List<IDebugger> debuggers;
+        public List<IDebugger> Debuggers
+        {
+            get { return debuggers; }
+            set { this.RaiseAndSetIfChanged(ref debuggers, value); }
+        }
+
+        private IDebugger selectedDebugger;
+        public IDebugger SelectedDebugger
+        {
+            get { return selectedDebugger; }
+            set { this.RaiseAndSetIfChanged(ref selectedDebugger, value); Save(); }
+        }
+    }
+}
