395609ce7d51ef5c8cd558f2483f6c782f13fc7a
danwalmsley <dan@walms.co.uk>
2016-02-28 22:53:24 +0000

    Attempt at adding toolchain compiler and linker settings under toolchain... datacontext getting overridden by Perspex.

---
+++ AvalonStudio/AvalonStudio.Toolchains.STM32/ToolchainSettingsForm.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace AvalonStudio.Toolchains.STM32
+{
+    public class ToolchainSettingsForm : UserControl
+    {
+        public ToolchainSettingsForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
