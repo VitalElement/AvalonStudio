1f64066896bece51d636b9d7bbc23984a7a2244a
danwalmsley <dan@walms.co.uk>
2016-03-10 16:43:10 +0000

    Initial implementation of test enumeration.

--- AvalonStudio/AvalonStudioBuild/Options.cs
+++ AvalonStudio/AvalonStudioBuild/Options.cs
@@ -17,8 +17,14 @@
         [Value(0, MetaName = "Solution", HelpText = "Solution file (asln)")]
         public string Solution { get; set; }
 
-        [Value(0, MetaName = "Project", HelpText = "Name of project to run command on")]
+        [Value(1, MetaName = "Project", HelpText = "Name of project to run command on")]
         public string Project { get; set; }
+    }
+
+    [Verb ("test")]
+    class TestOptions : ProjectOption
+    {
+
     }
 
     [Verb("build", HelpText = "Builds the project in the current directory.")]
