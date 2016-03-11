fd61375cf54ab280caf32702a2e8cdd153109122
danwalmsley <dan@walms.co.uk>
2016-03-11 19:44:33 +0000

    Implemented basic registers view.

---
+++ AvalonStudio/AvalonStudio/Controls/Registers.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace AvalonStudio.Controls
+{
+    public class Registers : UserControl
+    {
+        public Registers()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
