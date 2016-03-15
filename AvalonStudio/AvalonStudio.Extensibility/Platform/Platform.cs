c476be4177cac2e52970f2c3b513548b76514861
danwalmsley <dan@walms.co.uk>
2016-03-15 11:26:17 +0000

    Added unix helper functions for sending Ctrl + C to process. Untested.

--- AvalonStudio/AvalonStudio.Extensibility/Platform/Platform.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Platform/Platform.cs
@@ -3,6 +3,7 @@
     using AvalonStudio.Utils;
     using System;
     using System.IO;
+    using System.Runtime.InteropServices;
     using Utils;
 
     public static class Platform
@@ -28,7 +29,84 @@
             {
                 Directory.CreateDirectory(RepoCatalogDirectory);
             }
-        }       
+        }
+        
+        [Map]
+        public enum Signum : int
+        {
+            SIGHUP = 1, // Hangup (POSIX).
+            SIGINT = 2, // Interrupt (ANSI).
+            SIGQUIT = 3, // Quit (POSIX).
+            SIGILL = 4, // Illegal instruction (ANSI).
+            SIGTRAP = 5, // Trace trap (POSIX).
+            SIGABRT = 6, // Abort (ANSI).
+            SIGIOT = 6, // IOT trap (4.2 BSD).
+            SIGBUS = 7, // BUS error (4.2 BSD).
+            SIGFPE = 8, // Floating-point exception (ANSI).
+            SIGKILL = 9, // Kill, unblockable (POSIX).
+            SIGUSR1 = 10, // User-defined signal 1 (POSIX).
+            SIGSEGV = 11, // Segmentation violation (ANSI).
+            SIGUSR2 = 12, // User-defined signal 2 (POSIX).
+            SIGPIPE = 13, // Broken pipe (POSIX).
+            SIGALRM = 14, // Alarm clock (POSIX).
+            SIGTERM = 15, // Termination (ANSI).
+            SIGSTKFLT = 16, // Stack fault.
+            SIGCLD = SIGCHLD, // Same as SIGCHLD (System V).
+            SIGCHLD = 17, // Child status has changed (POSIX).
+            SIGCONT = 18, // Continue (POSIX).
+            SIGSTOP = 19, // Stop, unblockable (POSIX).
+            SIGTSTP = 20, // Keyboard stop (POSIX).
+            SIGTTIN = 21, // Background read from tty (POSIX).
+            SIGTTOU = 22, // Background write to tty (POSIX).
+            SIGURG = 23, // Urgent condition on socket (4.2 BSD).
+            SIGXCPU = 24, // CPU limit exceeded (4.2 BSD).
+            SIGXFSZ = 25, // File size limit exceeded (4.2 BSD).
+            SIGVTALRM = 26, // Virtual alarm clock (4.2 BSD).
+            SIGPROF = 27, // Profiling alarm clock (4.2 BSD).
+            SIGWINCH = 28, // Window size change (4.3 BSD, Sun).
+            SIGPOLL = SIGIO, // Pollable event occurred (System V).
+            SIGIO = 29, // I/O now possible (4.2 BSD).
+            SIGPWR = 30, // Power failure restart (System V).
+            SIGSYS = 31, // Bad system call.
+            SIGUNUSED = 31
+        }
+
+        internal const string LIBC = "libc";
+        private const string LIB = "MonoPosixHelper";
+
+        [DllImport(LIB, EntryPoint = "Mono_Posix_FromSignum")] 
+ 		private static extern int FromSignum(Signum value, out Int32 rval); 
+ 
+ 
+ 		public static bool TryFromSignum(Signum value, out Int32 rval)
+ 		{ 
+ 			return FromSignum(value, out rval) == 0; 
+ 		}
+
+
+        public static Int32 FromSignum(Signum value)
+        {
+            Int32 rval;
+            if (FromSignum(value, out rval) == -1)
+                throw new ArgumentException();
+            return rval;
+        }
+
+
+        [DllImport(LIBC, SetLastError = true, EntryPoint = "kill")] 
+ 		private static extern int sys_kill(int pid, int sig); 
+ 
+ 		private static int kill(int pid, Signum sig)
+ 		{ 
+ 			int _sig = FromSignum(sig); 
+ 			return sys_kill(pid, _sig); 
+		}
+
+
+        public static int SendSignal(int pid, Signum sig)
+        {
+            return kill(pid, sig);
+        }
         
         public static string ToAvalonPath (this string path)
         {
