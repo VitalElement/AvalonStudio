ff8b6caaa103ac223c1ca4d4ebcd3e791641362b
danwalmsley <dan@walms.co.uk>
2016-03-21 19:29:04 +0000

    begining to add behaviours to add interactivity in Xaml.

---
+++ AvalonStudio/AvalonStudio.TextEditor/Behaviours/PointerHoverBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Perspex.Controls;
+using Perspex.Input;
+using Perspex.Xaml.Interactivity;
+
+namespace AvalonStudio.Behaviors
+{
+    public class FocusOnPointerMovedBehavior : Behavior<Control>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PointerMoved += PointerMoved;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PointerMoved -= PointerMoved;
+        }
+
+        private void PointerMoved(object sender, PointerEventArgs args)
+        {
+            AssociatedObject.Focus();
+        }
+    }
+}
