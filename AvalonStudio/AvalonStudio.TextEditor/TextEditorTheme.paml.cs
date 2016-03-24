e48c83114f6d486f4ea8d7355973a3c60f1dd2c7
Dan Walmsley <dan@walms.co.uk>
2016-03-24 17:39:56 +0000

    removed debug code.

--- AvalonStudio/AvalonStudio.TextEditor/TextEditorTheme.paml.cs
+++ AvalonStudio/AvalonStudio.TextEditor/TextEditorTheme.paml.cs
@@ -8,13 +8,6 @@
     {
         public TextEditorTheme()
         {
-            var res = System.Reflection.Assembly.GetAssembly(typeof(TextEditorTheme)).GetManifestResourceNames();
-
-            foreach (var resource in res)
-            {
-                Console.WriteLine(resource);
-            }
-
             PerspexXamlLoader.Load(this);
         }
     }
