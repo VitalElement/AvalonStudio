5e806fd5a2f34536a45bc1a2b781c0ca1e6f881a
danwalmsley <dan@walms.co.uk>
2016-04-01 16:56:34 +0100

    Toolbar debugging buttons now hooked up.

--- AvalonStudio/AvalonStudio/Controls/ToolBarViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/ToolBarViewModel.cs
@@ -1,8 +1,65 @@
 ﻿namespace AvalonStudio.Controls
 {
+    using MVVM;
+    using System;
     using ReactiveUI;
 
-    public class ToolBarViewModel : ReactiveObject
+    public class ToolBarViewModel : ViewModel
     {
+        public ToolBarViewModel()
+        {
+            StartDebugCommand = ReactiveCommand.Create();
+            StartDebugCommand.Subscribe(_ =>
+            {
+                if (WorkspaceViewModel.Instance.CurrentPerspective == Perspective.Editor)
+                {
+                    if (WorkspaceViewModel.Instance.SolutionExplorer.Model?.StartupProject != null)
+                    {
+                        WorkspaceViewModel.Instance.DebugManager.StartDebug(WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject);
+                    }
+                }
+                else
+                {
+                    WorkspaceViewModel.Instance.DebugManager.Continue();
+                }
+            });
+
+            StepIntoCommand = ReactiveCommand.Create();
+            StepIntoCommand.Subscribe(_ =>
+            {
+                WorkspaceViewModel.Instance.DebugManager.StepInto();
+            });
+
+            StepOverCommand = ReactiveCommand.Create();
+            StepOverCommand.Subscribe(_ =>
+            {
+                WorkspaceViewModel.Instance.DebugManager.StepOver();
+            });
+
+            PauseCommand = ReactiveCommand.Create();
+            PauseCommand.Subscribe(_ =>
+            {
+                WorkspaceViewModel.Instance.DebugManager.Pause();
+            });
+
+            StopCommand = ReactiveCommand.Create();
+            StopCommand.Subscribe(_ =>
+            {
+                WorkspaceViewModel.Instance.DebugManager.Stop();
+            });
+
+            RestartCommand = ReactiveCommand.Create();
+            RestartCommand.Subscribe(_ =>
+            {
+                WorkspaceViewModel.Instance.DebugManager.Restart();
+            });
+        }
+
+        public ReactiveCommand<object> StartDebugCommand { get; }
+        public ReactiveCommand<object> StepOverCommand { get; }
+        public ReactiveCommand<object> StepIntoCommand { get; }
+        public ReactiveCommand<object> PauseCommand { get; }
+        public ReactiveCommand<object> StopCommand { get; }
+        public ReactiveCommand<object> RestartCommand { get; }
     }
 }
