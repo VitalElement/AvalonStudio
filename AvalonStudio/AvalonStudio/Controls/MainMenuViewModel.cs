37763951dce6e106c4feeaad7fe2e44f8f6a202a
Dan Walmsley <dan@walms.co.uk>
2016-04-16 20:08:18 +0100

    moved command body code out into appropriate methods in Workspace model.

--- AvalonStudio/AvalonStudio/Controls/MainMenuViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/MainMenuViewModel.cs
@@ -1,17 +1,7 @@
 ﻿namespace AvalonStudio.Controls
 {
-    using Platform;
-    using Perspex.Controls;
-    using Perspex.Threading;
-    using Projects;
     using ReactiveUI;
     using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using ViewModels;
 
     public class MainMenuViewModel : ReactiveObject
     {
@@ -19,19 +9,9 @@
         {
             LoadProjectCommand = ReactiveCommand.Create();
 
-            LoadProjectCommand.Subscribe(async _ =>
+            LoadProjectCommand.Subscribe(_ =>
             {
-                var dlg = new OpenFileDialog();
-                dlg.Title = "Open Solution";
-                dlg.Filters.Add(new FileDialogFilter { Name = "AvalonStudio Solution", Extensions = new List<string> { Solution.Extension } });
-                dlg.InitialFileName = string.Empty;
-                dlg.InitialDirectory = Platform.ProjectDirectory;
-                var result = await dlg.ShowAsync();
-
-                if (result != null)
-                {
-                    WorkspaceViewModel.Instance.SolutionExplorer.Model = Solution.Load(result[0]);
-                }
+                WorkspaceViewModel.Instance.LoadSolution();
             });
 
             SaveCommand = ReactiveCommand.Create();
@@ -49,104 +29,43 @@
             CleanProjectCommand = ReactiveCommand.Create();
             CleanProjectCommand.Subscribe(_ =>
             {
-                new Thread(new ThreadStart(new Action(async () =>
-                {
-                    if (WorkspaceViewModel.Instance.SolutionExplorer.Model != null)
-                    {
-                        if (WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject != null)
-                        {
-                            if (WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject.ToolChain != null)
-                            {
-                                await WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject.ToolChain.Clean(WorkspaceViewModel.Instance.Console, WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject);
-                            }
-                            else
-                            {
-                                WorkspaceViewModel.Instance.Console.WriteLine("No toolchain selected for project.");
-                            }
-                        }
-                        else
-                        {
-                            WorkspaceViewModel.Instance.Console.WriteLine("No Startup Project defined.");
-                        }
-                    }
-                    else
-                    {
-                        WorkspaceViewModel.Instance.Console.WriteLine("No project loaded.");
-                    }
-                }))).Start();
+                WorkspaceViewModel.Instance.Clean();
             });
 
             BuildProjectCommand = ReactiveCommand.Create();
             BuildProjectCommand.Subscribe(_ =>
             {
-                new Thread(new ThreadStart(new Action(async () =>
-                {
-                    if (WorkspaceViewModel.Instance.SolutionExplorer.Model != null)
-                    {
-                        if (WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject != null)
-                        {
-                            if (WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject.ToolChain != null)
-                            {
-                                await Task.Factory.StartNew(() => WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject.ToolChain.Build(WorkspaceViewModel.Instance.Console, WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject));
-                            }
-                            else
-                            {
-                                WorkspaceViewModel.Instance.Console.WriteLine("No toolchain selected for project.");
-                            }
-                        }
-                        else
-                        {
-                            WorkspaceViewModel.Instance.Console.WriteLine("No Startup Project defined.");
-                        }
-                    }
-                    else
-                    {
-                        WorkspaceViewModel.Instance.Console.WriteLine("No project loaded.");
-                    }
-                }))).Start();
+                WorkspaceViewModel.Instance.Build();
             });
 
             PackagesCommand = ReactiveCommand.Create();
             PackagesCommand.Subscribe((o) =>
             {
-                WorkspaceViewModel.Instance.ModalDialog = new PackageManagerDialogViewModel();
-                WorkspaceViewModel.Instance.ModalDialog.ShowDialog();
+                WorkspaceViewModel.Instance.ShowPackagesDialog();
             });
 
             ProjectPropertiesCommand = ReactiveCommand.Create();
             ProjectPropertiesCommand.Subscribe((o) =>
             {
-                WorkspaceViewModel.Instance.ModalDialog = new ProjectConfigurationDialogViewModel(WorkspaceViewModel.Instance.SolutionExplorer.SelectedProject, () => { });
-                WorkspaceViewModel.Instance.ModalDialog.ShowDialog();
+                WorkspaceViewModel.Instance.ShowProjectPropertiesDialog();
             });
 
             NewProjectCommand = ReactiveCommand.Create();
             NewProjectCommand.Subscribe((o) =>
             {
-                WorkspaceViewModel.Instance.ModalDialog = new NewProjectDialogViewModel();
-                WorkspaceViewModel.Instance.ModalDialog.ShowDialog();
+                WorkspaceViewModel.Instance.ShowNewProjectDialog();
             });
 
             ExitCommand = ReactiveCommand.Create();
             ExitCommand.Subscribe((o) =>
             {
-                Environment.Exit(1);
+                WorkspaceViewModel.Instance.ExitApplication();
             });
 
             StartDebugCommand = ReactiveCommand.Create();
             StartDebugCommand.Subscribe(_ =>
             {
-                if (WorkspaceViewModel.Instance.CurrentPerspective == Perspective.Editor)
-                {
-                    if (WorkspaceViewModel.Instance.SolutionExplorer.Model?.StartupProject != null)
-                    {
-                        WorkspaceViewModel.Instance.DebugManager.StartDebug(WorkspaceViewModel.Instance.SolutionExplorer.Model.StartupProject);
-                    }
-                }
-                else
-                {
-                    WorkspaceViewModel.Instance.DebugManager.Continue();
-                }
+                WorkspaceViewModel.Instance.StartDebugSession();
             });
 
             StepIntoCommand = ReactiveCommand.Create();
