b3c441871b563b82e5c23bcbe63b8edf6a64676d
Dan Walmsley <dan@walms.co.uk>
2016-04-17 00:59:09 +0100

    ported across from VEStudio models and Views to work with debug manager.

--- AvalonStudio/AvalonStudio/WatchListViewModel.cs
+++ AvalonStudio/AvalonStudio/WatchListViewModel.cs
@@ -1,19 +1,90 @@
-﻿namespace AvalonStudio
+﻿namespace AvalonStudio.Controls.ViewModels
 {
     using AvalonStudio.MVVM;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using ReactiveUI;
     using Debugging;
+    using ReactiveUI;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
 
     public class WatchListViewModel : ViewModel
     {
-       public void Invalidate (IList<VariableObjectChange> changes)
+        public WatchListViewModel()
         {
+            Watches = new ObservableCollection<WatchViewModel>();
+        }
 
+        private int watchId = 0;
+
+        private ObservableCollection<WatchViewModel> watches;
+        public ObservableCollection<WatchViewModel> Watches
+        {
+            get { return watches; }
+            set { this.RaiseAndSetIfChanged(ref watches, value); }
+        }
+
+        private IDebugger debugger;
+        public IDebugger Debugger { get; }
+
+        public void SetDebugger(IDebugger debugger)
+        {
+            this.debugger = debugger;
+        }
+
+        public void AddWatch(string expression)
+        {
+            var newWatch = debugger.CreateWatch(string.Format("var{0}", watchId), expression);
+
+            if (newWatch != null)
+            {
+                watchId++;
+
+                this.Add(newWatch);
+            }
+        }
+
+        public void RemoveWatch(WatchViewModel watch)
+        {
+            this.Watches.Remove(watch);
+
+            debugger.DeleteWatch(watch.Model.Id);
+        }
+
+        public void Add(VariableObject model)
+        {
+            var newWatch = new WatchViewModel(this, model);
+
+            newWatch.Evaluate(debugger);
+
+            this.Watches.Add(newWatch);
+
+            //InvalidateColumnWidths();
+        }
+
+        private void ApplyChange(VariableObjectChange change)
+        {
+            foreach (var watch in Watches)
+            {
+                if (watch.ApplyChange(change))
+                {
+                    break;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            Watches.Clear();
+        }
+
+        public void Invalidate(List<VariableObjectChange> updates)
+        {
+            if (updates != null)
+            {
+                foreach (var update in updates)
+                {
+                    ApplyChange(update);
+                }
+            }
         }
     }
 }
