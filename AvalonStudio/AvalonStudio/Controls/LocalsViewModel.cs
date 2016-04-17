fb46c546d8f133e763cac99cdd7a42d01a02229b
Dan Walmsley <dan@walms.co.uk>
2016-04-17 02:15:41 +0100

    Locals now actually works and is based on watch view model. Locals is implemented so display same data as Watch View.

--- AvalonStudio/AvalonStudio/Controls/LocalsViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/LocalsViewModel.cs
@@ -3,16 +3,61 @@
     using AvalonStudio.Debugging;
     using AvalonStudio.MVVM;
     using System.Collections.Generic;
+    using System.Linq;
+    using ViewModels;
 
-    public class LocalsViewModel : ViewModel<List<Variable>>
+    public class LocalsViewModel : WatchListViewModel
     {
-        public void Clear()
+        private List<Variable> locals;
+
+        public void InvalidateLocals(List<Variable> variables)
         {
-            this.Model = null;
+            var updated = new List<Variable>();
+            var removed = new List<Variable>();
+
+            for(int i = 0; i < locals.Count; i++)
+            {
+                var local = locals[i];
+
+                var currentVar = variables.FirstOrDefault(v => v.Name == local.Name);
+
+                if (currentVar == null)
+                {
+                    removed.Add(local);                    
+                }
+                else
+                {
+                    updated.Add(local);
+                }
+            }
+
+            foreach(var variable in variables)
+            {
+                var currentVar = updated.FirstOrDefault(v => v.Name == variable.Name);
+
+                if(currentVar == null)
+                {
+                    locals.Add(variable);
+                    AddWatch(variable.Name);
+                }
+            }
+
+            foreach(var removedvar in  removed)
+            {
+                locals.Remove(removedvar);
+                RemoveWatch(Watches.FirstOrDefault(w => w.Name == removedvar.Name));
+            }
         }
 
-        public LocalsViewModel() : base(new List<Variable>())
+        public override void Clear()
         {
+            locals.Clear();
+            base.Clear();
         }
+
+        public LocalsViewModel()
+        {
+            locals = new List<Variable>();
+        }        
     }
 }
