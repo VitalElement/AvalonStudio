f591e15a7384362fbba5c986641a6c1c685fb635
Dan Walmsley <dan@walms.co.uk>
2016-04-20 21:40:10 +0100

    added messages indeconstructors and restored margins.

--- AvalonStudio/AvalonStudio/Controls/Editor/EditorModel.cs
+++ AvalonStudio/AvalonStudio/Controls/Editor/EditorModel.cs
@@ -43,7 +43,7 @@
 
         ~EditorModel()
         {
-            ShutdownBackgroundWorkers();
+            System.Console.WriteLine(("Editor Model Destructed."));
         }
 
         private bool completionRequested = false;
