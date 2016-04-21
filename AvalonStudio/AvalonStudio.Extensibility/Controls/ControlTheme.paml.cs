16427d62c8f11f7176e5bd10593e8822d3d6c7a2
Dan Walmsley <dan@walms.co.uk>
2016-04-21 20:04:09 +0100

    Added beginings of ToolControl that dockable widgets will be based on.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Controls/ControlTheme.paml.cs
@@ -0,0 +1,13 @@
+﻿namespace AvalonStudio.Controls
+{
+    using Perspex;
+    using Perspex.Styling;
+
+    public class ControlTheme : Styles
+    {
+        public ControlTheme()
+        {
+            this.LoadFromXaml();
+        }
+    }
+}
