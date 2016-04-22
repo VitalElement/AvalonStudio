c6b0167235c2b4e576fd760a2e2f245bc52acffa
danwalmsley <dan@walms.co.uk>
2016-04-22 11:04:06 +0100

    added basic toolcontrol and migrated existing widgets.

---
+++ AvalonStudio/AvalonStudio/Controls/ToolControl.cs
@@ -0,0 +1,22 @@
+﻿namespace AvalonStudio.Controls
+{
+    using Perspex;
+    using Perspex.Controls;
+
+    public class ToolControl : ContentControl
+    {
+        public ToolControl()
+        {            
+            Styles.Add(new ControlTheme());
+        }
+
+        public static readonly PerspexProperty<string> TitleProprty =
+            PerspexProperty.Register<ToolControl, string>(nameof(Title));
+
+        public string Title
+        {
+            get { return GetValue(TitleProprty); }
+            set { SetValue(TitleProprty, value); }
+        }
+    }
+}
