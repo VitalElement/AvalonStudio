dbb3ccac5c1b8682b2e11ebb681bdef109c15e6c
danwalmsley <dan@walms.co.uk>
2016-04-22 13:25:27 +0100

    Attempted to add seperate colours for temporary tabs, requires help from @grokys with styling system,

--- AvalonStudio/AvalonStudio/Controls/DocumentTabsViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/DocumentTabsViewModel.cs
@@ -1,20 +1,27 @@
 ﻿namespace AvalonStudio.Controls
 {
     using AvalonStudio.MVVM;
-    using System;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using Perspex.Media;
     using ReactiveUI;
+    using System.Collections.ObjectModel;
 
     public class DocumentTabsViewModel : ViewModel
     {
         public DocumentTabsViewModel()
         {
             Documents = new ObservableCollection<EditorViewModel>();
-        }
+            tabBrush = Brush.Parse("#007ACC");
+            tabHighlightBrush = Brush.Parse("#1c97ea");
+
+            temporaryTabBrush = Brush.Parse("#68217A");
+            temporaryTabHighlighBrush = Brush.Parse("#B064AB");            
+        } 
+
+        private IBrush temporaryTabHighlighBrush;
+        private IBrush temporaryTabBrush;
+        private IBrush tabHighlightBrush;
+        private IBrush tabBrush;
+        private IBrush backgroundBrush;
 
         private ObservableCollection<EditorViewModel> documents;
         public ObservableCollection<EditorViewModel> Documents
@@ -27,7 +34,22 @@
         public EditorViewModel SelectedDocument
         {
             get { return selectedDocument; }
-            set { this.RaiseAndSetIfChanged(ref selectedDocument, value); value?.Model.Editor?.Focus(); }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref selectedDocument, value);
+                value?.Model.Editor?.Focus();
+
+                if(value == TemporaryDocument)
+                {
+                    TabBackgroundBrush = temporaryTabBrush;
+                    HoverTabBackgroundBrush = temporaryTabHighlighBrush;
+                }
+                else
+                {
+                    TabBackgroundBrush = tabBackgroundBrush;
+                    HoverTabBackgroundBrush = tabHighlightBrush;
+                }
+            }
         }
 
         private EditorViewModel temporaryDocument;
@@ -37,5 +59,18 @@
             set { temporaryDocument = value; }
         }
 
+        private IBrush tabBackgroundBrush;
+        public IBrush TabBackgroundBrush
+        {
+            get { return tabBackgroundBrush; }
+            set { this.RaiseAndSetIfChanged(ref tabBackgroundBrush, value); }
+        }
+
+        private IBrush hoverTabBackgroundBrush;
+        public IBrush HoverTabBackgroundBrush
+        {
+            get { return hoverTabBackgroundBrush; }
+            set { this.RaiseAndSetIfChanged(ref hoverTabBackgroundBrush, value); }
+        }
     }
 }
