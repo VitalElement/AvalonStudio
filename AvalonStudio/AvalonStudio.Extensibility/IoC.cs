aa592190a4db2a1124937d1585adbce70dd69e3c
danwalmsley <dan@walms.co.uk>
2016-04-25 11:51:34 +0100

    Added IoC container interface as a method of getting intances or shell, console, etc.

---
+++ AvalonStudio/AvalonStudio.Extensibility/IoC.cs
@@ -0,0 +1,22 @@
+﻿using Splat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Extensibility
+{
+    public static class IoC
+    {
+        public static object Get(Type t, string contract = null)
+        {
+            return Locator.CurrentMutable.GetService(t, contract);
+        }
+
+        public static T Get<T>(string contract = null)
+        {
+            return (T)Get(typeof(T), contract);
+        }
+    }
+}
