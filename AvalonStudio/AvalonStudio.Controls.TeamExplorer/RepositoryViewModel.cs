662b6b7a7712a503940a5014c1ff2402b566a16e
Dan Walmsley <dan@walms.co.uk>
2016-04-26 22:07:22 +0100

    Initial TeamExplorerCommit.

---
+++ AvalonStudio/AvalonStudio.Controls.TeamExplorer/RepositoryViewModel.cs
@@ -0,0 +1,54 @@
+﻿namespace AvalonStudio.Controls.TeamExplorer
+{
+    using LibGit2Sharp;
+    using MVVM;
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RepositoryViewModel : ViewModel<Repository>
+    {
+        public RepositoryViewModel(Repository model) : base(model)
+        {
+            name = Path.GetFileName(model.Info.Path.Replace("\\.git\\",string.Empty));
+
+            branches = new ObservableCollection<string>();
+            foreach(var branch in model.Branches)
+            {
+                branches.Add(branch.FriendlyName);
+
+                if(branch.IsCurrentRepositoryHead)
+                {
+                    selectedBranch = branch.FriendlyName;
+                }
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.RaiseAndSetIfChanged(ref name, value); }
+        }
+
+        private ObservableCollection<string> branches;
+        public ObservableCollection<string> Branches
+        {
+            get { return branches; }
+            set { this.RaiseAndSetIfChanged(ref branches, value); }
+        }
+
+        private string selectedBranch;
+        public string SelectedBranch
+        {
+            get { return selectedBranch; }
+            set { this.RaiseAndSetIfChanged(ref selectedBranch, value); }
+        }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.Controls.TeamExplorer/TeamExplorerView.paml.cs
@@ -0,0 +1,18 @@
+﻿namespace AvalonStudio.Controls.TeamExplorer
+{
+    using Perspex.Controls;
+    using Perspex;
+    
+    public class TeamExplorerView : UserControl
+    {
+        public TeamExplorerView()
+        {
+            this.InitializeComponent();            
+        }        
+
+        private void InitializeComponent()
+        {
+            this.LoadFromXaml();
+        }
+    }
+}
