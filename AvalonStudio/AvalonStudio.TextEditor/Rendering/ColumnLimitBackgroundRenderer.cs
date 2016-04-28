f10c10889d48f965d8f271b9c5fd2c5bb51a1337
danwalmsley <dan@walms.co.uk>
2016-04-28 14:47:21 +0100

    moved to 120 char column limit.

--- AvalonStudio/AvalonStudio.TextEditor/Rendering/ColumnLimitBackgroundRenderer.cs
+++ AvalonStudio/AvalonStudio.TextEditor/Rendering/ColumnLimitBackgroundRenderer.cs
@@ -11,7 +11,7 @@
 
         public void Draw(TextView textView, DrawingContext drawingContext)
         {
-            double xPos = textView.TextSurfaceBounds.X + textView.CharSize.Width * 80.0;
+            double xPos = textView.TextSurfaceBounds.X + textView.CharSize.Width * 120.0;
 
             drawingContext.DrawLine(new Pen(brush, 1), new Perspex.Point(xPos, 0), new Perspex.Point(xPos, textView.Bounds.Bottom));
         }
