fa9e45e3ad1522c5a9333c2273ab731c84edbdd8
James Walmsley <james@fullfat-fs.co.uk>
2016-04-29 09:18:10 +0100

    [Controls.Standard] Added AboutScreen

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/AboutScreen/Commands/AboutScreenCommandDefinition.cs
@@ -0,0 +1,58 @@
+﻿namespace AvalonStudio.Controls.Standard.AboutScreen.Commands
+{
+    using AvalonStudio.Extensibility.Commands;
+    using Extensibility;
+    using ReactiveUI;
+    using Shell;
+    using System;
+    using System.Windows.Input;
+
+    [CommandDefinition]
+    class AboutScreenCommandDefinition : CommandDefinition
+    {
+        public AboutScreenCommandDefinition()
+        {
+            command = ReactiveCommand.Create();
+            command.Subscribe(_ =>
+            {
+                IShell shell = IoC.Get<IShell>();
+                shell.ModalDialog = new AboutDialogViewModel();
+                shell.ModalDialog.ShowDialog();
+            });
+        }
+
+        private ReactiveCommand<object> command;
+
+        public override ICommand Command
+        {
+            get
+            {
+                return command;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Help.About";
+            }
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return "About";
+            }
+        }
+
+        public override string ToolTip
+        {
+            get
+            {
+                return "Opens the About Screen";
+            }
+        }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.Controls.Standard/AboutScreen/MenuDefinitions.cs
@@ -0,0 +1,15 @@
+﻿using AvalonStudio.Controls.Standard.AboutScreen.Commands;
+using AvalonStudio.Extensibility.Menus;
+
+namespace AvalonStudio.Controls.Standard.AboutScreen
+{
+    static class MenuDefinitions
+    {
+        [MenuGroup]
+        public static MenuItemGroupDefinition HelpAboutGroup = new MenuItemGroupDefinition(AvalonStudio.Extensibility.MainMenu.MenuDefinitions.HelpMenu, 300);
+
+        [MenuItem]
+        public static MenuItemDefinition HelpAboutItem = new CommandMenuItemDefinition<AboutScreenCommandDefinition>(HelpAboutGroup, 300);
+
+    }
+}
