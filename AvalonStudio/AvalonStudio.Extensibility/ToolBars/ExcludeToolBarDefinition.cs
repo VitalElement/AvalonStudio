4a330262311b8229932a0f2aca0d5974f506e783
Dan Walmsley <dan@walms.co.uk>
2016-05-01 12:45:56 +0100

    Added toolbar definitions.

---
+++ AvalonStudio/AvalonStudio.Extensibility/ToolBars/ExcludeToolBarDefinition.cs
@@ -0,0 +1,16 @@
+﻿namespace AvalonStudio.Extensibility.ToolBars
+{
+    public class ExcludeToolBarDefinition
+    {
+        private readonly ToolBarDefinition _toolBarDefinitionToExclude;
+        public ToolBarDefinition ToolBarDefinitionToExclude
+        {
+            get { return _toolBarDefinitionToExclude; }
+        }
+
+        public ExcludeToolBarDefinition(ToolBarDefinition toolBarDefinition)
+        {
+            _toolBarDefinitionToExclude = toolBarDefinition;
+        }
+    }
+}
