e7358fea3deb0010b17e1ea5148392a740e83017
Dan Walmsley <dan@walms.co.uk>
2016-05-06 23:53:24 +0100

    Ported ToolBar builder and toolbars are now built during inititialization, a little messy at the moment.

---
+++ AvalonStudio/AvalonStudio/Shell/ToolBarDefinitions.cs
@@ -0,0 +1,26 @@
+﻿namespace AvalonStudio.Shell
+{
+    using AvalonStudio.Extensibility.ToolBars;
+    using Commands;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    static class ToolBarDefinitions
+    {
+        [Export]
+        public static ToolBarItemGroupDefinition StandardOpenSaveToolBarGroup = new ToolBarItemGroupDefinition(
+            ToolBars.ToolBarDefinitions.MainToolBar, 8);
+
+        [Export]
+        public static ToolBarItemDefinition OpenFileToolBarItem = new CommandToolBarItemDefinition<CloseFileCommandDefinition>(
+            StandardOpenSaveToolBarGroup, 0);
+
+        [Export]
+        public static ToolBarItemDefinition SaveFileToolBarItem = new CommandToolBarItemDefinition<SaveFileCommandDefinition>(
+            StandardOpenSaveToolBarGroup, 2);
+    }
+}
