8303dbed2572bdc784bf67dd2ec7d91ec60478e1
Dan Walmsley <dan@walms.co.uk>
2016-05-06 13:54:02 +0100

    Added menu items definitions for basic debugging commands.

--- AvalonStudio/AvalonStudio.Debugging/MenuDefinitions.cs
+++ AvalonStudio/AvalonStudio.Debugging/MenuDefinitions.cs
@@ -13,5 +13,20 @@
 
         [MenuItem]
         public static MenuItemDefinition StartDebugging = new CommandMenuItemDefinition<StartDebuggingCommandDefinition>(DebugStartGroup, 0);
+
+        [MenuGroup]
+        public static MenuItemGroupDefinition DebugControlGroup = new MenuItemGroupDefinition(DebugMenu, 3);
+
+        [MenuItem]
+        public static MenuItemDefinition StepOver = new CommandMenuItemDefinition<StepOverCommandDefinition>(DebugControlGroup, 0);
+
+        public static MenuItemDefinition StepInto = new CommandMenuItemDefinition<StepIntoCommandDefinition>(DebugControlGroup, 1);
+
+        public static MenuItemDefinition StepIntruction = new CommandMenuItemDefinition<StepInstructionCommandDefinition>(DebugControlGroup, 2);
+
+        public static MenuItemDefinition Pause = new CommandMenuItemDefinition<PauseDebuggingCommandDefinition>(DebugControlGroup, 3);
+
+        public static MenuItemDefinition Stop = new CommandMenuItemDefinition<StopDebuggingCommandDefinition>(DebugControlGroup, 4);
+
     }
 }
