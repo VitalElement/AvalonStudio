a4c8cd4997dbbf0d0b510d3043cd7aca7d53d0d0
Dan Walmsley <dan@walms.co.uk>
2016-05-06 13:54:02 +0100

    Added menu items definitions for basic debugging commands.

---
+++ AvalonStudio/AvalonStudio.Debugging/Commands/StepInstuctionCommandDefinition.cs
@@ -0,0 +1,60 @@
+﻿namespace AvalonStudio.Controls.Standard.SolutionExplorer.Commands
+{
+    using AvalonStudio.Extensibility.Commands;
+    using Debugging;
+    using Extensibility;
+    using Perspex.Controls;
+    using Perspex.Input;
+    using Projects;
+    using ReactiveUI;
+    using Shell;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Windows.Input;
+    using Utils;
+
+    [CommandDefinition]
+    class StepInstructionCommandDefinition : CommandDefinition
+    {
+        public StepInstructionCommandDefinition()
+        {
+            command = ReactiveCommand.Create();
+            command.Subscribe(_ =>
+            {
+                var manager = IoC.Get<IDebugManager>();
+
+                manager.StepInstruction();
+            });
+        }
+
+        private ReactiveCommand<object> command;
+
+        public override ICommand Command
+        {
+            get
+            {
+                return command;
+            }
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return "Step Instruction";
+            }
+        }
+
+        public override string ToolTip
+        {
+            get
+            {
+                return "Steps a single instruction.";
+            }
+        }
+
+        [Export]
+        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<StepOverCommandDefinition>(new KeyGesture() { Key = Key.F9 });
+    }
+}
