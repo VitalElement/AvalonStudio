ec23bed3a952fd56c5f3a62f63cd7072f020d98a
Dan Walmsley <dan@walms.co.uk>
2016-05-22 23:04:38 +0100

    added begginings of completion advice for intellisense.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Languages/ICompletionAdviceControl.cs
@@ -0,0 +1,16 @@
+﻿namespace AvalonStudio.Languages
+{
+    using AvalonStudio.Languages;
+    using System.Collections.Generic;
+
+    public interface ICompletionAdviceControl
+    {
+        bool IsVisible { get; set; }
+
+        Symbol Symbol { get; set; }
+
+        int SelectedIndex { get; set; }
+
+        int Count { get; set; }
+    }
+}
