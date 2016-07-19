820f4a4ba6393e484fd9ab4e81293bfcf7ff49a2
Darnell Williams <williams.darnell56@yahoo.com>
2016-07-19 14:24:46 -0400

    Additional Code Style Changes

--- AvalonStudio/AvalonStudio.Extensibility/Projects/IProjectFolder.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Projects/IProjectFolder.cs
@@ -1,23 +1,22 @@
 namespace AvalonStudio.Projects
 {
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
+	using System.Collections.ObjectModel;
 
-    public interface IProjectFolder : IProjectItem
-    {
-        /// <summary>
-        /// List of items within the project folder
-        /// </summary>
-        ObservableCollection<IProjectItem> Items { get; }
+	public interface IProjectFolder : IProjectItem
+	{
+		/// <summary>
+		/// List of items within the project folder
+		/// </summary>
+		ObservableCollection<IProjectItem> Items { get; }
 
-        void AddFile(ISourceFile file);
-        void AddFolder(IProjectFolder folder);
+		void AddFile(ISourceFile file);
+		void AddFolder(IProjectFolder folder);
 
-        void RemoveFile(ISourceFile file);
-        void RemoveFolder(IProjectFolder folder);
+		void RemoveFile(ISourceFile file);
+		void RemoveFolder(IProjectFolder folder);
 
-        string Location { get; }
+		string Location { get; }
 
-        string LocationDirectory { get; }
-    }
+		string LocationDirectory { get; }
+	}
 }
