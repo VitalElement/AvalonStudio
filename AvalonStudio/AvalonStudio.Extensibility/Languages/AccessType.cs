1dd585b2eafcde46e8291a4e98a23c62d73a72ee
Darnell Williams <williams.darnell56@yahoo.com>
2016-07-19 12:24:15 -0400

    Corrects tabbing and file nesting
    in AvalonStudio.Extensibility

--- AvalonStudio/AvalonStudio.Extensibility/Languages/AccessType.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Languages/AccessType.cs
@@ -1,14 +1,14 @@
 ﻿namespace AvalonStudio.Languages
 {
-    /// <summary>
+	/// <summary>
 	/// Represents the C++ access control level to a base class for a
 	/// cursor with kind <c>CX_CXXBaseSpecifier</c>.
 	/// </summary>
 	public enum AccessType
-    {
-        Invalid,
-        Public,
-        Protected,
-        Private
-    }
+	{
+		Invalid,
+		Public,
+		Protected,
+		Private
+	}
 }
--- AvalonStudio/AvalonStudio.Extensibility/ToolBars/Models/ToolBarModel.cs
+++ AvalonStudio/AvalonStudio.Extensibility/ToolBars/Models/ToolBarModel.cs
@@ -2,8 +2,8 @@ using AvalonStudio.Extensibility.MVVM;
 
 namespace AvalonStudio.Extensibility.ToolBars.Models
 {
-    public class ToolBarModel : BindableCollection<ToolBarItemBase>, IToolBar
+	public class ToolBarModel : BindableCollection<ToolBarItemBase>, IToolBar
 	{
-        
+		
 	}
 }
