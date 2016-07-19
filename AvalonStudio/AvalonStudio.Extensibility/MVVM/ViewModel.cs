37309d149e24ebe9d0bdaca71ee92d4e679cf050
Darnell Williams <williams.darnell56@yahoo.com>
2016-07-19 14:09:12 -0400

    ViewModel Style Corrections

--- AvalonStudio/AvalonStudio.Extensibility/MVVM/ViewModel.cs
+++ AvalonStudio/AvalonStudio.Extensibility/MVVM/ViewModel.cs
@@ -1,116 +1,114 @@
 namespace AvalonStudio.MVVM
 {
-    using Extensibility;
-    using ReactiveUI;
-    using Shell;
-    using System.ComponentModel.Composition;
+	using System.ComponentModel.Composition;
+	using ReactiveUI;
 
-    public interface IActivatable
-    {
-        void Activate();
-    }
+	public interface IActivatable
+	{
+		void Activate();
+	}
 
-    public enum Location
-    {
-        Left,
-        Right,
-        Bottom,
-        BottomRight,
-        RightBottom,
-        RightMiddle,
-        RightTop,
-    }
+	public enum Location
+	{
+		Left,
+		Right,
+		Bottom,
+		BottomRight,
+		RightBottom,
+		RightMiddle,
+		RightTop,
+	}
 
 
-    [InheritedExport(typeof(ToolViewModel))]
-    public abstract class ToolViewModel : ViewModel
-    {
-        public ToolViewModel()
-        {
-            isVisible = true;
-        }
+	[InheritedExport(typeof(ToolViewModel))]
+	public abstract class ToolViewModel : ViewModel
+	{
+		protected ToolViewModel()
+		{
+			_isVisible = true;
+		}
 
-        private bool isVisible;
-        public bool IsVisible
-        {
-            get { return isVisible; }
-            set
-            {
-                this.RaiseAndSetIfChanged(ref isVisible, value);
-            }
-        }
-        
-        public abstract Location DefaultLocation { get; }
+		private bool _isVisible;
+		public bool IsVisible
+		{
+			get { return _isVisible; }
+			set
+			{
+				this.RaiseAndSetIfChanged(ref _isVisible, value);
+			}
+		}
 
-        // TODO This should use ToolControl
-        private string title;
-        public string Title
-        {
-            get { return title; }
-            set { this.RaiseAndSetIfChanged(ref title, value); }
-        }
-    }
-    
-    public abstract class ToolViewModel<T> : ToolViewModel
-    {
-        public ToolViewModel(T model)
-        {
-            this.model = model;
-        }
+		public abstract Location DefaultLocation { get; }
 
-        private T model;
-        new public T Model
-        {
-            get { return model; }
-            set { this.RaiseAndSetIfChanged(ref model, value); }
-        }
-    }
+		// TODO This should use ToolControl
+		private string _title;
+		public string Title
+		{
+			get { return _title; }
+			set { this.RaiseAndSetIfChanged(ref _title, value); }
+		}
+	}
 
-    public abstract class ViewModel : ViewModel<object>
-    {
-        public ViewModel() : base(null)
-        {
+	public abstract class ToolViewModel<T> : ToolViewModel
+	{
+		protected ToolViewModel(T model)
+		{
+			this._model = model;
+		}
 
-        }        
-    }
+		private T _model;
+		public new T Model
+		{
+			get { return _model; }
+			set { this.RaiseAndSetIfChanged(ref _model, value); }
+		}
+	}
 
-    public abstract class HeaderedViewModel : HeaderedViewModel<object>
-    {
-        public HeaderedViewModel(string header) : base(header, null)
-        {
-        }
-    }
+	public abstract class ViewModel : ViewModel<object>
+	{
+		protected ViewModel() : base(null)
+		{
 
-    public abstract class HeaderedViewModel<T> : ViewModel<T>
-    {
-        public HeaderedViewModel(string header, T model) : base(model)
-        {
-            Title = header;
-        }
+		}
+	}
 
-        public string Title
-        {
-            get; private set;
-        }
-    }
+	public abstract class HeaderedViewModel : HeaderedViewModel<object>
+	{
+		protected HeaderedViewModel(string header) : base(header, null)
+		{
+		}
+	}
 
-    public abstract class ViewModel<T> : ReactiveObject
-    {
-        public ViewModel(T model)
-        {
-            this.model = model;
-        }
+	public abstract class HeaderedViewModel<T> : ViewModel<T>
+	{
+		protected HeaderedViewModel(string header, T model) : base(model)
+		{
+			Title = header;
+		}
 
-        private T model;
-        public T Model
-        {
-            get { return model; }
-            set { this.RaiseAndSetIfChanged(ref model, value); Invalidate(); }
-        }
+		public string Title
+		{
+			get; private set;
+		}
+	}
 
-        public void Invalidate ()
-        {
-            this.RaisePropertyChanged("");
-        }
-    }
+	public abstract class ViewModel<T> : ReactiveObject
+	{
+		protected ViewModel(T model)
+		{
+			this._model = model;
+		}
+
+		private T _model;
+		public T Model
+		{
+			get { return _model; }
+			set { this.RaiseAndSetIfChanged(ref _model, value); Invalidate(); }
+		}
+
+		public void Invalidate()
+		{
+			this.RaisePropertyChanged("");
+		}
+	}
 }
