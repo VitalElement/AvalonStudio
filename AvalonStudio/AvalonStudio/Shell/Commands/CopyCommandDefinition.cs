fd00f3366efeb05c13651a7047e204ab8b1408cc
Darnell Williams <williams.darnell56@yahoo.com>
2016-07-19 13:57:59 -0400

    Adjusts Tabbing and Adds C#6 features

--- AvalonStudio/AvalonStudio/Shell/Commands/CopyCommandDefinition.cs
+++ AvalonStudio/AvalonStudio/Shell/Commands/CopyCommandDefinition.cs
@@ -1,27 +1,15 @@
 namespace AvalonStudio.Shell.Commands
 {
-    using AvalonStudio.Extensibility.Commands;
-    using ReactiveUI;
-    [CommandDefinition]
-    public class CopyCommandDefinition : CommandDefinition
-    {
-        public override string Text
-        {
-            get { return "Copy"; }
-        }
+	using AvalonStudio.Extensibility.Commands;
+	using ReactiveUI;
+	[CommandDefinition]
+	public class CopyCommandDefinition : CommandDefinition
+	{
+		public override string Text => "Copy";
 
-        public override string ToolTip
-        {
-            get { return "Copy ToolTip"; }
-        }
+		public override string ToolTip => "Copy ToolTip";
 
-        ReactiveCommand<object> command;
-        public override System.Windows.Input.ICommand Command
-        {
-            get
-            {
-                return command;
-            }
-        }
-    }
+		ReactiveCommand<object> _command;
+		public override System.Windows.Input.ICommand Command => _command;
+	}
 }
