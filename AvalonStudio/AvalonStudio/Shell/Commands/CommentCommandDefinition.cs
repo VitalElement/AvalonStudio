b8336db417354b7466b84623846c6842f2158260
Darnell Williams <williams.darnell56@yahoo.com>
2016-07-19 13:57:59 -0400

    Adjusts Tabbing and Adds C#6 features

--- AvalonStudio/AvalonStudio/Shell/Commands/CommentCommandDefinition.cs
+++ AvalonStudio/AvalonStudio/Shell/Commands/CommentCommandDefinition.cs
@@ -1,62 +1,41 @@
 namespace AvalonStudio.Shell.Commands
 {
-    using Extensibility.Commands;
-    using System;
-    using System.ComponentModel.Composition;
-    using Avalonia.Input;
-    using ReactiveUI;
-    using Extensibility;
-    using Avalonia.Controls.Shapes;
-    using Avalonia.Media;
+	using System;
+	using System.ComponentModel.Composition;
+	using Avalonia.Controls.Shapes;
+	using Avalonia.Input;
+	using Avalonia.Media;
+	using Extensibility;
+	using Extensibility.Commands;
+	using ReactiveUI;
 
-    [CommandDefinition]
-    public class CommentCommandDefinition : CommandDefinition
-    {
-        public CommentCommandDefinition()
-        {
-            command = ReactiveCommand.Create();
+	[CommandDefinition]
+	public class CommentCommandDefinition : CommandDefinition
+	{
+		public CommentCommandDefinition()
+		{
+			_command = ReactiveCommand.Create();
 
-            command.Subscribe(_ =>
-            {
-                var shell = IoC.Get<IShell>();
+			_command.Subscribe(_ =>
+			{
+				var shell = IoC.Get<IShell>();
 
-                shell.SelectedDocument?.Comment();
-            });
-        }
-        
-        public override string Text
-        {
-            get { return "Comment"; }
-        }
+				shell.SelectedDocument?.Comment();
+			});
+		}
 
-        public override string ToolTip
-        {
-            get { return "Comments the selected code."; }
-        }
+		public override string Text => "Comment";
 
-        public override Path IconPath
-        {
-            get
-            {
-                return new Path() { Fill = Brush.Parse("#FF8DD28A"), UseLayoutRounding = false, Stretch = Stretch.Uniform, Data = StreamGeometry.Parse("M3,3H21V5H3V3M9,7H21V9H9V7M3,11H21V13H3V11M9,15H21V17H9V15M3,19H21V21H3V19Z") };
-            }
-        }
+		public override string ToolTip => "Comments the selected code.";
 
-        public override Uri IconSource
-        {
-            get { return new Uri(""); }
-        }
+		public override Path IconPath => new Path() { Fill = Brush.Parse("#FF8DD28A"), UseLayoutRounding = false, Stretch = Stretch.Uniform, Data = StreamGeometry.Parse("M3,3H21V5H3V3M9,7H21V9H9V7M3,11H21V13H3V11M9,15H21V17H9V15M3,19H21V21H3V19Z") };
 
-        ReactiveCommand<object> command;
-        public override System.Windows.Input.ICommand Command
-        {
-            get
-            {
-                return command;
-            }
-        }
+		public override Uri IconSource => new Uri("");
 
-        [Export]
-        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<SaveFileCommandDefinition>(new KeyGesture() { Key = Key.S, Modifiers = InputModifiers.Control });
-    }
+		readonly ReactiveCommand<object> _command;
+		public override System.Windows.Input.ICommand Command => _command;
+
+		[Export]
+		public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<SaveFileCommandDefinition>(new KeyGesture() { Key = Key.S, Modifiers = InputModifiers.Control });
+	}
 }
