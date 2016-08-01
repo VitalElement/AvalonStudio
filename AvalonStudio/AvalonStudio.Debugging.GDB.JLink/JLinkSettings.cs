d9af9af437859323a8ce7a335909bfcaf8a0b5fb
James Walmsley <james@fullfat-fs.co.uk>
2016-08-01 21:13:49 +0100

    Jlink device selection with insta-filter.

--- AvalonStudio/AvalonStudio.Debugging.GDB.JLink/JLinkSettings.cs
+++ AvalonStudio/AvalonStudio.Debugging.GDB.JLink/JLinkSettings.cs
@@ -3,5 +3,6 @@ namespace AvalonStudio.Debugging.GDB.JLink
 	public class JLinkSettings
 	{
 		public JlinkInterfaceType Interface { get; set; }
+        public string TargetDevice { get; set; }
 	}
 }
