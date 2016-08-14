8669a24e20b199b4c7064eba8c28a9da417353a9
Dan Walmsley <dan@walms.co.uk>
2016-08-14 22:23:47 +0100

    added linux compatible postbuild scripts.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -9,4 +9,7 @@ apt-get install referenceassemblies-pcl -y
 mkdir -p .nuget
 wget -O .nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 
+echo "#!/bin/sh" > /usr/bin/call
+echo "bash \$*" >> /usr/bin/call
+chmod +x /usr/bin/call
 
