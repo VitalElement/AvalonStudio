86870bf75c9b4c85644a857a51c5d168b81ffb94
Dan Walmsley <dan@walms.co.uk>
2016-08-22 23:56:48 +0100

    updated dependencies.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -13,3 +13,6 @@ echo "#!/bin/sh" > /usr/bin/call
 echo "bash \$*" >> /usr/bin/call
 chmod +x /usr/bin/call
 
+nuget sources add -name AvaloniaUI -Source https://www.myget.org/F/avalonia-ci/api/v2
+nuget sources add -name AvaloniaXamlBehaviors -Source https://www.myget.org/F/xamlbehaviors-nightly/api/v2
+
