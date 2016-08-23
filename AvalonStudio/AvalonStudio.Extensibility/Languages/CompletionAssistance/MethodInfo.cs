c4ad65bffb65a72681825ccd440a364e8d842e79
Dan Walmsley <dan@walms.co.uk>
2016-08-23 11:09:24 +0100

    very crude, initial implementation of completion assistance.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Languages/CompletionAssistance/MethodInfo.cs
@@ -0,0 +1,19 @@
+﻿using AvalonStudio.Languages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Extensibility.Languages.CompletionAssistance
+{
+    public class MethodInfo
+    {
+        public MethodInfo (List<Symbol> overloads)
+        {
+            Overloads = overloads;
+        }
+
+        public List<Symbol> Overloads { get; private set; }
+    }
+}
