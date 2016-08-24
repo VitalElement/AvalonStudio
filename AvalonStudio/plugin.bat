4a91b7654b5bd2d189307e44c96555d57eb00757
Dan Walmsley <dan@walms.co.uk>
2016-08-24 10:40:11 +0100

    fixed excluding AnyCPU directory in windows build scripts.

--- AvalonStudio/plugin.bat
+++ AvalonStudio/plugin.bat
@@ -1,4 +1,7 @@
 echo AvalonStudio Plugin
 
-copy /Y %1 %2AvalonStudio\bin\%3\%4\Plugins\%5
-if NOT "%6" == "false" copy /Y %1 %2AvalonStudioBuild\bin\%3\%4\Plugins\%5
+set platform=%3
+if %platform% == "AnyCPU" set platform=""
+
+copy /Y %1 %2AvalonStudio\bin\%platform%\%4\Plugins\%5
+if NOT "%6" == "false" copy /Y %1 %2AvalonStudioBuild\bin\%platform%\%4\Plugins\%5
