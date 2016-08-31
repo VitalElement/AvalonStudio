e862265085e4563acc726ad3de44ad5d2e00d49d
danwalmsley <dan@walms.co.uk>
2016-08-31 17:03:25 +0100

    Added beginings of Signature Help to ILanguageService.

---
+++ AvalonStudio/AvalonStudio.Languages.CSharp/OmniSharp/SignatureHelpOmniSharpRequest.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Languages.CSharp.OmniSharp
+{
+    class SignatureHelpOmniSharpRequest : OmniSharpRequest<RestResponse>
+    {
+        public override string EndPoint
+        {
+            get
+            {
+                return "signaturehelp";
+            }
+        }
+    }
+}
