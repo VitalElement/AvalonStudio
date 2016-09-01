9711315a0b819080f745452c6b7547766b6b0a63
Dan Walmsley <dan@walms.co.uk>
2016-09-01 19:05:45 +0100

    csharp and cplusplus working on same completion data types.

--- AvalonStudio/AvalonStudio.Extensibility/Languages/CodeCompletionData.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Languages/CodeCompletionData.cs
@@ -1,10 +1,27 @@
 namespace AvalonStudio.Languages
 {
+    public enum CodeCompletionKind
+    {
+        Keyword,
+        Namespace,
+        Delegate,
+        Interface,
+        Property,
+        Event,
+        Enum,
+        Struct,
+        Class,
+        Method,
+        None,
+        Macro,
+
+    }
+
 	public class CodeCompletionData
 	{
 		public uint Priority { get; set; }
 		public string Suggestion { get; set; }
-		public CursorKind Kind { get; set; }
+		public CodeCompletionKind Kind { get; set; }
 		public string Hint { get; set; }
 		public string BriefComment { get; set; }
 	}
