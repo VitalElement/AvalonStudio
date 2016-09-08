20d5f8e4c7806a86ef30ebfe3be56151e700a15d
Mathias Danielsen <mathais234@gmail.com>
2016-09-08 21:10:31 +0200

    Started work on Welcome Screen

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/WelcomeScreen/WelcomeScreenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Extensibility.Dialogs;
+using AvalonStudio.Shell;
+
+namespace AvalonStudio.Controls.Standard.WelcomeScreen {
+    public class WelcomeScreenViewModel : ModalDialogViewModelBase {
+
+        private ObservableCollection<string> solutionPaths;
+
+        public WelcomeScreenViewModel() : base("Welcome Screen") {
+            var shell = IoC.Get<IShell>();
+
+            solutionPaths = new ObservableCollection<string>();
+
+            for (int i = 0; i < 5; i++) {
+                solutionPaths.Add("I = " + i);
+            }
+
+        }
+    }
+}
