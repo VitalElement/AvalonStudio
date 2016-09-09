abe56014f4cbee2eeb90656759c867316f8e34ac
Mathias Danielsen <mathais234@gmail.com>
2016-09-09 23:22:57 +0200

    More work on Welcome Screen

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/WelcomeScreen/RecentProjectViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AvalonStudio.Extensibility;
+using AvalonStudio.MVVM;
+using AvalonStudio.Shell;
+using ReactiveUI;
+
+namespace AvalonStudio.Controls.Standard.WelcomeScreen {
+    public class RecentProjectViewModel : ViewModel {
+        public RecentProjectViewModel(string name, string location) {
+            this.name = name;
+
+            ClickCommand = ReactiveCommand.Create();
+
+            ClickCommand.Subscribe(_ => {
+                var shell = IoC.Get<IShell>();
+
+                // add shell code to load solution...
+            });
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.RaiseAndSetIfChanged(ref name, value); }
+        }
+
+        public ReactiveCommand<object> ClickCommand { get; }
+    }
+}
