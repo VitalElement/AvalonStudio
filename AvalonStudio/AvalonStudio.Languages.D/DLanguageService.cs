0732d3aa8c1dbc4b6e9bf6cf0374dfc6b63165ef
Pursche <pursche01@gmail.com>
2016-09-11 00:25:44 +0200

    Add DLanguageService

---
+++ AvalonStudio/AvalonStudio.Languages.D/DLanguageService.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AvalonStudio.Extensibility.Languages.CompletionAssistance;
+using AvalonStudio.Extensibility.Threading;
+using AvalonStudio.Projects;
+using AvalonStudio.Projects.DUB;
+using AvalonStudio.TextEditor.Document;
+using AvalonStudio.TextEditor.Indentation;
+using AvalonStudio.TextEditor.Rendering;
+using D_Parser.Dom;
+using D_Parser.Dom.Expressions;
+using D_Parser.Parser;
+
+namespace AvalonStudio.Languages.D
+{
+    public class DLanguageService : ILanguageService
+    {
+        private readonly JobRunner intellisenseJobRunner;
+
+        public DLanguageService()
+        {
+            IndentationStrategy = new DefaultIndentationStrategy();
+            intellisenseJobRunner = new JobRunner();
+
+            Task.Factory.StartNew(() => { intellisenseJobRunner.RunLoop(new CancellationToken()); });
+        }
+
+        public IIndentationStrategy IndentationStrategy
+        {
+            get;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return "D";
+            }
+        }
+
+        public Type BaseTemplateType
+        {
+            get;
+        }
+
+        public Task<List<CodeCompletionData>> CodeCompleteAtAsync(ISourceFile sourceFile, int line, int column, List<UnsavedFile> unsavedFiles, string filter = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CodeAnalysisResults> RunCodeAnalysisAsync(ISourceFile file, List<UnsavedFile> unsavedFiles, Func<bool> interruptRequested)
+        {
+            
+            throw new NotImplementedException(); // Work out the highlighting data
+        }
+
+        public IList<IDocumentLineTransformer> GetDocumentLineTransformers(ISourceFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<IBackgroundRenderer> GetBackgroundRenderers(ISourceFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSourceFile(IIntellisenseControl intellisenseControl, ICompletionAssistant completionAssistant,
+            TextEditor.TextEditor editor, ISourceFile file, TextDocument textDocument)
+        {
+            
+            
+        }
+
+        public void UnregisterSourceFile(TextEditor.TextEditor editor, ISourceFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanHandle(ISourceFile file)
+        {
+            bool result = false;
+
+            switch (Path.GetExtension(file.Location))
+            {
+                case ".d":
+                    result = true;
+                    break;
+            }
+
+            // ???
+            /*if (!(file.Project.Solution is DUBProject))
+            {
+                result = false;
+            }*/
+
+            return result;
+        }
+
+        public int Format(TextDocument textDocument, uint offset, uint length, int cursor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Comment(TextDocument textDocument, ISegment segment, int caret = -1, bool format = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UnComment(TextDocument textDocument, ISegment segment, int caret = -1, bool format = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SignatureHelp> SignatureHelp(ISourceFile file, UnsavedFile buffer, List<UnsavedFile> unsavedFiles, int line, int column, int offset,
+            string methodName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Symbol> GetSymbolAsync(ISourceFile file, List<UnsavedFile> unsavedFiles, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Symbol>> GetSymbolsAsync(ISourceFile file, List<UnsavedFile> unsavedFiles, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
