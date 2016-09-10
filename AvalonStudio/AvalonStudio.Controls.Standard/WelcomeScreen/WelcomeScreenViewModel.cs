e3e583ca63e7feb87031b0cb35c811a49c6d9f61
Mathias Danielsen <mathais234@gmail.com>
2016-09-10 14:55:27 +0200

    Work on News Feed in Welcome Screen

--- AvalonStudio/AvalonStudio.Controls.Standard/WelcomeScreen/WelcomeScreenViewModel.cs
+++ AvalonStudio/AvalonStudio.Controls.Standard/WelcomeScreen/WelcomeScreenViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
 using AvalonStudio.Extensibility;
 using AvalonStudio.Extensibility.Plugin;
 using AvalonStudio.Shell;
@@ -8,11 +10,13 @@ using ReactiveUI;
 namespace AvalonStudio.Controls.Standard.WelcomeScreen {
     public class WelcomeScreenViewModel : DocumentTabViewModel, IExtension {
         private ObservableCollection<RecentProjectViewModel> _recentProjects;
+        private ObservableCollection<NewsFeedViewModel> _newsFeed;
 
         public WelcomeScreenViewModel() {
             Title = "Welcome Screen";
 
             _recentProjects = new ObservableCollection<RecentProjectViewModel>();
+            _newsFeed = new ObservableCollection<NewsFeedViewModel>();
 
             var recentProjects = RecentProjectsCollection.RecentProjects;
 
@@ -23,6 +27,20 @@ namespace AvalonStudio.Controls.Standard.WelcomeScreen {
                 if (i < recentProjects.Count) {
                     _recentProjects.Add(new RecentProjectViewModel(recentProjects[i].Name, recentProjects[i].Path));
                 }
+            }
+
+
+            // RSS Releated
+            var RSSURL = @"http://sxp.microsoft.com/feeds/2.0/devblogs";
+            var reader = XmlReader.Create(RSSURL);
+            var feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            if (feed == null)
+                return;
+
+            foreach (var syndicationItem in feed.Items) {
+                _newsFeed.Add(new NewsFeedViewModel(syndicationItem.Summary.Text, syndicationItem.Categories.Count > 0 ? syndicationItem.Categories[0].Label : "null", syndicationItem.Authors[0].Name, syndicationItem.Title.Text));
             }
         }
 
@@ -46,12 +64,6 @@ namespace AvalonStudio.Controls.Standard.WelcomeScreen {
             RecentProjectsCollection.Save();
         }
 
-        public ObservableCollection<RecentProjectViewModel> RecentProjects
-        {
-            get { return _recentProjects; }
-            set { this.RaiseAndSetIfChanged(ref _recentProjects, value); }
-        }
-
         public void Activation() {
             var shell = IoC.Get<IShell>();
             shell.AddDocument(this);
@@ -60,6 +72,18 @@ namespace AvalonStudio.Controls.Standard.WelcomeScreen {
 
         public void BeforeActivation() {
 
+        }
+
+        public ObservableCollection<RecentProjectViewModel> RecentProjects
+        {
+            get { return _recentProjects; }
+            set { this.RaiseAndSetIfChanged(ref _recentProjects, value); }
+        }
+
+        public ObservableCollection<NewsFeedViewModel> NewsFeed
+        {
+            get { return _newsFeed; }
+            set { this.RaiseAndSetIfChanged(ref _newsFeed, value); }
         }
     }
 }
