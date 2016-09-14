7af3ef114f7a7adb78f7f50c46861dfbbe5dbcef
Dan Walmsley <dan@walms.co.uk>
2016-09-14 19:51:02 +0100

    Added and implemented ISolutionExplorer.

--- AvalonStudio/AvalonStudio.Controls.Standard/SolutionExplorer/Commands/NewSolutionCommandDefinition.cs
+++ AvalonStudio/AvalonStudio.Controls.Standard/SolutionExplorer/Commands/NewSolutionCommandDefinition.cs
@@ -1,41 +1,38 @@
-using System;
-using System.Windows.Input;
-using AvalonStudio.Extensibility;
-using AvalonStudio.Extensibility.Commands;
-using AvalonStudio.Shell;
-using ReactiveUI;
-
 namespace AvalonStudio.Controls.Standard.SolutionExplorer.Commands
 {
-	[CommandDefinition]
-	internal class NewSolutionCommandDefinition : CommandDefinition
-	{
-		private readonly ReactiveCommand<object> command;
+    using AvalonStudio.Extensibility;
+    using AvalonStudio.Extensibility.Commands;
+    using ReactiveUI;
+    using System;
+    using System.Windows.Input;
 
-		public NewSolutionCommandDefinition()
-		{
-			command = ReactiveCommand.Create();
-			command.Subscribe(_ =>
-			{
-				var shell = IoC.Get<IShell>();
-				shell.ModalDialog = new NewProjectDialogViewModel(shell.CurrentSolution);
-				shell.ModalDialog.ShowDialog();
-			});
-		}
+    [CommandDefinition]
+    internal class NewSolutionCommandDefinition : CommandDefinition
+    {
+        private readonly ReactiveCommand<object> command;
 
-		public override ICommand Command
-		{
-			get { return command; }
-		}
+        public NewSolutionCommandDefinition()
+        {
+            command = ReactiveCommand.Create();
+            command.Subscribe(_ =>
+            {
+                IoC.Get<ISolutionExplorer>().NewSolution();
+            });
+        }
 
-		public override string Text
-		{
-			get { return "New Solution"; }
-		}
+        public override ICommand Command
+        {
+            get { return command; }
+        }
 
-		public override string ToolTip
-		{
-			get { return "Creates a new Solution"; }
-		}
-	}
+        public override string Text
+        {
+            get { return "New Solution"; }
+        }
+
+        public override string ToolTip
+        {
+            get { return "Creates a new Solution"; }
+        }
+    }
 }
