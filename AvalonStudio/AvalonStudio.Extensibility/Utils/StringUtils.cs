9546fb06c753c8e1a63af77464708efc5ad37f4d
Darnell Williams <williams.darnell56@yahoo.com>
2016-09-14 18:24:20 -0400

    Corrects CS1513

--- AvalonStudio/AvalonStudio.Extensibility/Utils/StringUtils.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Utils/StringUtils.cs
@@ -168,18 +168,19 @@ namespace AvalonStudio.Extensibility
 			// Start on the next chunk
 			startIndex = endIndex + 1;
 		}
-	}
-	
-	/// <summary>
-	/// Compiled regular expression for performance.
-    	/// </summary>
-    	static Regex _htmlRegex = new Regex("<.*?>", RegexOptions.Compiled);
 
-    	/// <summary>
-    	/// Remove HTML from string with compiled Regex.
-    	/// </summary>
-    	public static string StripTags(string source)
-    	{
-		return _htmlRegex.Replace(source, string.Empty);
-    	}
+	
+		/// <summary>
+		/// Compiled regular expression for performance.
+	    	/// </summary>
+	    	static Regex _htmlRegex = new Regex("<.*?>", RegexOptions.Compiled);
+	
+	    	/// <summary>
+	    	/// Remove HTML from string with compiled Regex.
+	    	/// </summary>
+	    	public static string StripTags(string source)
+	    	{
+			return _htmlRegex.Replace(source, string.Empty);
+	    	}
+	}
 }
