45b7a7c221e62ad01b0e5fe50fc97d4f8680b946
0xFireball <0xFireball@outlook.com>
2016-10-25 21:47:23 -0700

    add typescript template project

---
+++ AvalonStudio/AvalonStudio.Projects.TypeScript/BlankTypeScriptProjectTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Projects.TypeScript
+{
+    public class BlankTypeScriptProjectTemplate : IProjectTemplate
+    {
+        public string DefaultProjectName => "TypeScriptProject";
+
+        public string Description => "Creates a blank TypeScript project";
+
+        public string Title => "Empty TypeScript Project";
+
+        public Task<IProject> Generate(ISolution solution, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
