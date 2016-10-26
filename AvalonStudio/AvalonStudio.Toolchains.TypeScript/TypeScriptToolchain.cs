3a551eb84e8484fd2f898863a0f1b733ede127e0
0xFireball <0xFireball@outlook.com>
2016-10-25 21:39:59 -0700

    implement StandardToolchain instead of IToolchain

--- AvalonStudio/AvalonStudio.Toolchains.TypeScript/TypeScriptToolchain.cs
+++ AvalonStudio/AvalonStudio.Toolchains.TypeScript/TypeScriptToolchain.cs
@@ -1,4 +1,5 @@
 ﻿using AvalonStudio.Projects;
+using AvalonStudio.Toolchains.Standard;
 using AvalonStudio.Utils;
 using System;
 using System.Collections.Generic;
@@ -6,47 +7,78 @@ using System.Threading.Tasks;
 
 namespace AvalonStudio.Toolchains.TypeScript
 {
-    public class TypeScriptToolchain : IToolChain
+    public class TypeScriptToolchain : StandardToolChain
     {
-        public string Description => "TypeScript Toolchain";
+        public override string Description => "TypeScript Toolchain";
 
-        public IList<string> Includes => new List<string>(); //None for now
+        public override string ExecutableExtension => "js";
 
-        public string Name => "TypeScript";
+        public override string StaticLibraryExtension => "js";
 
-        public Version Version => new Version(0, 1, 1, 0);
+        public override Version Version => new Version(0, 1, 1, 0);
 
-        public void Activation()
+        public override bool CanHandle(IProject project)
         {
             throw new NotImplementedException();
         }
 
-        public void BeforeActivation()
+        public override CompileResult Compile(IConsole console, Projects.Standard.IStandardProject superProject, Projects.Standard.IStandardProject project, ISourceFile file, string outputFile)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> Build(IConsole console, IProject project, string label = "", IEnumerable<string> definitions = null)
+        public override string GetCompilerArguments(Projects.Standard.IStandardProject superProject, Projects.Standard.IStandardProject project, ISourceFile sourceFile)
         {
             throw new NotImplementedException();
         }
 
-        public bool CanHandle(IProject project)
+        public override IList<object> GetConfigurationPages(IProject project)
         {
             throw new NotImplementedException();
         }
 
-        public Task Clean(IConsole console, IProject project)
+        public override string GetLinkerArguments(Projects.Standard.IStandardProject superProject, Projects.Standard.IStandardProject project)
         {
             throw new NotImplementedException();
         }
 
-        public IList<object> GetConfigurationPages(IProject project)
+        public override List<string> GetToolchainIncludes()
         {
             throw new NotImplementedException();
         }
 
-        public void ProvisionSettings(IProject project)
+        public override LinkResult Link(IConsole console, Projects.Standard.IStandardProject superProject, Projects.Standard.IStandardProject project, CompileResult assemblies, string outputPath)
+        {
+            var result = new LinkResult()
+            {
+                ExitCode = 0,
+            };
+            //TODO: Executable for JS?
+            //I'm assuming this would be an entrypoint module or something
+            return result;
+        }
+
+        public override async Task<bool> PostBuild(IConsole console, IProject project, LinkResult linkResult)
+        {
+            return true;
+        }
+
+        public override async Task<bool> PreBuild(IConsole console, IProject project)
+        {
+            return true;
+        }
+
+        public override void ProvisionSettings(IProject project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProcessResult Size(IConsole console, Projects.Standard.IStandardProject project, LinkResult linkResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SupportsFile(ISourceFile file)
         {
             throw new NotImplementedException();
         }
