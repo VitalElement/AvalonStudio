7cfca8c08693ffa49b25dcf267cad7102337becf
0xFireball <0xFireball@outlook.com>
2016-10-26 23:53:04 -0700

    port NTypeScript bindings to work with JINT

---
+++ AvalonStudio/AvalonStudio.Languages.TypeScript/TSCompiler/Resources/nTypescriptBootstrap.js
@@ -0,0 +1,13 @@
+﻿function tsTranspile(source) {
+    var compilerOptions = {};
+    var transpileResult = ts.transpileModule(source, { compilerOptions: compilerOptions, reportDiagnostics: true });
+    return transpileResult;
+}
+
+function getTranspileResultCode(transpileResult) {
+    return transpileResult.outputText;
+}
+
+function getTranspileResultDiagnostics(transpileResult) {
+    return transpileResult.diagnostics;
+}
