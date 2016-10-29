38619d1b97a14b2144e4f5f88a571a1e061c0762
0xFireball <0xFireball@outlook.com>
2016-10-28 20:51:06 -0700

    add command line tools for running CLI processes

---
+++ AvalonStudio/AvalonStudio.Extensibility/CommandLineTools/ShellExecuteResult.cs
@@ -0,0 +1,8 @@
+﻿namespace AvalonStudio.CommandLineTools
+{
+    public struct ShellExecuteResult
+    {
+        public int ExitCode { get; set; }
+        public string Output { get; set; }
+    }
+}
