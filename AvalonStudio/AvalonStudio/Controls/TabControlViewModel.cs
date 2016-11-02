167fedcb164b33b020a9e71b63c9f08b1897e772
danwalmsley <dan@walms.co.uk>
2016-11-02 13:51:23 +0000

    unsubscribe events.

--- AvalonStudio/AvalonStudio/Controls/TabControlViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/TabControlViewModel.cs
@@ -5,48 +5,71 @@
     using ReactiveUI;
     using System.Collections.Specialized;
     using System;
+    using System.Reactive.Disposables;
+    using System.Collections.Generic;
 
     public class TabControlViewModel : ViewModel
-	{
-		private object selectedTool;
+    {
+        private object selectedTool;
 
-		private ObservableCollection<object> tools;        
+        private ObservableCollection<object> tools;
 
-		public TabControlViewModel()
-		{
-			tools = new ObservableCollection<object>();
+        private Dictionary<ToolViewModel, IDisposable> disposableVisibleObservers;
+
+        public TabControlViewModel()
+        {
+            tools = new ObservableCollection<object>();
+            disposableVisibleObservers = new Dictionary<ToolViewModel, IDisposable>();
 
             tools.CollectionChanged += (sender, e) =>
             {
-                switch(e.Action)
+                switch (e.Action)
                 {
                     case NotifyCollectionChangedAction.Add:
-                        foreach(var item in e.NewItems)
+                        foreach (var item in e.NewItems)
                         {
-                            if(item is ToolViewModel)
+                            if (item is ToolViewModel)
                             {
                                 var tvm = item as ToolViewModel;
 
-                                tvm.IsVisibleObservable.Subscribe(_ => InvalidateIsVisible());                                                                
+                                var disposable = tvm.IsVisibleObservable.Subscribe(_ => InvalidateIsVisible());
+
+                                disposableVisibleObservers.Add(tvm, disposable);
                             }
                         }
 
                         InvalidateIsVisible();
                         break;
+
+                    case NotifyCollectionChangedAction.Remove:
+                        foreach (var item in e.OldItems)
+                        {
+                            if (item is ToolViewModel)
+                            {
+                                IDisposable disposable;
+
+                                var tvm = item as ToolViewModel;
+
+                                if (disposableVisibleObservers.TryGetValue(tvm, out disposable))
+                                {
+                                    disposable.Dispose();
+                                }
+                            }
+                        }
+                        break;
                 }
-                
             };
-		}
-        
+        }
+
         private void InvalidateIsVisible()
         {
             bool result = false;
 
-            foreach(var tool in Tools)
+            foreach (var tool in Tools)
             {
-                if(tool is ToolViewModel)
+                if (tool is ToolViewModel)
                 {
-                    if((tool as ToolViewModel).IsVisible)
+                    if ((tool as ToolViewModel).IsVisible)
                     {
                         result = true;
                         break;
@@ -55,19 +78,19 @@
             }
 
             IsVisible = result;
-        }       
+        }
 
-		public ObservableCollection<object> Tools
-		{
-			get { return tools; }
-			set { this.RaiseAndSetIfChanged(ref tools, value); }
-		}
+        public ObservableCollection<object> Tools
+        {
+            get { return tools; }
+            set { this.RaiseAndSetIfChanged(ref tools, value); }
+        }
 
-		public object SelectedTool
-		{
-			get { return selectedTool; }
-			set { this.RaiseAndSetIfChanged(ref selectedTool, value); }
-		}
+        public object SelectedTool
+        {
+            get { return selectedTool; }
+            set { this.RaiseAndSetIfChanged(ref selectedTool, value); }
+        }
 
         private bool isVisible;
         public bool IsVisible
