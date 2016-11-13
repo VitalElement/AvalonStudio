9943dd4b2736f297beca47a4491026c0e042902e
0xFireball <0xFireball@outlook.com>
2016-11-12 16:46:10 -0800

    register data association

---
+++ AvalonStudio/AvalonStudio.Languages.TypeScript/TypeScriptDataAssociation.cs
@@ -0,0 +1,14 @@
+﻿using AvalonStudio.TextEditor.Document;
+
+namespace AvalonStudio.Languages.TypeScript
+{
+    internal class TypeScriptDataAssociation
+    {
+        private TextDocument textDocument;
+
+        public TypeScriptDataAssociation(TextDocument textDocument)
+        {
+            this.textDocument = textDocument;
+        }
+    }
+}
