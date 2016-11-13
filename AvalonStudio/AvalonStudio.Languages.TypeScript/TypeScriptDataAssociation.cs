011dece3d7fa6fd229c8a87516412e42b1241343
0xFireball <0xFireball@outlook.com>
2016-11-12 16:54:06 -0800

    update Avalonia, add bracket matcher from CPP

--- AvalonStudio/AvalonStudio.Languages.TypeScript/TypeScriptDataAssociation.cs
+++ AvalonStudio/AvalonStudio.Languages.TypeScript/TypeScriptDataAssociation.cs
@@ -1,14 +1,31 @@
-﻿using AvalonStudio.TextEditor.Document;
+﻿using Avalonia.Input;
+using AvalonStudio.Languages.TypeScript.Rendering;
+using AvalonStudio.TextEditor.Document;
+using AvalonStudio.TextEditor.Rendering;
+using System;
+using System.Collections.Generic;
 
 namespace AvalonStudio.Languages.TypeScript
 {
     internal class TypeScriptDataAssociation
     {
-        private TextDocument textDocument;
-
         public TypeScriptDataAssociation(TextDocument textDocument)
         {
-            this.textDocument = textDocument;
+            BackgroundRenderers = new List<IBackgroundRenderer>();
+            DocumentLineTransformers = new List<IDocumentLineTransformer>();
+
+            TextColorizer = new TextColoringTransformer(textDocument);
+            TextMarkerService = new TextMarkerService(textDocument);
+
+            BackgroundRenderers.Add(new BracketMatchingBackgroundRenderer());
+            BackgroundRenderers.Add(TextMarkerService);
         }
+
+        public TextColoringTransformer TextColorizer { get; }
+        public TextMarkerService TextMarkerService { get; }
+        public List<IBackgroundRenderer> BackgroundRenderers { get; }
+        public List<IDocumentLineTransformer> DocumentLineTransformers { get; }
+        public EventHandler<KeyEventArgs> KeyUpHandler { get; set; }
+        public EventHandler<TextInputEventArgs> TextInputHandler { get; set; }
     }
 }
