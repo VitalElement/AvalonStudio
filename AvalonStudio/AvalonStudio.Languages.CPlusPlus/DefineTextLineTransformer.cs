deb24af9ea7608ed11f0599244cee776cb37e0f3
Dan Walmsley <dan@walms.co.uk>
2016-11-17 23:47:14 +0000

    Improved include and define highlighters.

--- AvalonStudio/AvalonStudio.Languages.CPlusPlus/DefineTextLineTransformer.cs
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/DefineTextLineTransformer.cs
@@ -4,22 +4,30 @@ using AvalonStudio.TextEditor.Rendering;
 
 namespace AvalonStudio.Languages.CPlusPlus.Rendering
 {
-	internal class DefineTextLineTransformer : IDocumentLineTransformer
-	{
-		private readonly SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(0xD0, 0xB8, 0x48, 0xFF));
-		private readonly IBrush pragmaBrush = Brush.Parse("#68217A");
+    internal class DefineTextLineTransformer : IDocumentLineTransformer
+    {
+        private readonly SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(0xD0, 0xB8, 0x48, 0xFF));
+        private readonly IBrush pragmaBrush = Brush.Parse("#68217A");
 
         public event EventHandler<EventArgs> DataChanged;
 
         public void TransformLine(TextView textView, VisualLine line)
-		{
-			if (line.RenderedText.Text.Contains("#define") && !line.RenderedText.Text.Trim().StartsWith("//"))
-			{
-				var startIndex = line.RenderedText.Text.IndexOf("#define");
+        {
+            var trimmed = line.RenderedText.Text.Trim();
 
-				line.RenderedText.SetForegroundBrush(pragmaBrush, startIndex, 7);
-				line.RenderedText.SetForegroundBrush(brush, startIndex + 7, line.RenderedText.Text.Length - 7);
-			}
-		}
-	}
+            if (trimmed.StartsWith("#") && !trimmed.StartsWith("#include"))
+            {
+                var startIndex = line.RenderedText.Text.IndexOf("#");
+
+                var firstEndOffset = line.RenderedText.Text.IndexOf(" ", startIndex);
+
+                var lastWordOffset = firstEndOffset != -1 ? line.RenderedText.Text.LastIndexOf(" ", firstEndOffset) + 1 : -1;
+
+                if (lastWordOffset != -1)
+                {
+                    line.RenderedText.SetForegroundBrush(brush, lastWordOffset, line.RenderedText.Text.Length - lastWordOffset);
+                }
+            }
+        }
+    }
 }
