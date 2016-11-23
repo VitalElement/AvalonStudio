4882f248c4ac17d00ecd23a025befc0ca4107bd7
Mathias Danielsen <mathais234@gmail.com>
2016-11-23 17:17:53 +0100

    Match whole words now works correctly

--- AvalonStudio/AvalonStudio/Controls/FindInFileViewModel.cs
+++ AvalonStudio/AvalonStudio/Controls/FindInFileViewModel.cs
@@ -1,12 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Avalonia;
-using AvalonStudio.Languages;
-using AvalonStudio.Languages.ViewModels;
 using AvalonStudio.MVVM;
-using AvalonStudio.TextEditor.Document;
 using ReactiveUI;
 
 namespace AvalonStudio.Controls {
@@ -14,20 +7,17 @@ namespace AvalonStudio.Controls {
         private EditorModel _editor;
         private EditorViewModel _editorViewModel;
 
-        private Thickness _position;
-
         private string _searchField;
         private bool _caseSensitive;
         private bool _matchWholeWord;
 
-        private int _caretIndex = 0;
-        private int _lastLine = 0;
+        private int _caretIndex;
+        private int _lastLine;
 
         public FindInFileViewModel(EditorModel editor, EditorViewModel viewModel) {
             _editor = editor;
             _editorViewModel = viewModel;
 
-            //Position = new Thickness(300, 50);
             Find = ReactiveCommand.Create();
 
             CaseSensitive = true;
@@ -36,11 +26,14 @@ namespace AvalonStudio.Controls {
             Find.Subscribe(_ => {
                 var lines = viewModel.TextDocument.Text.Split('\n');
 
+                _caretIndex = 0;
+
                 for (var i = 0; i < lines.Length; i++) {
                     var line = lines[i];
 
-                    if (MatchLine(line)) {
-                        viewModel.CaretIndex = _caretIndex;
+                    int matchPosition;
+                    if (MatchLine(line, out matchPosition)) {
+                        viewModel.CaretIndex = _caretIndex + matchPosition;
                         _lastLine++;
                         break;
                     }
@@ -59,23 +52,45 @@ namespace AvalonStudio.Controls {
                 _caretIndex += line.Length + 1;
         }
 
-        public bool MatchLine(string line) {
-            if (!_caseSensitive) {
-                if (line.ToLower().Contains(SearchField.ToLower()))
-                    return true;
-            }
-            else {
-                if (line.Contains(SearchField))
-                    return true;
+        public bool ContainsWord(string line) {
+            var words = line.Split(' ');
+                
+            foreach (var word in words) {
+                if (CaseSensitive) {
+                    if (word == SearchField)
+                        return true;
+                } else {
+                    if (string.Equals(word, SearchField, StringComparison.CurrentCultureIgnoreCase))
+                        return true;
+                }
             }
 
             return false;
         }
 
-        public Thickness Position
-        {
-            get { return _position; }
-            set { this.RaiseAndSetIfChanged(ref _position, value); }
+        public bool MatchLine(string line, out int matchPosition) {
+            if (MatchWholeWord) {
+                if (ContainsWord(line)) {
+                    matchPosition = 0;
+                    return true;
+                }
+            } else {
+                if (CaseSensitive) {
+                    if (line.Contains(SearchField)) {
+                        matchPosition = 0;
+                        return true;
+                    }
+                }
+                else {
+                    if (line.ToLower().Contains(SearchField.ToLower())) {
+                        matchPosition = 0;
+                        return true;
+                    }
+                }
+            }
+
+            matchPosition = 0;
+            return false;
         }
 
         public void Dispose() {
@@ -85,20 +100,17 @@ namespace AvalonStudio.Controls {
 
         public ReactiveCommand<object> Find { get; }
 
-        public string SearchField
-        {
+        public string SearchField {
             get { return _searchField; }
             set { this.RaiseAndSetIfChanged(ref _searchField, value); }
         }
 
-        public bool CaseSensitive
-        {
+        public bool CaseSensitive {
             get { return _caseSensitive; }
             set { this.RaiseAndSetIfChanged(ref _caseSensitive, value); }
         }
 
-        public bool MatchWholeWord
-        {
+        public bool MatchWholeWord {
             get { return _matchWholeWord; }
             set { this.RaiseAndSetIfChanged(ref _matchWholeWord, value); }
         }
