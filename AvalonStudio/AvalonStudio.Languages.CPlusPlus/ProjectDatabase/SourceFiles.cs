b268afd1cfe67eb11f02be9a0dd88f4f0139e99f
Dan Walmsley <dan@walms.co.uk>
2017-01-03 19:50:10 +0000

    Installed entityframework and enabled migrations.

---
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/SourceFiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
+{
+    public class SourceFiles
+    {
+        public int SourceFilesId { get; set; }
+        public string RelativePath { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
