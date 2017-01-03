38de3b82725861c25606fa6b11cdc5aead84e46b
Dan Walmsley <dan@walms.co.uk>
2017-01-03 19:50:10 +0000

    Installed entityframework and enabled migrations.

---
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<SourceFiles> SourceFiles { get; set; }
+    }
+}
