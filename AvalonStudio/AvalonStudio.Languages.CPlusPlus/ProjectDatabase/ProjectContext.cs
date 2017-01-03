597df9be0c7e71e1f99c548c1db95aedf60038aa
Dan Walmsley <dan@walms.co.uk>
2017-01-03 22:20:58 +0000

    now using new ef core.

--- AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/ProjectContext.cs
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/ProjectContext.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
+﻿namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
 {
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class ProjectContext : DbContext
     {
         public DbSet<SourceFiles> SourceFiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./contact.db");
+        }
     }
 }
