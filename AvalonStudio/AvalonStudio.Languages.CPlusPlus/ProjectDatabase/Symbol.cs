9b7ce72c74a644e5b5eb0752db48ffc2e27b40d0
Dan Walmsley <dan@walms.co.uk>
2017-01-04 16:22:44 +0000

    Added various tables to the database.

---
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/Symbol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
+{
+    public class Symbol
+    {
+        public int SymbolId { get; set; }
+        public virtual Definition Definition { get; set; }
+        public virtual SymbolReference USR { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/SymbolReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
+{
+    public class SymbolReference
+    {
+        public int SymbolReferenceId { get; set; }
+        
+        public string Reference { get; set; }
+    }
+}
