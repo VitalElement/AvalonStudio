4a3d1bb0cd2ea13fd458bba2d3e64f67aacc851c
Dan Walmsley <dan@walms.co.uk>
2017-01-06 23:42:03 +0000

    Correctly configured database relationships.

--- AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/Symbol.cs
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/Symbol.cs
@@ -1,19 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Symbol
     {
         public int SymbolId { get; set; }
-
-        //public int USRForeignKey { get; set; }
-        //[InverseProperty(   )]
-        public virtual SymbolReference USR { get; set; }
+        
+        public SymbolReference SymbolReference { get; set; }
 
         public int Line { get; set; }
         public int Column { get; set; }
--- AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/SymbolReference.cs
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/ProjectDatabase/SymbolReference.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
+﻿namespace AvalonStudio.Languages.CPlusPlus.ProjectDatabase
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class SymbolReference
     {
-        public int SymbolReferenceId { get; set; }        
+        public int SymbolReferenceId { get; set; }
+
         public string Reference { get; set; }
 
-        //public int DefinitionForeignKey { get; set; }
-        //[InverseProperty("USR")]
-        public virtual Symbol Definition { get; set; }
-
-        [InverseProperty("USR")]
-        public virtual List<Symbol> Symbols { get; set; }
+        public int? DefinitionForeignKey { get; set; }
+        public Symbol Definition { get; set; }
+        
+        public List<Symbol> Symbols { get; set; }
     }
 }
