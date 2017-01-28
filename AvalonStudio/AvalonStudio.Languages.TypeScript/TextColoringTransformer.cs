3308a31d6225f281671038868f4fad39f811f604
0xFireball <0xFireball@outlook.com>
2017-01-27 18:19:20 -0800

    improved highlighting

--- AvalonStudio/AvalonStudio.Languages.TypeScript/TextColoringTransformer.cs
+++ AvalonStudio/AvalonStudio.Languages.TypeScript/TextColoringTransformer.cs
@@ -18,7 +18,7 @@ namespace AvalonStudio.Languages.TypeScript.Rendering
 
             CommentBrush = Brush.Parse("#559A3F");
             CallExpressionBrush = Brush.Parse("Pink");
-            IdentifierBrush = Brush.Parse("#7A7777");
+            IdentifierBrush = Brush.Parse("#8C8C8C");
             KeywordBrush = Brush.Parse("#569CD6");
             LiteralBrush = Brush.Parse("#D69D85");
             PunctuationBrush = Brush.Parse("#D4D4D4");
