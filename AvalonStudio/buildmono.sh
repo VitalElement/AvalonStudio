74452cabcb81bd446f805c88f32681628ff9a729
Dan Walmsley <dan@walms.co.uk>
2017-01-29 20:16:55 +0000

    Revert "now using nuget.config, no need to manually add feeds."
    This reverts commit 75b0a966d1aea796e9b14b9d0ee5795616c576b4.

--- AvalonStudio/buildmono.sh
+++ AvalonStudio/buildmono.sh
@@ -2,6 +2,10 @@
 
 export PATH=$PATH:$(pwd)/bin
 
+
+mono .nuget/nuget.exe sources add -name AvaloniaUI -Source https://www.myget.org/F/avalonia-ci/api/v2
+mono .nuget/nuget.exe sources add -name AvaloniaXamlBehaviors -Source https://www.myget.org/F/xamlbehaviors-nightly/api/v2
+mono .nuget/nuget.exe sources add -name MabiAvalon -source https://www.myget.org/F/mabiavalon-ci/api/v2
 mono .nuget/nuget.exe restore AvalonStudio.sln
 xbuild /p:Configuration=Release ./AvalonStudio.sln 
 
