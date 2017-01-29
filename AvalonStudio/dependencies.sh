937fbd5abb2df9c782fb304624773cd7e212fe5d
Dan Walmsley <dan@walms.co.uk>
2017-01-29 20:47:55 +0000

    dependencies doesnt need to nuget packages.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -11,8 +11,3 @@ apt-get install mono-complete -y
 apt-get install referenceassemblies-pcl -y
 mkdir -p .nuget
 wget -O .nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-
-nuget sources add -name AvaloniaUI -Source https://www.myget.org/F/avalonia-ci/api/v2
-nuget sources add -name AvaloniaXamlBehaviors -Source https://www.myget.org/F/xamlbehaviors-nightly/api/v2
-nuget sources add -name DockNC -Source https://www.myget.org/F/mabiavalon-ci/api/v2
-
