3f37798e79708bcf898bad8296b6cff5453e7ce7
Unknown <dan@walms.co.uk>
2017-02-02 12:53:27 +0000

    make sure mono 4.6.2 gets installed on ubuntu.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -14,6 +14,9 @@ if [[ $ID == "arch" ]] ; then
     sudo pacman -S mono clang  
 fi
 if [[ $ID == "ubuntu" ]]; then
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
     sudo apt-get update
     sudo apt-get install mono-complete -y
     sudo apt-get install referenceassemblies-pcl -y
