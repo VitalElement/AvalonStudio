958542baa0273578cfc4dda0f4b11143304d0b00
Dan Walmsley <dan@walms.co.uk>
2017-02-04 10:18:50 +0000

    Main menu bar defined.

---
+++ AvalonStudio/AvalonStudio.Extensibility/MainMenu/MenuDefinitions/MainMenuBar.cs
@@ -0,0 +1,20 @@
+﻿using AvalonStudio.Extensibility.Menus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Extensibility.MainMenu
+{
+    class MainMenuBar : MenuBarDefinition
+    {
+        public override void Activation()
+        {
+            
+        }
+
+        public override void BeforeActivation()
+        {
+            IoC.RegisterConstant(this, typeof(MenuBarDefinition), "MainMenu");
+        }
+    }
+}
