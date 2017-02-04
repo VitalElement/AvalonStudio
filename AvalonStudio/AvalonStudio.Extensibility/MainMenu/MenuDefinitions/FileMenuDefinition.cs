7998cac46e8dd74aa44576e26dd29b808cbe93d1
Dan Walmsley <dan@walms.co.uk>
2017-02-04 10:19:08 +0000

    File menu defined.

---
+++ AvalonStudio/AvalonStudio.Extensibility/MainMenu/MenuDefinitions/FileMenuDefinition.cs
@@ -0,0 +1,19 @@
+﻿using AvalonStudio.Extensibility.Menus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Extensibility.MainMenu.MenuDefinitions
+{
+    class FileMenuDefinition : MenuDefinition
+    {
+        public FileMenuDefinition() : base(()=>IoC.Get<MenuBarDefinition>("MainMenu"), 0, "_File")
+        {
+        }
+
+        public override void BeforeActivation()
+        {
+            IoC.RegisterConstant(this, typeof(MenuDefinition), "FileMenu");
+        }
+    }
+}
