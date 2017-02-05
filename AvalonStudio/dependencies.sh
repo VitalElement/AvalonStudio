8548873a9c6e3a8ca23f61f9ab71c6d9820bcd94
Dan Walmsley <dan@walms.co.uk>
2017-02-05 21:56:19 +0000

    correctly install dotnet.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -11,7 +11,14 @@ export PATH=$PATH:$(pwd)/bin
 source /etc/os-release
 
 if [[ $ID == "ubuntu" ]]; then
+    wget https://dotnetcli.blob.core.windows.net/dotnet/preview/Installers/Latest/dotnet-host-ubuntu-x64.latest.deb
+    wget https://dotnetcli.blob.core.windows.net/dotnet/preview/Installers/Latest/dotnet-hostfxr-ubuntu-x64.latest.deb
+    wget https://dotnetcli.blob.core.windows.net/dotnet/preview/Installers/Latest/dotnet-hostfxr-ubuntu-x64.latest.deb
     wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/rel-1.0.0/dotnet-sdk-ubuntu-x64.latest.deb
+
+    sudo dpkg -i dotnet-host-ubuntu-x64.latest.deb
+    sudo dpkg -i dotnet-hostfxr-ubuntu-x64.latest.deb
+    sudo dpkg -i dotnet-hostfxr-ubuntu-x64.latest.deb
     sudo dpkg -i dotnet-sdk-ubuntu-x64.latest.deb
     dotnet --version
 fi
