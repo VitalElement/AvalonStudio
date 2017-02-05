5bb54d4e1cf75ace826645b14bec5d1fcd58af90
PFCKrutonium <PFCKrutonium@gmail.com>
2017-02-05 14:05:38 -0500

    Now installs .Net Core for use with AvalonStudio.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -1,4 +1,12 @@
+#
 #! /bin/bash
+#
+
+#
+# If you do not use yaourt, but instead another AUR helper, you can specify the helper as an argument - for example,
+# ./dependencies.sh pacaur
+# would use pacaur instead of yaourt.
+#
 
 mkdir ./bin &> /dev/null
 
@@ -10,8 +18,15 @@ export PATH=$PATH:$(pwd)/bin
 
 source /etc/os-release
 
-if [[ $ID == "arch" ]] ; then
-    sudo pacman -S mono clang  
+if [[ $ID == "arch" ]]; then
+    sudo pacman -S mono clang
+    echo If this errors because yaourt is not found, specify your AUR helper as an argument
+    echo for example, ./dependancies.sh pacaur.
+    if [[ $1 == "" ]]; then
+        yaourt -S dotnet-bin dotnet-coreclr-git icu52
+    else
+        $1 -S dotnet-bin dotnet-coreclr-git icu52
+    fi  
 fi
 if [[ $ID == "ubuntu" ]]; then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
