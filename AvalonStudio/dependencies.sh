a0b429be05205d9b7804d986a39e67e6b219ae9b
Dan Walmsley <dan@walms.co.uk>
2017-02-05 21:47:56 +0000

    reinstate travis.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -11,28 +11,10 @@ export PATH=$PATH:$(pwd)/bin
 source /etc/os-release
 
 if [[ $ID == "arch" ]] ; then
-    sudo pacman -S mono clang  
+    
 fi
 if [[ $ID == "ubuntu" ]]; then
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-
-    sudo apt-get update
-    sudo apt-get install mono-complete -y
-    sudo apt-get install referenceassemblies-pcl -y
+    wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/rel-1.0.0/dotnet-sdk-ubuntu-x64.latest.deb
+    sudo dpkg -i dotnet-sdk-ubuntu-x64.latest.deb
+    dotnet --version
 fi
-
-mkdir -p ./.nuget &> /dev/null
-
-which wget &> /dev/null
-
-if [[ $? == 0 ]]; then
-    wget -O ./.nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-else
-    curl --create-dirs -o ./.nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-fi
-
-mono ./.nuget/nuget.exe sources add -name AvaloniaUI -Source https://www.myget.org/F/avalonia-ci/api/v2
-mono ./.nuget/nuget.exe sources add -name AvaloniaXamlBehaviors -Source https://www.myget.org/F/xamlbehaviors-nightly/api/v2
-mono ./.nuget/nuget.exe sources add -name DockNC -Source https://www.myget.org/F/mabiavalon-ci/api/v2
-mono ./.nuget/nuget.exe restore AvalonStudio.sln
