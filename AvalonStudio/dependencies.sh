935c5199cccf296d8f048e41a33a56b38e07771b
Dan Walmsley <dan@walms.co.uk>
2017-02-05 21:51:08 +0000

    no longer install mono & remove arch build for now.

--- AvalonStudio/dependencies.sh
+++ AvalonStudio/dependencies.sh
@@ -10,9 +10,6 @@ export PATH=$PATH:$(pwd)/bin
 
 source /etc/os-release
 
-if [[ $ID == "arch" ]] ; then
-    
-fi
 if [[ $ID == "ubuntu" ]]; then
     wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/rel-1.0.0/dotnet-sdk-ubuntu-x64.latest.deb
     sudo dpkg -i dotnet-sdk-ubuntu-x64.latest.deb
