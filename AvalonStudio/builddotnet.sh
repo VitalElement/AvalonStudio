0395f4a86c831858fadef356de525d243a6600e9
Dan Walmsley <dan@walms.co.uk>
2017-02-06 00:46:31 +0000

    test build

--- AvalonStudio/builddotnet.sh
+++ AvalonStudio/builddotnet.sh
@@ -1,4 +1,6 @@
 cd AvalonStudio
-
+pwd
+pwd
+pwd
 dotnet restore .\AvalonStudio.sln --no-cache -r win7-x64
 dotnet publish .\AvalonStudio\AvalonStudio.csproj -c Release -r win7-x64 /p:RuntimeIdentifier=win7-x64
