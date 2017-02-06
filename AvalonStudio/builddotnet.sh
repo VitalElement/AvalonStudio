b0edc5bd3f336b6e6225cea679908ec46e2eb5e5
danwalmsley <dan@walms.co.uk>
2017-02-06 12:24:33 +0000

    corrected line endings and invokation of build script.

--- AvalonStudio/builddotnet.sh
+++ AvalonStudio/builddotnet.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-cd AvalonStudio
-pwd
-pwd
-pwd
-dotnet restore .\AvalonStudio.sln --no-cache -r win7-x64
-dotnet publish .\AvalonStudio\AvalonStudio.csproj -c Release -r win7-x64 /p:RuntimeIdentifier=win7-x64
+dotnet restore ./AvalonStudio.sln --no-cache -r win7-x64
+dotnet publish ./AvalonStudio/AvalonStudio.csproj -c Release -r win7-x64 /p:RuntimeIdentifier=win7-x64
