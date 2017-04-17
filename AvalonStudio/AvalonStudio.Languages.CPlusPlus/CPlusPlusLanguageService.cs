cfc42371df82ebfc546dea6f37173890e1cd6c96
Dan Walmsley <dan@walms.co.uk>
2017-04-17 19:55:28 +0100

    Split CPPDataAssociation into a seperate class.

--- AvalonStudio/AvalonStudio.Languages.CPlusPlus/CPlusPlusLanguageService.cs
+++ AvalonStudio/AvalonStudio.Languages.CPlusPlus/CPlusPlusLanguageService.cs
@@ -1198,33 +1198,5 @@ namespace AvalonStudio.Languages.CPlusPlus
         {
             
         }
-    }
-
-    internal class CPlusPlusDataAssociation
-    {
-        public CPlusPlusDataAssociation(TextDocument textDocument)
-        {
-            BackgroundRenderers = new List<IBackgroundRenderer>();
-            DocumentLineTransformers = new List<IDocumentLineTransformer>();
-
-            TextColorizer = new TextColoringTransformer(textDocument);
-            TextMarkerService = new TextMarkerService(textDocument);
-
-            BackgroundRenderers.Add(new BracketMatchingBackgroundRenderer());
-            BackgroundRenderers.Add(TextMarkerService);
-
-            DocumentLineTransformers.Add(TextColorizer);
-            DocumentLineTransformers.Add(new DefineTextLineTransformer());
-            DocumentLineTransformers.Add(new PragmaMarkTextLineTransformer());
-            DocumentLineTransformers.Add(new IncludeTextLineTransformer());
-        }
-
-        public ClangTranslationUnit TranslationUnit { get; set; }
-        public TextColoringTransformer TextColorizer { get; }
-        public TextMarkerService TextMarkerService { get; }
-        public List<IBackgroundRenderer> BackgroundRenderers { get; }
-        public List<IDocumentLineTransformer> DocumentLineTransformers { get; }
-        public EventHandler<KeyEventArgs> KeyUpHandler { get; set; }
-        public EventHandler<TextInputEventArgs> TextInputHandler { get; set; }
-    }
+    }    
 }
