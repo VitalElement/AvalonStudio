c69a566895e44705f98868c89766c78b549088d3
Dan Walmsley <dan@walms.co.uk>
2017-04-19 10:45:17 +0100

    fix build.

--- AvalonStudio/AvalonStudio.Extensibility/ToolBars/GenericToolBarItemDefinition.cs
+++ AvalonStudio/AvalonStudio.Extensibility/ToolBars/GenericToolBarItemDefinition.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AvalonStudio.Extensibility.ToolBars
+﻿namespace AvalonStudio.Extensibility.ToolBars
 {
+    using AvalonStudio.Extensibility.Commands;
+
     public class ToolBarItemDefinition<TCommand> : ToolBarItemDefinition where TCommand : CommandDefinition
     {
         public ToolBarItemDefinition(ToolBarItemGroupDefinition group, int sortOrder, ToolBarItemDisplay display = ToolBarItemDisplay.IconOnly) : base(group, sortOrder, display)
