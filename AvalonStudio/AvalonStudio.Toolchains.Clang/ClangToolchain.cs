4badbcec245f4a0a5f4e68212bb8e15feab05e6e
Dan Walmsley <dan@walms.co.uk>
2017-05-09 17:14:20 +0100

    stm32 and clang toolchains use new library paths.

--- AvalonStudio/AvalonStudio.Toolchains.Clang/ClangToolchain.cs
+++ AvalonStudio/AvalonStudio.Toolchains.Clang/ClangToolchain.cs
@@ -69,12 +69,12 @@ namespace AvalonStudio.Toolchains.Clang
         {
             return new List<string>
             {
-                Path.Combine(ContentDirectory, "arm-none-eabi", "include", "c++", "5.4.1"),
-                Path.Combine(ContentDirectory, "arm-none-eabi", "include", "c++", "5.4.1", "arm-none-eabi"),
-                Path.Combine(ContentDirectory, "arm-none-eabi", "include", "c++", "5.4.1", "backward"),
+                Path.Combine(ContentDirectory, "arm-none-eabi", "include", "c++", "6.3.1"),
+                Path.Combine(ContentDirectory, "arm-none-eabi", "include", "c++", "6.3.1", "arm-none-eabi"),
+                Path.Combine(ContentDirectory, "arm-none-eabi", "include", "c++", "6.3.1", "backward"),
                 Path.Combine(ContentDirectory, "arm-none-eabi", "include"),
-                Path.Combine(ContentDirectory, "lib", "gcc", "arm-none-eabi", "5.4.1", "include"),
-                Path.Combine(ContentDirectory, "lib", "gcc", "arm-none-eabi", "5.4.1", "include-fixed"),
+                Path.Combine(ContentDirectory, "lib", "gcc", "arm-none-eabi", "6.3.1", "include"),
+                Path.Combine(ContentDirectory, "lib", "gcc", "arm-none-eabi", "6.3.1", "include-fixed"),
                 Path.Combine(ContentDirectory, "arm-none-eabi", "include")
             };
         }
