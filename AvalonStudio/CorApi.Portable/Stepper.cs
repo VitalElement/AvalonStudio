362968c58ada1bc57e076b28e8d88ca964079679
Dan Walmsley <dan@walms.co.uk>
2017-05-09 13:11:37 +0100

    more copyright

--- AvalonStudio/CorApi.Portable/Stepper.cs
+++ AvalonStudio/CorApi.Portable/Stepper.cs
@@ -1,4 +1,10 @@
-﻿using SharpDX.Mathematics.Interop;
+﻿// Copyright (c) 2017 Vital Element Avalon Studio - Dan Walmsley dan at walms dot co dot uk
+// 
+// This code is licensed for use only with AvalonStudio. It is not permitted for use in any 
+// project unless explicitly authorized.
+//
+
+using SharpDX.Mathematics.Interop;
 using System;
 using System.Collections.Generic;
 using System.Text;
