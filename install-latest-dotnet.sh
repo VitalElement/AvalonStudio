e2bab57b0e76bb930c19eb06682f8d2daca9790a
Dan Walmsley <dan@walms.co.uk>
2017-05-11 09:28:50 +0100

    fix build server to dotnet 2 preview 1

--- install-latest-dotnet.sh
+++ install-latest-dotnet.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev uuid-dev unzip
-wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-dev-ubuntu-x64.latest.tar.gz
+wget https://download.microsoft.com/download/0/6/5/0656B047-5F2F-4281-A851-F30776F8616D/dotnet-dev-linux-x64.2.0.0-preview1-005977.tar.gz
 mkdir ~/dotnet
-tar zxf dotnet-dev-ubuntu-x64.latest.tar.gz -C ~/dotnet
+tar zxf dotnet-dev-linux-x64.2.0.0-preview1-005977.tar.gz -C ~/dotnet
 sudo ln -s ~/dotnet/dotnet /usr/bin/
 dotnet --info
