91ed1719734cd4abd2bc83ab328e8edbf6025961
Dan Walmsley <dan@walms.co.uk>
2017-05-16 10:57:11 +0100

    Added new updater project, and also migrated metro window to extensibility.
    Extensibility is now netstandard1.6

---
+++ AvalonStudio/AvalonStudio.Updater/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvalonStudio.Extensibility.Controls;
+
+namespace AvalonStudio.Updater
+{
+    public class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
