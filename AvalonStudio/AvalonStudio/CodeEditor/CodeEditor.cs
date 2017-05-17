6e117d89b172645b9088a5e69c2387092c663995
Dan Walmsley <dan@walms.co.uk>
2017-05-17 11:22:49 +0100

    add a code editor control for our new text editor.

---
+++ AvalonStudio/AvalonStudio/CodeEditor/CodeEditor.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using AvaloniaEdit.Editing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.CodeEditor
+{
+    public class CodeEditor : AvaloniaEdit.TextEditor
+    {
+        public CodeEditor()
+        {
+            var lineNumberMargin = new LineNumberMargin(this) { Margin = new Thickness(0, 0, 10, 0) };
+
+            TextArea.LeftMargins.Add(lineNumberMargin);
+        }
+    }
+}
