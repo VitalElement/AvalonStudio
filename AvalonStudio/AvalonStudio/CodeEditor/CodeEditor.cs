5f42eaf906be5230172aacbdfc34db58ea8aba63
Dan Walmsley <dan@walms.co.uk>
2017-05-17 12:02:54 +0100

    Add implementation for breakpoint margin.

--- AvalonStudio/AvalonStudio/CodeEditor/CodeEditor.cs
+++ AvalonStudio/AvalonStudio/CodeEditor/CodeEditor.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using AvaloniaEdit.Editing;
+using AvalonStudio.Debugging;
+using AvalonStudio.Extensibility;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,9 +13,22 @@ namespace AvalonStudio.CodeEditor
     {
         public CodeEditor()
         {
+            var breakpointMargin = new BreakPointMargin(this, IoC.Get<IDebugManager2>().Breakpoints);
+
+            TextArea.LeftMargins.Add(breakpointMargin);
+
             var lineNumberMargin = new LineNumberMargin(this) { Margin = new Thickness(0, 0, 10, 0) };
 
             TextArea.LeftMargins.Add(lineNumberMargin);
+        }
+
+        public int GetOffsetFromPoint(Point point)
+        {
+            var position = GetPositionFromPoint(point);
+
+            var offset = position != null ? Document.GetOffset(position.Value.Location) : -1;
+
+            return offset;
         }
     }
 }
