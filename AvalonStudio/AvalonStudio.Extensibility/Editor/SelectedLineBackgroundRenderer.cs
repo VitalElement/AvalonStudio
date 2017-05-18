b9392ebecab5e88e5d115a93db6d753dd303243c
Dan Walmsley <dan@walms.co.uk>
2017-05-18 14:21:30 +0100

    re-implemented selected line bg renderer with aligned pixels.

--- AvalonStudio/AvalonStudio.Extensibility/Editor/SelectedLineBackgroundRenderer.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Editor/SelectedLineBackgroundRenderer.cs
@@ -1,42 +1,63 @@
 using Avalonia;
 using Avalonia.Media;
+using AvaloniaEdit.Document;
 using AvaloniaEdit.Rendering;
+using AvaloniaEdit.Utils;
 using System;
 
 namespace AvalonStudio.TextEditor.Rendering
 {
     public class SelectedLineBackgroundRenderer : IBackgroundRenderer
     {
-        private readonly IBrush selectedLineBg;
+        private readonly AvaloniaEdit.TextEditor _textEditor;
 
-        public SelectedLineBackgroundRenderer()
+        public static readonly IBrush BackgroundBrush = new SolidColorBrush(Color.FromArgb(22, 0x0e, 0x0e, 0x0e));
+        public static readonly IBrush BorderBrush = new SolidColorBrush(Color.FromArgb(255, 57, 57, 57));
+
+        public Pen BorderPen { get; set; }
+
+        public SelectedLineBackgroundRenderer(AvaloniaEdit.TextEditor textEditor)
         {
-            selectedLineBg = Brush.Parse("#FF0E0E0E");
+            _textEditor = textEditor;
+
+            BorderPen = new Pen(BorderBrush);
         }
 
         public KnownLayer Layer => KnownLayer.Background;
 
-        public event EventHandler<EventArgs> DataChanged;
-
         public void Draw(TextView textView, DrawingContext drawingContext)
         {
-            /*if (textView.SelectionStart == textView.SelectionEnd && textView.CaretIndex != -1 &&
-                textView.CaretIndex <= textView.TextDocument.TextLength)
+            if (_textEditor.SelectionLength == 0 && _textEditor.CaretOffset != -1 &&
+                _textEditor.CaretOffset <= textView.Document.TextLength)
             {
-                var currentLine = textView.TextDocument.GetLineByOffset(textView.CaretIndex);
+                var currentLine = textView.Document.GetLocation(_textEditor.CaretOffset).Line;
 
-                var rects = VisualLineGeometryBuilder.GetRectsForSegment(textView, currentLine, true);
+                var visualLine = textView.GetVisualLine(currentLine);
+                if (visualLine == null) return;
 
-                foreach (var rect in rects)
+                BackgroundGeometryBuilder builder = new BackgroundGeometryBuilder();
+                
+                var linePosY = visualLine.VisualTop - textView.ScrollOffset.Y;
+                var lineBottom = linePosY + visualLine.Height;
+
+                Size pixelSize = PixelSnapHelpers.GetPixelSize(textView);
+                
+                
+                double x = PixelSnapHelpers.PixelAlign(0, pixelSize.Width);
+                double y = PixelSnapHelpers.PixelAlign(linePosY, pixelSize.Height);
+                var x2 = PixelSnapHelpers.PixelAlign(textView.Bounds.Width, pixelSize.Width);
+                var y2 = PixelSnapHelpers.PixelAlign(lineBottom, pixelSize.Height);
+
+                builder.AddRectangle(textView, new Rect(new Point(x, y), new Point(x2, y2)));
+
+                Geometry geometry = builder.CreateGeometry();
+                if (geometry != null)
                 {
-                    var drawRect = new Rect(rect.TopLeft.X, rect.TopLeft.Y, textView.Bounds.Width, rect.Height);
-                    drawingContext.FillRectangle(selectedLineBg, drawRect);
-                }
-            }*/
-        }
+                    drawingContext.DrawGeometry(BackgroundBrush, BorderPen, geometry);
 
-        public void TransformLine(TextView textView, DrawingContext drawingContext, VisualLine line)
-        {
+                    
+                }
+            }
         }
     }
 }
