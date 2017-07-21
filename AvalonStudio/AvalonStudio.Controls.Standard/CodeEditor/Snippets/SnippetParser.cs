5728c66302e6c39e03e209a9adeb23b39cdc454e
danwalmsley <dan@walms.co.uk>
2017-07-21 12:19:52 +0100

    support language specific functions in snippets.

--- AvalonStudio/AvalonStudio.Controls.Standard/CodeEditor/Snippets/SnippetParser.cs
+++ AvalonStudio/AvalonStudio.Controls.Standard/CodeEditor/Snippets/SnippetParser.cs
@@ -1,4 +1,5 @@
 ﻿using AvaloniaEdit.Snippets;
+using AvalonStudio.Languages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,13 @@ namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
 
         public static List<ISnippetElementProvider> SnippetElementProviders { get; } = new List<ISnippetElementProvider> { new DefaultSnippetElementProvider() };        
 
-        public static Snippet Parse(string snippetText)
+        public static Snippet Parse(ILanguageService languageService, string snippetText)
         {
             snippetText = "for (${type=int} ${counter=i} = ${initial=0}; ${counter} ${Caret}< ${end=end}; ${counter}++) {\n\t${Selection}\n}";
 
             snippetText = "private ${type=int} ${field=myVar};\n\npublic ${type} ${property=MyProperty}\n{\n\tget { return ${field}; }\n\tset { ${field} = value; }${Caret}\n}";
+
+            snippetText = "${type=int} ${class=ClassName}::get_${property=Property}()\n{\n\treturn ${toFieldName(property)};\n}\n\nvoid ${class}::set_${property}(${type} value)\n{\t${toFieldName(property)} = value;\n}";
 
             if (snippetText == null)
                 throw new ArgumentNullException("text");
@@ -43,7 +46,7 @@ namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
                     pos = m.Index;
                 }
 
-                snippet.Elements.Add(CreateElementForValue(replaceableElements, m.Groups[1].Value, m.Index, snippetText));
+                snippet.Elements.Add(CreateElementForValue(languageService, replaceableElements, m.Groups[1].Value, m.Index, snippetText));
 
                 pos = m.Index + m.Length;
             }
@@ -60,7 +63,9 @@ namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
             return snippet;
         }
 
-        static SnippetElement CreateElementForValue(Dictionary<string, SnippetReplaceableTextElement> replaceableElements, string val, int offset, string snippetText)
+        readonly static Regex functionPattern = new Regex(@"^([a-zA-Z]+)\(([^\)]*)\)$", RegexOptions.CultureInvariant);
+
+        static SnippetElement CreateElementForValue(ILanguageService languageService, Dictionary<string, SnippetReplaceableTextElement> replaceableElements, string val, int offset, string snippetText)
         {
             SnippetReplaceableTextElement srte;
             int equalsSign = val.IndexOf('=');
@@ -85,6 +90,29 @@ namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
             if (replaceableElements.TryGetValue(val, out srte))
                 return new SnippetBoundElement { TargetElement = srte };
 
+            Match m = functionPattern.Match(val);
+            if (m.Success)
+            {
+                Func<string, string> f = GetFunction(languageService, m.Groups[1].Value);
+                string innerVal = m.Groups[2].Value;
+
+                if (f != null)
+                {                    
+                    if (replaceableElements.TryGetValue(innerVal, out srte))
+                        return new FunctionBoundElement { TargetElement = srte, function = f };
+                    string result2 = GetValue(innerVal);
+                    if (result2 != null)
+                        return new SnippetTextElement { Text = f(result2) };
+                    else
+                        return new SnippetTextElement { Text = f(innerVal) };
+                }
+                else if (replaceableElements.TryGetValue(innerVal, out srte))
+                {
+                    return new SnippetBoundElement { TargetElement = srte };
+                }
+            }
+
+
             string result = GetValue(val);
             if (result != null)
                 return new SnippetTextElement { Text = result };
@@ -102,6 +130,31 @@ namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
             // Todo implement a way to expand environment variables here...
 
             return propertyName;
+        }
+
+        static Func<string, string> GetFunction(ILanguageService languageService, string name)
+        {
+            if ("toLower".Equals(name, StringComparison.OrdinalIgnoreCase))
+                return s => s.ToLower();
+            if ("toUpper".Equals(name, StringComparison.OrdinalIgnoreCase))
+                return s => s.ToUpper();
+
+            if(languageService != null && languageService.SnippetCodeGenerators.ContainsKey(name))
+            {
+                return languageService.SnippetCodeGenerators[name];
+            }
+            
+            return null;
+        }
+
+        sealed class FunctionBoundElement : SnippetBoundElement
+        {
+            internal Func<string, string> function;
+
+            public override string ConvertText(string input)
+            {
+                return function(input);
+            }
         }
     }
 }
