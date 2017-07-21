f36c7462b7e88b3ea065fbe6534f80be1f556828
danwalmsley <dan@walms.co.uk>
2017-07-21 15:11:14 +0100

    add a snippet manager to manage snippets. (unparsed)

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/CodeEditor/Snippets/CodeSnippet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
+{
+    public class CodeSnippet
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Snippet { get; set; }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.Controls.Standard/CodeEditor/Snippets/SnippetManager.cs
@@ -0,0 +1,46 @@
+﻿using AvaloniaEdit.Snippets;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Extensibility.Plugin;
+using AvalonStudio.Languages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Controls.Standard.CodeEditor.Snippets
+{
+    public class SnippetManager : IExtension
+    {
+        public void Activation()
+        {
+        }
+
+        public void BeforeActivation()
+        {
+            IoC.RegisterConstant(this);
+
+            // Todo parse snippets directory.
+
+            AddSnippet("cpp", new CodeSnippet { Name = "propfull", Description = "cpp property implementation", Snippet = "${type=int} ${ClassName}::get_${property=Property}()\n{\n\treturn ${ToFieldName(property)};\n}\n\nvoid ${ClassName}::set_${property}(${type} value)\n{\t${ToFieldName(property)} = value;${Caret}\n}" });
+        }
+
+        public void AddSnippet (string languageId, CodeSnippet snippet)
+        {
+            if(!_snippets.ContainsKey(languageId))
+            {
+                _snippets.Add(languageId, new Dictionary<string, CodeSnippet>());
+            }
+
+            if (!_snippets[languageId].ContainsKey(snippet.Name))
+            {
+                _snippets[languageId][snippet.Name] = snippet;
+            }
+        }
+
+        private Dictionary<string, IDictionary<string, CodeSnippet>> _snippets = new Dictionary<string, IDictionary<string, CodeSnippet>>();
+
+        public IDictionary<string, CodeSnippet> GetSnippets(ILanguageService languageService)
+        {
+            return _snippets[languageService.LanguageId];
+        }
+    }
+}
