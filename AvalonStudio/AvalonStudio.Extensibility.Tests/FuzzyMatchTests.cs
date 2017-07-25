99f386913cb43a56f211843382d08a25bd7ec8df
James Walmsley <james@fullfat-fs.co.uk>
2017-07-25 19:17:12 +0100

    [QuickCommander] Added a fuzzy match algorithm.

---
+++ AvalonStudio/AvalonStudio.Extensibility.Tests/FuzzyMatchTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using AvalonStudio.Extensibility.Utils;
+
+namespace AvalonStudio.Extensibility.Tests
+{
+    public class FuzzyMatchTests
+    {
+        void runtest(string pattern, string str, bool match, int expected_score, string expected_matches)
+        {
+            int score = 0;
+            string format;
+            bool retmatch = FuzzyMatch.fuzzy_match(pattern, str, out score, out format);
+
+            Assert.Equal(match, retmatch);
+            if (retmatch)
+            {
+                Assert.Equal(expected_score, score);
+            }
+            Assert.Equal(expected_matches, format);
+        }
+
+        [Fact]
+        private void test_test_match_25()
+        {
+            runtest("test", "test", true, 25, "{t}{e}{s}{t}");
+        }
+
+        [Fact]
+        private void test_tes_match()
+        {
+            runtest("tes", "test", true, 19, "{t}{e}{s}t");
+        }
+
+        [Fact]
+        private void test_sets_nomatch()
+        {
+            runtest("sets", "test", false, 0, "te{s}t");
+        }
+
+        [Fact]
+        private void test_tt_match()
+        {
+            runtest("tt", "test", true, 8, "{t}es{t}");
+        }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.Extensibility/Utils/FuzzyMatch.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Extensibility.Utils
+{
+    public class FuzzyMatch
+    {
+
+        static public bool fuzzy_match(string pattern, string str, out int pa_score, out string pa_formatStr)
+        {
+            const int adjacency_bonus = 5;
+            const int separator_bonus = 10;
+            const int camel_bonus = 10;
+            const int leading_letter_penalty = -3;
+            const int max_leading_letter_penalty = -9;
+            const int unamtched_letter_penalty = -1;
+
+            // loop vars
+            int score = 0;
+            int patternIdx = 0;
+            int patternLength = pattern.Length;
+            int strIdx = 0;
+            int strLength = str.Length;
+            bool prevMatched = false;
+            bool prevLower = false;
+            bool prevSeparator = true;   // True so first letter match gets separator bonus.
+
+            char? bestLetter = null;
+            char? bestLower = null;
+            int? bestLetterIdx = null;
+            int bestLetterScore = 0;
+
+            string formattedStr = "";
+
+            List<int> matchedIndices = new List<int>();
+
+            while (strIdx != strLength)
+            {
+                char? patternChar = null;
+                if (patternIdx != patternLength) {
+                    patternChar = pattern[patternIdx];
+                }
+
+                char strChar = str[strIdx];
+
+                char? patternLower = null;
+                if (patternChar != null)
+                {
+                    patternLower = Char.ToLower(patternChar.Value);
+                }
+
+                char strLower = Char.ToLower(strChar);
+                char strUpper = Char.ToUpper(strChar);
+
+                bool nextMatch = (patternChar == strLower || patternLower == strLower);
+                bool rematch = (bestLetter == strLower || bestLower == strLower);
+
+                bool advanced = nextMatch && bestLetter.HasValue;
+                bool patternRepeat = (bestLetter == strLower && bestLower == strLower);
+
+                if(advanced || patternRepeat)
+                {
+                    score += bestLetterScore;
+                    matchedIndices.Add(bestLetterIdx.Value);
+                    bestLetter = null;
+                    bestLower = null;
+                    bestLetterIdx = null;
+                    bestLetterScore = 0;
+                }
+
+                if(nextMatch || rematch)
+                {
+                    int newScore = 0;
+                    if(patternIdx == 0)
+                    {
+                        int penalty = Math.Max(strIdx * max_leading_letter_penalty, max_leading_letter_penalty);
+                        score += penalty;
+                    }
+
+                    if(prevMatched)
+                    {
+                        newScore += adjacency_bonus;
+                    }
+
+                    if(prevSeparator)
+                    {
+                        newScore += separator_bonus;
+                    }
+
+                    if(prevLower && strChar == strUpper && strLower != strUpper)
+                    {
+                        newScore += camel_bonus;
+                    }
+
+                    if(nextMatch)
+                    {
+                        patternIdx += 1;
+                    }
+
+                    if(newScore >= bestLetterScore)
+                    {
+                        if(bestLetter.HasValue)
+                        {
+                            score += unamtched_letter_penalty;
+                        }
+
+                        bestLetter = strChar;
+                        bestLower = Char.ToLower(bestLetter.Value);
+                        bestLetterIdx = strIdx;
+                        bestLetterScore = newScore;
+                    }
+
+                    prevMatched = true;
+
+                } else
+                {
+                    //formattedStr += strChar;
+                    score += unamtched_letter_penalty;
+                    prevMatched = false;
+                }
+
+                prevLower = (strChar == strLower || strLower != strUpper);
+                prevSeparator = strChar == '_' || strChar == ' ';
+
+                strIdx += 1;
+            }
+
+            if(bestLetter.HasValue)
+            {
+                score += bestLetterScore;
+                matchedIndices.Add(bestLetterIdx.Value);
+            }
+
+            int lastIdx = 0;
+            for (int i = 0; i < matchedIndices.Count; i++)
+            {
+                var idx = matchedIndices[i];
+                formattedStr += str.Substring(lastIdx, idx - lastIdx) + "{" + str[idx] + "}";
+                lastIdx = idx + 1;
+            }
+
+            formattedStr += str.Substring(lastIdx, str.Length - lastIdx);
+
+            bool matched = patternIdx == patternLength;
+
+            pa_score = score;
+            pa_formatStr = formattedStr;
+
+            return matched;
+        }
+    }
+}
