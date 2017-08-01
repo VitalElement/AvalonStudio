d4d38bc342f26c5b8c9a9bbadf27a3d425a3ce9c
Dan Walmsley <dan@walms.co.uk>
2017-08-01 19:36:06 +0100

    gain access to roslyn without signing.

--- AvalonStudio/AvalonStudio.Projects.OmniSharp/Roslyn/FileTextLoader.cs
+++ AvalonStudio/AvalonStudio.Projects.OmniSharp/Roslyn/FileTextLoader.cs
@@ -55,37 +55,5 @@ namespace AvalonStudio.Projects.OmniSharp.Roslyn
 
             return textAndVersion;
         }
-
-        internal override TextAndVersion LoadTextAndVersionSynchronously(Workspace workspace, DocumentId documentId, CancellationToken cancellationToken)
-        {
-            System.Console.WriteLine($"Roslyn is Loading file sync {_path}");
-            var fileInfo = new System.IO.FileInfo(_path);
-            DateTime prevLastWriteTime = fileInfo.LastWriteTime;
-
-            TextAndVersion textAndVersion;
-
-            // Open file for reading with FileShare mode read/write/delete so that we do not lock this file.
-            using (var stream = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete, bufferSize: 4096, useAsync: false))
-            {
-                var version = VersionStamp.Create(prevLastWriteTime);
-                var text = CreateText(stream, workspace);
-                textAndVersion = TextAndVersion.Create(text, version, _path);
-            }
-
-            // Check if the file was definitely modified and closed while we were reading. In this case, we know the read we got was
-            // probably invalid, so throw an IOException which indicates to our caller that we should automatically attempt a re-read.
-            // If the file hasn't been closed yet and there's another writer, we will rely on file change notifications to notify us
-            // and reload the file.
-            fileInfo = new System.IO.FileInfo(_path);
-            DateTime newLastWriteTime = fileInfo.LastWriteTime;
-            if (!newLastWriteTime.Equals(prevLastWriteTime))
-            {
-                var message = string.Format(WorkspacesResources.File_was_externally_modified_colon_0, _path);
-                throw new IOException(message);
-            }
-
-            return textAndVersion;
-        }
-
     }
 }
