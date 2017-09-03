7a9ce5048b6670f21cbe14d5c1c9bda320e5b710
Dan Walmsley <dan@walms.co.uk>
2017-09-03 11:39:44 +0100

    initial attempt at app launcher (splash screen)

---
+++ AvalonStudio/AvalonStudioLauncher/Program.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using System;
+
+namespace AvalonStudioLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = AppBuilder.Configure<App>();
+
+            builder.UsePlatformDetect().UseSkia();
+
+            builder.AfterSetup(b =>
+            {
+                Console.WriteLine("Launch main app");
+            }).Start<SplashScreen>();
+        }
+    }
+
+    class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            Width = 800;
+            Height = 450;
+            HasSystemDecorations = false;
+        }
+    }
+}
