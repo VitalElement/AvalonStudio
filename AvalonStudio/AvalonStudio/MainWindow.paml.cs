b14032f0ec625f4f386d5ed0fd8ff7d0f1ac1a0f
Dan Walmsley <dan@walms.co.uk>
2017-09-07 12:32:24 +0100

    example of changing resources at runtime.

--- AvalonStudio/AvalonStudio/MainWindow.paml.cs
+++ AvalonStudio/AvalonStudio/MainWindow.paml.cs
@@ -1,13 +1,18 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
 using AvalonStudio.Controls;
 using AvalonStudio.Extensibility;
+using System;
 
 namespace AvalonStudio
 {
     public class MainWindow : MetroWindow
     {
+        private bool set = false;
+
         public MainWindow()
         {
             IoC.RegisterConstant<Window>(this);
@@ -19,6 +24,27 @@ namespace AvalonStudio
             KeyBindings.AddRange(IoC.Get<ShellViewModel>().KeyBindings);
 
             this.AttachDevTools();
+
+            var timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += (sender, e) =>
+            {
+                if(set)
+                {
+                    Resources["AvalonBorderBrush"] = Brushes.Red;
+                }
+                else
+                {
+                    Resources["AvalonBorderBrush"] = Brushes.Blue;
+                }
+
+                set = !set;
+            };
+
+            timer.Start();
+
+            this.Resources.Add("AvalonBorderBrush", Brushes.Green);
         }
 
         private void InitializeComponent()
