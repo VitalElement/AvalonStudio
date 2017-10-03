55fe6fdcc576c1e5910247b33838cfa4932b5b7b
Dan Walmsley <dan@walms.co.uk>
2017-10-03 16:50:05 +0100

    add an extensible settings dialog.

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/SettingsDialog/SettingsDialogViewModel.cs
@@ -0,0 +1,61 @@
+﻿using AvalonStudio.Extensibility;
+using AvalonStudio.Extensibility.Plugin;
+using AvalonStudio.Shell;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AvalonStudio.Controls.Standard.SettingsDialog
+{
+    public class SettingsDialogViewModel : DocumentTabViewModel, IExtension, ISettingsManager
+    {
+        private Dictionary<string, SettingsCategoryViewModel> _categories = new Dictionary<string, SettingsCategoryViewModel>();
+        private ObservableCollection<SettingsCategoryViewModel> _categoryViewModels = new ObservableCollection<SettingsCategoryViewModel>();
+        private SettingsViewModel _selectedSetting;
+
+        public void Activation()
+        {
+            var shell = IoC.Get<IShell>();
+            shell.AddDocument(this);
+        }
+
+        public void BeforeActivation()
+        {
+            IoC.RegisterConstant<ISettingsManager>(this);
+        }
+
+        public void RegisterSettingsDialog(string category, SettingsViewModel viewModel)
+        {
+            if(!_categories.ContainsKey(category))
+            {
+                _categories[category] = new SettingsCategoryViewModel { Title = category, Dialogs = new ObservableCollection<SettingsViewModel>() };
+                _categoryViewModels.Add(_categories[category]);
+            }
+
+            _categories[category].Dialogs.Add(viewModel);
+        }
+
+        public ObservableCollection<SettingsCategoryViewModel> Categories => _categoryViewModels;
+
+        public SettingsViewModel SelectedSetting
+        {
+            get { return _selectedSetting; }
+            set
+            {
+                if (value is SettingsViewModel)
+                {
+                    this.RaiseAndSetIfChanged(ref _selectedSetting, value);
+                }
+            }
+        }
+    }
+
+    public class SettingsCategoryViewModel
+    {
+        public string Title { get; set; }
+
+        public ObservableCollection<SettingsViewModel> Dialogs { get; set; }
+    }
+}
--- AvalonStudio/AvalonStudio/MainWindow.paml.cs
+++ AvalonStudio/AvalonStudio/MainWindow.paml.cs
@@ -2,10 +2,18 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using AvalonStudio.Controls;
+using AvalonStudio.Controls.Standard.SettingsDialog;
 using AvalonStudio.Extensibility;
 
 namespace AvalonStudio
 {
+    public class TestSetting : SettingsViewModel
+    {
+        public TestSetting(string title) : base(title)
+        {
+        }
+    }
+
     public class MainWindow : MetroWindow
     {
         public MainWindow()
@@ -19,6 +27,14 @@ namespace AvalonStudio
             KeyBindings.AddRange(IoC.Get<ShellViewModel>().KeyBindings);
 
             this.AttachDevTools();
+
+            IoC.Get<ISettingsManager>().RegisterSettingsDialog("IDE", new TestSetting("Test1"));
+            IoC.Get<ISettingsManager>().RegisterSettingsDialog("IDE", new TestSetting("Test2"));
+            IoC.Get<ISettingsManager>().RegisterSettingsDialog("IDE", new TestSetting("Test3"));
+
+            IoC.Get<ISettingsManager>().RegisterSettingsDialog("Editor", new TestSetting("Test1"));
+            IoC.Get<ISettingsManager>().RegisterSettingsDialog("Editor", new TestSetting("Test2"));
+            IoC.Get<ISettingsManager>().RegisterSettingsDialog("Editor", new TestSetting("Test3"));
         }
 
         private void InitializeComponent()
