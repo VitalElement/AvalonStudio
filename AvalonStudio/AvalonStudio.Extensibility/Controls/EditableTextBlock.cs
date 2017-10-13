07d4e8946e1d4c94c18e4f6ed733305036ead129
Dan Walmsley <dan@walms.co.uk>
2017-10-13 14:44:02 +0100

    begin to add an editable textblock control.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Controls/EditableTextBlock.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Extensibility.Controls
+{
+    public class EditableTextBlock : TextBlock
+    {
+        public EditableTextBlock()
+        {
+            this.GetObservable(InEditModeProperty).Subscribe(editMode =>
+            {
+
+            });
+        }
+
+        public static readonly StyledProperty<bool> InEditModeProperty =
+            AvaloniaProperty.Register<EditableTextBlock, bool>(nameof(InEditMode));
+
+        public bool InEditMode
+        {
+            get { return GetValue(InEditModeProperty); }
+            set { SetValue(InEditModeProperty, value); }
+        }
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            if(e.MouseButton == MouseButton.Middle)
+            {
+                InEditMode = true;
+            }
+            else if (e.ClickCount == 2)
+            {
+                InEditMode = true;
+            }
+        }
+    }
+}
