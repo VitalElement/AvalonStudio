5657cb6175ea3f2ae4e1838509c56effacea2b33
Dan Walmsley <dan@walms.co.uk>
2017-10-13 17:01:42 +0100

    early prototype of editable textblock

--- AvalonStudio/AvalonStudio.Extensibility/Controls/EditableTextBlock.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Controls/EditableTextBlock.cs
@@ -1,20 +1,30 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
+using Avalonia.LogicalTree;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace AvalonStudio.Extensibility.Controls
+namespace AvalonStudio.Controls
 {
-    public class EditableTextBlock : TextBlock
+    public class EditableTextBlock : TemplatedControl
     {
+        private TextBox _textBox;
+
         public EditableTextBlock()
         {
             this.GetObservable(InEditModeProperty).Subscribe(editMode =>
             {
 
             });
+        }
+
+        public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<EditableTextBlock, string>(nameof(Text));
+
+        public string Text
+        {
+            get { return GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
         }
 
         public static readonly StyledProperty<bool> InEditModeProperty =
@@ -26,11 +36,40 @@ namespace AvalonStudio.Extensibility.Controls
             set { SetValue(InEditModeProperty, value); }
         }
 
+        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+        {
+            base.OnTemplateApplied(e);
+
+            _textBox = e.NameScope.Find<TextBox>("PART_TextBox");
+
+            _textBox.KeyUp += _textBox_KeyUp;
+        }
+
+        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToLogicalTree(e);
+        }
+
+        protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromLogicalTree(e);
+
+            _textBox.KeyUp -= _textBox_KeyUp;
+        }
+
+        private void _textBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                InEditMode = false;
+            }
+        }
+
         protected override void OnPointerPressed(PointerPressedEventArgs e)
         {
             base.OnPointerPressed(e);
 
-            if(e.MouseButton == MouseButton.Middle)
+            if (e.MouseButton == MouseButton.Middle)
             {
                 InEditMode = true;
             }
