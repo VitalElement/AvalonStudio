c4abf7980a9390bec2c0b6bae4d18b0424022a96
Dan Walmsley <dan@walms.co.uk>
2017-10-24 14:16:34 +0100

    test removing folder removes child items.

--- AvalonStudio/AvalonStudio.Extensibility.Tests/VisualStudioSolutionTests.cs
+++ AvalonStudio/AvalonStudio.Extensibility.Tests/VisualStudioSolutionTests.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using AvalonStudio.Platforms;
 using Xunit;
+using AvalonStudio.Projects;
+using System.Linq;
 
 namespace AvalonStudio.Extensibility.Tests
 {
@@ -22,6 +24,33 @@ namespace AvalonStudio.Extensibility.Tests
             solution.RemoveItem(startupProject);
 
             Assert.Null(solution.StartupProject);
+        }
+
+        [Fact]
+        public void Removing_Solution_Folder_Removes_Children()
+        {
+            var solution = VisualStudioSolution.Create(@"c:\TestProject\Test.sln", "Test", false);
+            solution.SaveEnabled = false;
+
+            var startupProject = new TestProject("c:\\TestProject\\Project1\\Project.csproj");
+
+            solution.AddItem(startupProject);
+
+            solution.StartupProject = startupProject;
+
+            var folder = solution.AddItem(SolutionFolder.Create("SubFolder"));
+
+            solution.AddItem(new TestProject("c:\\TestProject\\Project1\\Project1.csproj"), folder);
+            solution.AddItem(new TestProject("c:\\TestProject\\Project1\\Project2.csproj"), folder);
+
+            Assert.Equal(3, solution.Projects.Count());
+            Assert.Equal(4, solution.Model.Projects.Count);
+
+            solution.RemoveItem(folder);
+            Assert.Equal(1, solution.Projects.Count());
+            Assert.Equal(1, solution.Model.Projects.Count);
+
+            Assert.Equal(startupProject, solution.Projects.FirstOrDefault());
         }
     }
 }
