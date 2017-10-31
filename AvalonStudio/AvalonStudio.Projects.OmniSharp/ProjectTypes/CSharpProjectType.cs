914a04c19f3f61fa0955e9db6c3dd07281588e98
Dan Walmsley <dan@walms.co.uk>
2017-10-31 15:46:20 +0000

    support .net framework project types.

--- AvalonStudio/AvalonStudio.Projects.OmniSharp/ProjectTypes/CSharpProjectType.cs
+++ AvalonStudio/AvalonStudio.Projects.OmniSharp/ProjectTypes/CSharpProjectType.cs
@@ -1,33 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AvalonStudio.Projects.OmniSharp.ProjectTypes
 {
-    class CSharpProjectType : IProjectType
+    class CSharpProjectType : DotNetCoreCSharpProjectType
     {
-        public static Guid TypeId = Guid.Parse("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}");
+        public static Guid CSharpProjectTypeId = Guid.Parse("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}");
 
-        public Guid ProjectTypeId { get; } = TypeId;
-
-        public List<string> Extensions { get; } = new List<string>
-        {
-            "csproj"
-        };
-
-        public string Description => "Dotnet Core C# Projects";
-
-        public void Activation()
-        {
-        }
-
-        public void BeforeActivation()
-        {
-        }
-
-        public IProject Load(ISolution solution, string filePath)
-        {
-            return OmniSharpProject.Create(solution, filePath).GetAwaiter().GetResult();
-        }
+        public override Guid ProjectTypeId => CSharpProjectTypeId;
     }
 }
