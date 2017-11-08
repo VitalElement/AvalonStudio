470a87d132b7939d5aed994088468c2ec3a128dd
Dan Walmsley <dan@walms.co.uk>
2017-11-08 14:33:59 +0000

    fix expanding startup item and parents in tree.

--- AvalonStudio/AvalonStudio/ShellViewModel.cs
+++ AvalonStudio/AvalonStudio/ShellViewModel.cs
@@ -726,9 +726,11 @@ namespace AvalonStudio
 
                 if (solutionType != null)
                 {
-                    CurrentSolution = await solutionType.LoadAsync(path);
+                    var solution = await solutionType.LoadAsync(path);
 
-                    await CurrentSolution.LoadSolutionAsync();
+                    await solution.LoadSolutionAsync();
+
+                    CurrentSolution = solution;
 
                     await CurrentSolution.LoadProjectsAsync();
                 }
