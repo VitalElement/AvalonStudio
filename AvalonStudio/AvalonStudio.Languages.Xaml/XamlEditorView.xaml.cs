129a0be23dc7ccfe6336645b88609dbf8b886d10
Nikita Tsukanov <keks9n@gmail.com>
2017-11-09 12:55:47 +0300

    Enable syntax highlighting for XAML

--- AvalonStudio/AvalonStudio.Languages.Xaml/XamlEditorView.xaml.cs
+++ AvalonStudio/AvalonStudio.Languages.Xaml/XamlEditorView.xaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaEdit.Highlighting;
 
 namespace AvalonStudio.Languages.Xaml
 {
@@ -8,6 +9,8 @@ namespace AvalonStudio.Languages.Xaml
         public XamlEditorView()
         {
             InitializeComponent();
+            var editor = this.FindControl<AvaloniaEdit.TextEditor>("editor");
+            editor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("XML");
         }
 
         private void InitializeComponent()
