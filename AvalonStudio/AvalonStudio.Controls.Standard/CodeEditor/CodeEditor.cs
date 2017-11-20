876b13e87d186fb7a9330ced0c87f11f3eb391a2
Dan Walmsley <dan@walms.co.uk>
2017-11-20 14:56:59 +0000

    fix code editor tooltip crashes.

--- AvalonStudio/AvalonStudio.Controls.Standard/CodeEditor/CodeEditor.cs
+++ AvalonStudio/AvalonStudio.Controls.Standard/CodeEditor/CodeEditor.cs
@@ -499,27 +499,30 @@ namespace AvalonStudio.Controls.Standard.CodeEditor
                     return new ErrorProbeViewModel(matching);
                 }
 
-                var symbol = await LanguageService?.GetSymbolAsync(SourceFile, UnsavedFiles, offset);
-
-                if(symbol != null)
+                if(LanguageService != null)
                 {
-                    switch (symbol.Kind)
+                    var symbol = await LanguageService?.GetSymbolAsync(SourceFile, UnsavedFiles, offset);
+
+                    if(symbol != null)
                     {
-                        case CursorKind.CompoundStatement:
-                        case CursorKind.NoDeclarationFound:
-                        case CursorKind.NotImplemented:
-                        case CursorKind.FirstDeclaration:
-                        case CursorKind.InitListExpression:
-                        case CursorKind.IntegerLiteral:
-                        case CursorKind.ReturnStatement:
-                        case CursorKind.WhileStatement:
-                        case CursorKind.BinaryOperator:
-                            return null;
+                        switch (symbol.Kind)
+                        {
+                            case CursorKind.CompoundStatement:
+                            case CursorKind.NoDeclarationFound:
+                            case CursorKind.NotImplemented:
+                            case CursorKind.FirstDeclaration:
+                            case CursorKind.InitListExpression:
+                            case CursorKind.IntegerLiteral:
+                            case CursorKind.ReturnStatement:
+                            case CursorKind.WhileStatement:
+                            case CursorKind.BinaryOperator:
+                                return null;
 
-                        default:
-                            return new SymbolViewModel(symbol);
+                            default:
+                                return new SymbolViewModel(symbol);
+                        }
+
                     }
-
                 }
 
                 var tooltipRequestEventArgs = new TooltipDataRequestEventArgs();
