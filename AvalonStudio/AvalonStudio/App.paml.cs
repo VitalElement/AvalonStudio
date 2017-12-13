ad256a2938b8fe7f8bf0819b115fdb6185694670
Dan Walmsley <dan@walms.co.uk>
2017-12-13 22:20:42 +0000

    add msft templating engine to solution ready for development

--- AvalonStudio/AvalonStudio/App.paml.cs
+++ AvalonStudio/AvalonStudio/App.paml.cs
@@ -7,16 +7,72 @@ using AvalonStudio.Controls;
 using AvalonStudio.Extensibility.Projects;
 using AvalonStudio.Platforms;
 using AvalonStudio.Repositories;
+using Microsoft.TemplateEngine.Cli;
+using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Edge.TemplateUpdates;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Config;
+using Microsoft.TemplateEngine.Utils;
 using Serilog;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
 
 namespace AvalonStudio
 {
     internal class App : Application
     {
+        private const string HostIdentifier = "dotnetcli-preview";
+        private const string HostVersion = "1.0.0";
+        private const string CommandName = "new3";
+
+        private static DefaultTemplateEngineHost CreateHost(bool emitTimings)
+        {
+            var preferences = new Dictionary<string, string>
+            {
+                { "prefs:language", "C#" }
+            };
+
+            try
+            {
+                string versionString = Dotnet.Version().CaptureStdOut().Execute().StdOut;
+                if (!string.IsNullOrWhiteSpace(versionString))
+                {
+                    preferences["dotnet-cli-version"] = versionString.Trim();
+                }
+            }
+            catch
+            { }
+
+            var builtIns = new AssemblyComponentCatalog(new[]
+            {
+                typeof(RunnableProjectGenerator).GetTypeInfo().Assembly,
+                typeof(ConditionalConfig).GetTypeInfo().Assembly,
+                typeof(NupkgInstallUnitDescriptorFactory).GetTypeInfo().Assembly
+            });
+
+            DefaultTemplateEngineHost host = new DefaultTemplateEngineHost(HostIdentifier, HostVersion, CultureInfo.CurrentCulture.Name, preferences, builtIns, new[] { "dotnetcli" });
+
+            if (emitTimings)
+            {
+                host.OnLogTiming = (label, duration, depth) =>
+                {
+                    string indent = string.Join("", Enumerable.Repeat("  ", depth));
+                    Console.WriteLine($"{indent} {label} {duration.TotalMilliseconds}");
+                };
+            }
+
+
+            return host;
+        }
+
         [STAThread]
         private static void Main(string[] args)
         {
+            DefaultTemplateEngineHost host = CreateHost(false);            
+
             if (args == null)
             {
                 throw new ArgumentNullException(nameof(args));
