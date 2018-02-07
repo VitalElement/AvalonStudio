66b3ba6ec29fb493912c1fb5e50eadfa34e6cadb
Dan Walmsley <dan@walms.co.uk>
2018-02-07 21:09:22 +0000

    fix travis.

--- install-latest-dotnet.sh
+++ install-latest-dotnet.sh
@@ -2,7 +2,7 @@
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-artful-prod artful main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 sudo apt-get install apt-transport-https
 sudo apt-get update
