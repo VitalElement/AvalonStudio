eb1fbd5df2915641ae415484131abbb7b30801ab
José Pedro <jpedrolmail@gmail.com>
2018-02-26 14:10:27 +0000

    Implemented simple extension discovery.

---
+++ AvalonStudio/AvalonStudio.Extensibility/ExtensionManager.cs
@@ -0,0 +1,37 @@
+﻿using AvalonStudio.Platforms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AvalonStudio.Extensibility
+{
+    public class ExtensionManager
+    {
+        public IEnumerable<IExtensionManifest> LoadExtensions()
+        {
+            foreach (var directory in Directory.GetDirectories(Platform.ExtensionsFolder))
+            {
+                var extensionManifest = Path.Combine(directory, "extension.json");
+
+                if (File.Exists(extensionManifest))
+                {
+                    IExtensionManifest extension = null;
+
+                    try
+                    {
+                        extension = ExtensionManifest.LoadFromManifest(extensionManifest);
+                    }
+                    catch (Exception e)
+                    {
+                        // todo: log exception
+                    }
+
+                    if (extension != null)
+                    {
+                        yield return extension;
+                    }
+                }
+            }
+        }
+    }
+}
