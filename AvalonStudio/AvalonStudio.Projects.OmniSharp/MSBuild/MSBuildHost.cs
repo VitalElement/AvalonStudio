c309696c4f615cb0e9f44e954a92d5c660e6c89d
Dan Walmsley <dan@walms.co.uk>
2018-02-27 14:39:25 +0000

    use msbuild directly.

--- AvalonStudio/AvalonStudio.Projects.OmniSharp/MSBuild/MSBuildHost.cs
+++ AvalonStudio/AvalonStudio.Projects.OmniSharp/MSBuild/MSBuildHost.cs
@@ -61,7 +61,7 @@ namespace AvalonStudio.Projects.OmniSharp.MSBuild
                         var path = typeof(NextRequestType).Assembly.GetModules()[0].FullyQualifiedName;
                         path = Path.Combine(Path.GetDirectoryName(path), "host.csproj");
 
-                        string args = $"msbuild /p:AvaloniaIdePort={l.Port} {path}";
+                        string args = $"\"{_sdkPath}MSBuild.dll\" /p:AvaloniaIdePort={l.Port} {path}";
 
                         Console.WriteLine(args);
 
