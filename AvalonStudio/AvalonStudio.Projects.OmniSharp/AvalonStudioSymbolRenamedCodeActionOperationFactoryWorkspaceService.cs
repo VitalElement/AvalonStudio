384e0806c448557f303c7dd83310c1fc92ebd135
Dan Walmsley <dan@walms.co.uk>
2018-03-14 20:59:57 +0000

    minimal implementation of SymbolRenamedCodeAction factory.

--- AvalonStudio/AvalonStudio.Projects.OmniSharp/AvalonStudioSymbolRenamedCodeActionOperationFactoryWorkspaceService.cs
+++ AvalonStudio/AvalonStudio.Projects.OmniSharp/AvalonStudioSymbolRenamedCodeActionOperationFactoryWorkspaceService.cs
@@ -1,24 +1,16 @@
-﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-
-using System;
-using System.Collections.Generic;
-using System.Composition;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeActions.WorkspaceServices;
-using Microsoft.CodeAnalysis.Editor;
 using Microsoft.CodeAnalysis.Host.Mef;
+using System;
+using System.Composition;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation
 {
-    using Workspace = Microsoft.CodeAnalysis.Workspace;
-
     [ExportWorkspaceService(typeof(ISymbolRenamedCodeActionOperationFactoryWorkspaceService), ServiceLayer.Host), Shared]
-    internal sealed class VisualStudioSymbolRenamedCodeActionOperationFactoryWorkspaceService : ISymbolRenamedCodeActionOperationFactoryWorkspaceService
+    internal sealed class AvalonStudioRenameSymbolCodeActionOperationFactoryWorkspaceService : ISymbolRenamedCodeActionOperationFactoryWorkspaceService
     {
-        public VisualStudioSymbolRenamedCodeActionOperationFactoryWorkspaceService()
+        public AvalonStudioRenameSymbolCodeActionOperationFactoryWorkspaceService()
         {
         }
 
@@ -48,23 +40,6 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation
                 _newName = newName;
                 _startingSolution = startingSolution;
                 _updatedSolution = updatedSolution;
-            }
-
-            public override void Apply(Workspace workspace, CancellationToken cancellationToken = default)
-            {
-                //var updatedDocumentIds = _updatedSolution.GetChanges(_startingSolution).GetProjectChanges().SelectMany(p => p.GetChangedDocuments());
-
-                //foreach (var refactorNotifyService in _refactorNotifyServices)
-                //{
-                //    // If something goes wrong and some language service rejects the rename, we 
-                //    // can't really do anything about it because we're potentially in the middle of
-                //    // some unknown set of CodeActionOperations. This is a best effort approach.
-
-                //    if (refactorNotifyService.TryOnBeforeGlobalSymbolRenamed(workspace, updatedDocumentIds, _symbol, _newName, throwOnFailure: false))
-                //    {
-                //        refactorNotifyService.TryOnAfterGlobalSymbolRenamed(workspace, updatedDocumentIds, _symbol, _newName, throwOnFailure: false);
-                //    }
-                //}
             }
 
             public override string Title => string.Format("Rename '{0}' to '{1}'", _symbol.Name, _newName);
