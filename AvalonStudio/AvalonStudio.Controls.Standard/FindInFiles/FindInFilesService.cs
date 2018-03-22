75518b893341da8acc4080c4c086453d0a8a7f4f
Dan Walmsley <dan@walms.co.uk>
2018-03-22 20:10:56 +0000

    add initial implementation of scanning through files to search.

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/FindInFiles/FindInFilesService.cs
@@ -0,0 +1,63 @@
+﻿using AvaloniaEdit.Document;
+using AvaloniaEdit.Search;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Projects;
+using AvalonStudio.Shell;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AvalonStudio.Controls.Standard.FindInFiles
+{
+    public class FindResult
+    {
+        public FindResult(ISourceFile file, int offset, int length, int lineOffset, string lineText)
+        {
+            File = file;
+            Offset = offset;
+            Length = length;
+            LineOffset = lineOffset;
+            LineText = lineText;
+        }
+
+        public ISourceFile File { get; }
+        public int Offset { get; }
+        public int Length { get; }
+
+        public int LineOffset { get; }
+        public string LineText { get; }
+    }
+
+
+    public class FindInFilesService : IFindInFilesService
+    {
+        private IEnumerable<FindResult> GetResults(ISearchStrategy strategy, ISourceFile file)
+        {
+            using (var fileStream = file.OpenText())
+            {
+                using (var reader = new StreamReader(fileStream))
+                {
+                    var document = new TextDocument(reader.ReadToEnd());
+
+                    var results = strategy.FindAll(document, 0, document.TextLength);
+
+                    foreach (var result in results)
+                    {
+                        var line = document.GetLineByOffset(result.Offset);
+
+                        yield return new FindResult(file, result.Offset, result.Length, line.Offset, document.GetText(line));
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<FindResult> Find(string searchString)
+        {
+            var shell = IoC.Get<IShell>();
+
+            var searchStrategy = SearchStrategyFactory.Create(searchString, true, false, SearchMode.Normal);
+
+            return shell.CurrentSolution.Projects.SelectMany(p => p.SourceFiles).SelectMany(f => GetResults(searchStrategy, f));
+        }
+    }
+}
---
+++ AvalonStudio/AvalonStudio.Controls.Standard/FindInFiles/IFindInFilesService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AvalonStudio.Controls.Standard.FindInFiles
+{
+    public interface IFindInFilesService
+    {
+        IEnumerable<FindResult> Find(string searchString)
+    }
+}
