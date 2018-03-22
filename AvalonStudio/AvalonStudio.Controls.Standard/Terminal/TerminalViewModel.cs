10dfee701235f25e55bcfc08b5eca1efd42a6455
Dan Walmsley <dan@walms.co.uk>
2018-03-22 18:14:27 +0000

    prep for integration of vt100 terminal.

---
+++ AvalonStudio/AvalonStudio.Controls.Standard/Terminal/TerminalViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AvalonStudio.Extensibility.Plugin;
+using AvalonStudio.MVVM;
+using System;
+
+namespace AvalonStudio.Controls.Standard.Terminal
+{
+    class TerminalViewModel : ToolViewModel, IExtension
+    {
+        public override Location DefaultLocation => Location.BottomRight;
+
+        public void Activation()
+        {
+        }
+
+        public void BeforeActivation()
+        {
+        }
+    }
+}
