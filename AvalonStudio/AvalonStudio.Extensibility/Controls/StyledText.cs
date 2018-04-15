9695b25cbad59c4f2bd1532cf990fa8423ca02e9
Dan Walmsley <dan@walms.co.uk>
2018-04-15 14:46:45 +0100

    implement a textblock that accepts styled text.

---
+++ AvalonStudio/AvalonStudio.Extensibility/Controls/StyledText.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia.Media;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvalonStudio.Controls
+{
+    public class StyledText
+    {
+        private StringBuilder _builder;
+        private List<FormattedTextStyleSpan> _spans;
+
+        private StyledText ()
+        {
+            _builder = new StringBuilder();
+            _spans = new List<FormattedTextStyleSpan>();
+        }
+
+        public static StyledText Create() => new StyledText();
+
+        public string Text => _builder.ToString();
+        public IReadOnlyList<FormattedTextStyleSpan> Spans => _spans.AsReadOnly();
+
+        public StyledText AppendLine(string text, IBrush brush = null)
+        {
+            if (brush != null)
+            {
+                _spans.Add(new FormattedTextStyleSpan(_builder.Length, text.Length, brush));
+            }
+
+            _builder.AppendLine(text);
+
+            return this;
+        }
+
+        public StyledText Append(string text, IBrush brush = null)
+        {
+            if (brush != null)
+            {
+                _spans.Add(new FormattedTextStyleSpan(_builder.Length, text.Length, brush));
+            }
+
+            _builder.Append(text);
+
+            return this;
+        }
+
+        public StyledText Clear ()
+        {
+            _builder.Clear();
+            _spans.Clear();
+            return this;
+        }
+    }
+}
