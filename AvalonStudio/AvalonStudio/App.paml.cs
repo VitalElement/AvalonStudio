e0e96c8565c4a5184c8f91b3d0fd3e432cb3fe76
Dan Walmsley <dan@walms.co.uk>
2018-05-14 16:45:28 +0100

    test for @marchincube with gpu rendering.

--- AvalonStudio/AvalonStudio/App.paml.cs
+++ AvalonStudio/AvalonStudio/App.paml.cs
@@ -57,7 +57,7 @@ namespace AvalonStudio
         }
 
         public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>().UsePlatformDetect().UseReactiveUI();
+            => AppBuilder.Configure<App>().UseWin32().UseSkia(Avalonia.Skia.RenderBackendType.OpenGL).UseReactiveUI();
 
         public override void Initialize()
         {
