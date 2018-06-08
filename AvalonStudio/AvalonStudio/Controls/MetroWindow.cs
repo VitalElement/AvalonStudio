097e04ed901b9017e1de0500360bbb0b6eac32c5
danwalmsley <dan@walms.co.uk>
2018-06-08 23:16:02 +0100

    Merge pull request #678 from lontivero/fix-5
    Adds tooltips to windows upper right buttons

--- AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
+++ AvalonStudio/AvalonStudio/Controls/MetroWindow.cs
@@ -113,10 +113,12 @@ namespace AvalonStudio.Controls
             {
                 case WindowState.Maximized:
                     WindowState = WindowState.Normal;
+                    ToolTip.SetTip(restoreButton, "Maximize");
                     break;
 
                 case WindowState.Normal:
                     WindowState = WindowState.Maximized;
+                    ToolTip.SetTip(restoreButton, "Restore");
                     break;
             }
         }
