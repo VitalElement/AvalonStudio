f1c7a53dc38622c1df8dc9af67e66476bb3db194
Dan Walmsley <dan@walms.co.uk>
2018-07-28 18:18:50 +0100

    fix parameter name.

--- AvalonStudio/AvalonStudio.Extensibility/Editor/ICodeEditorInputHelper.cs
+++ AvalonStudio/AvalonStudio.Extensibility/Editor/ICodeEditorInputHelper.cs
@@ -7,6 +7,6 @@ namespace AvalonStudio.Editor
     public interface ICodeEditorInputHelper
     {
         bool BeforeTextInput(ILanguageService languageService, ITextEditor editor,  string inputText);
-        bool AfterTextInput(ILanguageService languageServivce, ITextEditor document, string inputText);
+        bool AfterTextInput(ILanguageService languageServivce, ITextEditor editor, string inputText);
     }
 }
