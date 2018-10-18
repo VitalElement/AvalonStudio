76b3031d07005fc9056f1620290aa931016fbc70
Dan Walmsley <dan@walms.co.uk>
2018-10-18 22:06:56 +0100

    copy osx zip to artifacts dir.

--- packages/package-osx.sh
+++ packages/package-osx.sh
@@ -3,5 +3,5 @@ rm -rf ./osx/AvalonStudio.app/Contents/MacOS
 mkdir ./osx/AvalonStudio.app/Contents/MacOS
 cp -R ../AvalonStudio/AvalonStudio/bin/Release/netcoreapp2.1/osx-x64/publish/ ./osx/AvalonStudio.app/Contents/MacOS/
 cd osx
-zip -r -X ../AvalonStudio.osx-x64-$BUILD_SOURCEBRANCHNAME-$BUILD_SOURCEVERSION.zip *
+zip -r -X ../artifacts/AvalonStudio.osx-x64-$BUILD_SOURCEBRANCHNAME-$BUILD_SOURCEVERSION.zip *
 cd ..
