891f35661ef1302b3894e3c2e6bd7ff84c5bd70a
Dan Walmsley <dan@walms.co.uk>
2018-10-18 20:30:05 +0100

    fix mac packaging
    github release.

--- packages/package-osx.sh
+++ packages/package-osx.sh
@@ -1,3 +1,5 @@
 dotnet publish -c Release -f netcoreapp2.1 -r osx-x64 ../AvalonStudio/AvalonStudio
 rm -rf ./osx/AvalonStudio/Contents/MacOS
+mkdir ./osx/AvalonStudio/Contents/MacOS
 cp -R ../AvalonStudio/AvalonStudio/bin/Release/netcoreapp2.1/osx-x64/publish/ ./osx/AvalonStudio.app/Contents/MacOS/
+zip -r -X ./AvalonStudio.osx-x64.zip ./osx/AvalonStudio.app
