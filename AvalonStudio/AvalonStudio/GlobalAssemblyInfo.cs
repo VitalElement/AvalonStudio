fbacdd4318f5a91b424a689e425cb75b381df0a5
Dan Walmsley <dan@walms.co.uk>
2019-09-10 01:38:21 +0100

    update assembly info

--- AvalonStudio/AvalonStudio/GlobalAssemblyInfo.cs
+++ AvalonStudio/AvalonStudio/GlobalAssemblyInfo.cs
@@ -36,7 +36,11 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyFileVersion("1.0.0.0")]
 //
 
-
-[assembly: AssemblyVersion (ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
-[assembly: AssemblyFileVersion (ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
-
+[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+[assembly: AssemblyFileVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.Commits + "-" +
+    ThisAssembly.Git.Branch + "+" +
+    ThisAssembly.Git.Commit)]
